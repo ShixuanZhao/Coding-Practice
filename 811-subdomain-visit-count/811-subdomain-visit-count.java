38bf31ab48378044bb4e608f6552f2782669a4d9
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-01-07 18:25:48 -0800

    Time: 21 ms (51.05%), Space: 47.7 MB (25.98%) - LeetHub

--- 811-subdomain-visit-count/811-subdomain-visit-count.java
+++ 811-subdomain-visit-count/811-subdomain-visit-count.java
@@ -2,15 +2,15 @@ class Solution {
     public List<String> subdomainVisits(String[] cpdomains) {
         List<String> res = new ArrayList<>();
         Map<String, Integer> map = new HashMap<>();
-        for (String cd : cpdomains) {
+        for (String s : cpdomains) {
             //find the index of " "
-            int i = cd.indexOf(' ');
-            int n = Integer.valueOf(cd.substring(0, i));
-            String s = cd.substring(i + 1);
-            map.put(s, map.getOrDefault(s, 0) + n);
-            for (i = 0; i < s.length(); i++) {
-                if (s.charAt(i) == '.') {
-                    String d = s.substring(i + 1);
+            int i = s.indexOf(' ');
+            int n = Integer.valueOf(s.substring(0, i));
+            String s1 = s.substring(i + 1);
+            map.put(s1, map.getOrDefault(s1, 0) + n);
+            for (int j = 0; j < s1.length(); j++) {
+                if (s1.charAt(j) == '.') {
+                    String d = s1.substring(j + 1);
                     map.put(d, map.getOrDefault(d, 0) + n);
                 }
             }
