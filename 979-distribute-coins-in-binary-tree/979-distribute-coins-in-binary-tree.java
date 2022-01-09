4f18e4535f397e53b930e0ea8aef53c5913c688b
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-01-09 13:47:12 -0800

    Time: 1 ms (24.35%), Space: 39.4 MB (16.70%) - LeetHub

---
+++ 979-distribute-coins-in-binary-tree/979-distribute-coins-in-binary-tree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+//     Quite similar problem as this one 968.Binary Tree Cameras.
+// Write a dfs helper, return the number of coins given to the parent.
+    int res = 0;
+    public int distributeCoins(TreeNode root) {
+        dfs(root);
+        return res;
+    }
+    
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        res += Math.abs(left) + Math.abs(right);
+        return left + right + root.val - 1;
+    }
+}
