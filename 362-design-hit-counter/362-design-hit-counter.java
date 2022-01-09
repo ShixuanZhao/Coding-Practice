8d4712d522e36cb34ddf9beb1c83af5cf8fcc059
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-01-08 20:41:18 -0800

    Time: 1 ms (92.87%), Space: 37.2 MB (50.51%) - LeetHub

---
+++ 362-design-hit-counter/362-design-hit-counter.java
@@ -0,0 +1,35 @@
+class HitCounter {
+    int[] time;
+    int[] hit;
+    public HitCounter() {
+        time = new int[300];
+        hit = new int[300];
+    }
+    
+    public void hit(int timestamp) {
+        int index = timestamp % 300;
+        if (time[index] != timestamp) {
+            time[index] = timestamp;
+            hit[index] = 1;
+        } else {
+            hit[index]++;
+        }
+    }
+    
+    public int getHits(int timestamp) {
+        int res = 0;
+        for (int i = 0; i < 300; i++) {
+            if (timestamp - time[i] < 300) {
+                res += hit[i];
+            }
+        }
+        return res;
+    }
+}
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter obj = new HitCounter();
+ * obj.hit(timestamp);
+ * int param_2 = obj.getHits(timestamp);
+ */
