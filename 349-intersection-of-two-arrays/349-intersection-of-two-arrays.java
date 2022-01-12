8a99f8479c48c8cc274aac1ae06aad68f1afc072
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-01-11 18:44:31 -0800

    Time: 5 ms (28.80%), Space: 40.8 MB (13.04%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.java
@@ -1,21 +1,45 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         //M1 two hashset
+        // Set<Integer> set = new HashSet<>();
+        // Set<Integer> intersection = new HashSet<>();
+        // for (int num : nums1) {
+        //     set.add(num);
+        // }
+        // for (int num : nums2) {
+        //     if (set.contains(num)) {
+        //         intersection.add(num);
+        //     }
+        // }
+        // int i = 0;
+        // int[] res = new int[intersection.size()];
+        // for (int num : intersection) {
+        //     res[i++] = num;
+        // }
+        // return res;
+        
+        //Sort both arrays, use two pointers
         Set<Integer> set = new HashSet<>();
-        Set<Integer> intersection = new HashSet<>();
-        for (int num : nums1) {
-            set.add(num);
-        }
-        for (int num : nums2) {
-            if (set.contains(num)) {
-                intersection.add(num);
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                set.add(nums1[i]);
+                i++;
+                j++;
             }
         }
-        int i = 0;
-        int[] res = new int[intersection.size()];
-        for (int num : intersection) {
-            res[i++] = num;
+        int[] result = new int[set.size()];
+        int k = 0;
+        for (Integer num : set) {
+            result[k++] = num;
         }
-        return res;
+        return result;
     }
 }
