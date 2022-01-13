ab31252b8b0e20431b45339b6158b4c7fbcbfd7e
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-01-13 02:15:41 -0800

    Time: 0 ms (100.00%), Space: 38.3 MB (12.84%) - LeetHub

--- 168-excel-sheet-column-title/168-excel-sheet-column-title.java
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.java
@@ -2,6 +2,8 @@ class Solution {
     public String convertToTitle(int columnNumber) {
         StringBuilder sb = new StringBuilder();
         while (columnNumber > 0) {
+            //!!!!!!!
+            //because it is 26-nary problem, but we start from 1
             columnNumber--;
             sb.append((char)(columnNumber % 26 + 'A'));
             columnNumber /= 26;
