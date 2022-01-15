5c6a18afc3895bbfcd7fa17ca06b05f777c3b838
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-01-14 23:10:08 -0800

    Time: 8 ms (46.96%), Space: 75.5 MB (9.14%) - LeetHub

---
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -0,0 +1,58 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        //有可能重复的数字出现多次
+        //M1:HashSet
+        // Set<Integer> set = new HashSet<>();
+        // for (int num : nums) {
+        //     if (!set.add(num)) {
+        //         return num;
+        //     }
+        // }
+        // return -1;
+        
+        //sort
+        // Arrays.sort(nums);
+        // for (int i = 0; i < nums.length - 1; i++) {
+        //     if (nums[i] == nums[i + 1]) {
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
+        
+        //swap-swap
+//         for (int i = 0; i < nums.length - 1; i++) {
+//              // 如果元素的 值 和 下标 不匹配，则将其交换至对的位置
+//             while (i != nums[i] - 1) {
+//                 // 如果发现待交换的两个元素相同则直接返回 如：[3,1,3,4,2]
+//                 if (nums[i] == nums[nums[i] - 1]) {
+//                     return nums[i];
+//                 }
+//                 swap(nums, i, nums[i] - 1);
+//             }
+//         }
+//         return nums[nums.length - 1];
+    
+//     private void swap(int[] nums, int i, int j) {
+//         int temp = nums[i];
+//         nums[i] = nums[j];
+//         nums[j] = temp;
+//     }
+        
+        //O（n） O（1）
+        //转换成find the start of circle in linkedlist
+        int slow = nums[0];
+        int fast = nums[0];
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (slow == fast) {
+                fast = nums[0];
+                while (fast != slow) {
+                    slow = nums[slow];
+                    fast = nums[fast];
+                }
+                return slow;
+            }
+        }
+    }     
+}
