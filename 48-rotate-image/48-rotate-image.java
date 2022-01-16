01bc84a921ffcc20f4977f9d10392b87fdaecfaf
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-01-15 18:56:52 -0800

    Time: 0 ms (100.00%), Space: 39.9 MB (5.25%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,24 +1,32 @@
 class Solution {
-    // public void rotate(int[][] matrix) {
-    //     //推导：new -> old  {I = n - 1 - j    J = i} i和j耦合
-    //     int n = matrix.length;
-    //     if (n <= 1) {
-    //         return;
-    //     }
-    //     int round = n / 2;
-    //     for (int level = 0; level < round; level++) {
-    //         int left = level;
-    //         int right = n - 2 - level;
-    //         //i:当前level需要的次数
-    //         for (int i = left; i <= right; i++) {
-    //             int temp = matrix[left][i];
-    //             matrix[left][i] = matrix[n - 1 - i][left];
-    //             matrix[n - 1 - i][left] = matrix[n - 1 - left][n - 1 - i];
-    //             matrix[n - 1 - left][n - 1 - i] = matrix[i][n - 1 - left];
-    //             matrix[i][n - 1 - left] = temp;
-    //         }
-    //     }
-    // }
+    /*
+    for level
+        for each the ele in a certain edge
+            upper
+            right
+            down
+            left
+            
+            
+    */
+    public void rotate(int[][] matrix) {
+        //推导：new -> old  {I = n - 1 - j    J = i} 
+        int n = matrix.length;
+        if (n <= 1) {
+            return;
+        }
+        for (int level = 0; level < n / 2; level++) {
+            int left = level;
+            int right = n - 2 - level;
+            for (int i = left; i <= right; i++) {
+                int temp = matrix[left][i];
+                matrix[left][i] = matrix[n - 1 - i][left];
+                matrix[n - 1 - i][left] = matrix[n - 1 - left][n - 1 - i];
+                matrix[n - 1 - left][n - 1 - i] = matrix[i][n - 1 - left];
+                matrix[i][n - 1 - left] = temp;
+            }
+        }
+    }
     /*
  * clockwise rotate
  * first reverse up to down, then swap the symmetry 
@@ -26,24 +34,24 @@ class Solution {
  * 4 5 6  => 4 5 6  => 8 5 2
  * 7 8 9     1 2 3     9 6 3
 */
-    public void rotate(int[][] matrix) {
-        int start = 0;
-        int end = matrix.length - 1;
-        while (start < end) {
-            int[] temp = matrix[start];
-            matrix[start] = matrix[end];
-            matrix[end] = temp;
-            start++;
-            end--;
-        }
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = i + 1; j < matrix[0].length; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
-            }
-        }
-    }
+    // public void rotate(int[][] matrix) {
+    //     int start = 0;
+    //     int end = matrix.length - 1;
+    //     while (start < end) {
+    //         int[] temp = matrix[start];
+    //         matrix[start] = matrix[end];
+    //         matrix[end] = temp;
+    //         start++;
+    //         end--;
+    //     }
+    //     for (int i = 0; i < matrix.length; i++) {
+    //         for (int j = i + 1; j < matrix[0].length; j++) {
+    //             int temp = matrix[i][j];
+    //             matrix[i][j] = matrix[j][i];
+    //             matrix[j][i] = temp;
+    //         }
+    //     }
+    // }
     /*
  * anticlockwise rotate
  * first reverse left to right, then swap the symmetry
