a6c990157cc53a12bf344c51d69923fd144d7c29
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-01-15 21:14:02 -0800

    Time: 19 ms (59.51%), Space: 42.4 MB (42.03%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -1,13 +1,18 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int res = 0;
+        //similiar to 2SUM, prefixSum
+        //------------
+        //     p1  T   p2
+        //p2 - p1 = target,找在map里面是否存在p2 - target
+        //map： key ：prefixSum, value:有几个值是这样的prefixSum
         Map<Integer, Integer> map = new HashMap<>();
+        int prefixSum = 0;
         map.put(0, 1);
-        int sum = 0;
+        int res = 0;
         for (int num : nums) {
-            sum += num;
-            res += map.getOrDefault(sum - k, 0);
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
+            prefixSum = prefixSum + num;
+            res += map.getOrDefault(prefixSum - k, 0);
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
         }
         return res;
     }
