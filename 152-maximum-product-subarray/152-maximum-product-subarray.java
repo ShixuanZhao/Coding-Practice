e36b4d38d36c84445151ca2066646683f6cbc3bf
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-01-16 00:49:12 -0800

    Time: 5 ms (11.95%), Space: 41.9 MB (9.49%) - LeetHub

---
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        //母题：max subarray sum
+         if(nums.length == 0)
+            return 0;
+        int ans = nums[0];
+        //两个mDP分别定义为以i结尾的子数组的最大积与最小积；
+        int[] maxDP = new int[nums.length];
+        int[] minDP = new int[nums.length];
+        //初始化DP；
+        maxDP[0] = nums[0]; minDP[0] = nums[0];
+
+        for(int i = 1; i < nums.length; i++){
+            //最大积的可能情况有：元素i自己本身，上一个最大积与i元素累乘，上一个最小积与i元素累乘；
+            //与i元素自己进行比较是为了处理i元素之前全都是0的情况；
+           if (nums[i] > 0) {
+               maxDP[i] = Math.max(nums[i], nums[i] * maxDP[i - 1]);
+               minDP[i] = Math.min(nums[i], nums[i] * minDP[i - 1]);
+           } else {
+               maxDP[i] = Math.max(nums[i], nums[i] * minDP[i - 1]);
+               minDP[i] = Math.min(nums[i], nums[i] * maxDP[i - 1]);
+           }
+            //记录ans；
+            ans = Math.max(ans, maxDP[i]);
+        }
+        return ans;
+    
+    }
+}
