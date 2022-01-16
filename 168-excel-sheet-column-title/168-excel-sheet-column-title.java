37dccd02ba855f424203487cc43ea2e243bb1e5f
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-01-16 15:51:03 -0800

    Time: 0 ms (100.00%), Space: 38.3 MB (15.25%) - LeetHub

--- 168-excel-sheet-column-title/168-excel-sheet-column-title.java
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.java
@@ -4,6 +4,7 @@ class Solution {
         while (columnNumber > 0) {
             //!!!!!!!
             //because it is 26-nary problem, but we start from 1
+            //就像提取整数的每一位一样，从右往左
             columnNumber--;
             sb.append((char)(columnNumber % 26 + 'A'));
             columnNumber /= 26;
