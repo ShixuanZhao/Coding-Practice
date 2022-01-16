91bbc6c3107b5affd2efec66027e60daf3787834
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-01-15 20:54:03 -0800

    Time: 277 ms (23.78%), Space: 114 MB (29.11%) - LeetHub

---
+++ 752-open-the-lock/752-open-the-lock.java
@@ -0,0 +1,67 @@
+class Solution {
+    // 将 s[j] 向上拨动一次
+String plusOne(String s, int j) {
+    char[] ch = s.toCharArray();
+    if (ch[j] == '9')
+        ch[j] = '0';
+    else
+        ch[j] += 1;
+    return new String(ch);
+}
+// 将 s[i] 向下拨动一次
+String minusOne(String s, int j) {
+    char[] ch = s.toCharArray();
+    if (ch[j] == '0')
+        ch[j] = '9';
+    else
+        ch[j] -= 1;
+    return new String(ch);
+}
+    public int openLock(String[] deadends, String target) {
+//        比如说从 "0000" 开始，转一次，可以穷举出 "1000", "9000", "0100", "0900"... 共 8 种密码。然后，再以这 8 种密码作为基础，对每个密码再转一下，穷举出所有可能…
+
+// 仔细想想，这就可以抽象成一幅图，每个节点有 8 个相邻的节点，又让你求最短距离，这不就是典型的 BFS 嘛 
+        // 记录需要跳过的死亡密码
+    Set<String> deads = new HashSet<>();
+    for (String s : deadends) deads.add(s);
+    // 记录已经穷举过的密码，防止走回头路
+    Set<String> visited = new HashSet<>();
+    Queue<String> q = new LinkedList<>();
+    // 从起点开始启动广度优先搜索
+    int step = 0;
+    q.offer("0000");
+    visited.add("0000");
+    
+    while (!q.isEmpty()) {
+        int sz = q.size();
+        /* 将当前队列中的所有节点向周围扩散 */
+        for (int i = 0; i < sz; i++) {
+            String cur = q.poll();
+            
+            /* 判断是否到达终点 */
+            if (deads.contains(cur))
+                continue;
+            if (cur.equals(target))
+                return step;
+            
+            /* 将一个节点的未遍历相邻节点加入队列 */
+            for (int j = 0; j < 4; j++) {
+                String up = plusOne(cur, j);
+                if (!visited.contains(up)) {
+                    q.offer(up);
+                    visited.add(up);
+                }
+                String down = minusOne(cur, j);
+                if (!visited.contains(down)) {
+                    q.offer(down);
+                    visited.add(down);
+                }
+            }
+        }
+        /* 在这里增加步数 */
+        step++;
+    }
+    // 如果穷举完都没找到目标密码，那就是找不到了
+    return -1;
+    }
+}
