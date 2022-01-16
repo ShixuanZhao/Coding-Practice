895f6886be16be47c77491c8d900358f740771ac
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-01-15 18:38:28 -0800

    Time: 0 ms (100.00%), Space: 39.7 MB (8.68%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,6 +1,6 @@
 class Solution {
     // public void rotate(int[][] matrix) {
-    //     //推导：new -> old  {i = n - 1 - j    j = i} i和j耦合
+    //     //推导：new -> old  {I = n - 1 - j    J = i} i和j耦合
     //     int n = matrix.length;
     //     if (n <= 1) {
     //         return;
@@ -44,4 +44,11 @@ class Solution {
             }
         }
     }
+    /*
+ * anticlockwise rotate
+ * first reverse left to right, then swap the symmetry
+ * 1 2 3     3 2 1     3 6 9
+ * 4 5 6  => 6 5 4  => 2 5 8
+ * 7 8 9     9 8 7     1 4 7
+*/
 }
