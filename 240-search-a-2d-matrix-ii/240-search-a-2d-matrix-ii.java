be17120417e85e5cde89921b0ffa689f786acc81
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-01-15 21:15:46 -0800

    Time: 4 ms (100.00%), Space: 45.2 MB (41.84%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        //start from upper-right corner, it is a BST,
+        //so it is search a target in BST
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int i = 0;
+        int j = n - 1;
+        while (i < m && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return false;
+    }
+}
