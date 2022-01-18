567e63a74639b1151561063e4a88b8267bbed1ca
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-01-17 17:11:45 -0800

    Time: 0 ms (100.00%), Space: 38.7 MB (21.14%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.java
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.java
@@ -3,6 +3,7 @@ class Solution {
         int[][] res = new int[n][n];
         int left = 0;
         int right = n - 1;
+        //Obviously, you could merge colStart and colEnd into rowStart and rowEnd because it is a square matrix. But this is easily extensible to matrices that are m*n.
         int up = 0;
         int down = n - 1;
         int x = 1;
