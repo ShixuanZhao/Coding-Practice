42f4da6601bb7ba3d946166767a1f2d39a8a2ebf
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-01-18 21:32:57 -0800

    Time: 6 ms (100.00%), Space: 43.8 MB (52.61%) - LeetHub

--- 303-range-sum-query-immutable/303-range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/303-range-sum-query-immutable.java
@@ -7,10 +7,11 @@ class NumArray {
         for (int i = 1; i < nums.length; i++) {
             prefixSum[i] = prefixSum[i - 1] + nums[i];
         }
-        num = new int[nums.length];
-       for (int i = 0; i < nums.length; i++) {
-           num[i] = nums[i];
-       }
+       //  num = new int[nums.length];
+       // for (int i = 0; i < nums.length; i++) {
+       //     num[i] = nums[i];
+       // }
+        num = nums;
     }
     
     public int sumRange(int left, int right) {
