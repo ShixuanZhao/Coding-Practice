aad66b87991dc04facd49b92f23b5f0515b0854c
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-01-19 00:48:34 -0800

    Time: 6 ms (93.41%), Space: 45.7 MB (36.32%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -22,21 +22,32 @@ class Solution {
         // return sb.toString();
         
         //use two pointer to replace stack, the slow pointer point to the top ele in the stack,[0, slow] is what we want to return
+        //this is wrong because we only need to remove two duplicated num other than all
+        // int slow = -1;
+        // int fast = 0;
+        // char[] arr = s.toCharArray();
+        // while (fast < s.length()) {
+        //     if (slow == -1 || arr[slow] != arr[fast]) {
+        //         arr[++slow] = arr[fast++];
+        //     } else {
+        //         slow--;
+        //         fast++;
+        //         //OR skip all duplicate
+        //         //"aaaaaa" we should return "a" rather than ""
+        //         // int temp = arr[fast];
+        //         while (fast < arr.length && arr[fast] == temp) {
+        //             fast++;
+        //         }
+        //     }
+        // }
+        // return new String(arr, 0, slow + 1);
+        char[] arr = s.toCharArray();
         int slow = -1;
         int fast = 0;
-        char[] arr = s.toCharArray();
-        while (fast < s.length()) {
-            if (slow == -1 || arr[slow] != arr[fast]) {
-                arr[++slow] = arr[fast++];
-            } else {
-                slow--;
-                fast++;
-                //OR skip all duplicate
-                //"aaaaaa" we should return "a" rather than ""
-                // int temp = arr[fast];
-                // while (fast < arr.length && arr[fast] == temp) {
-                //     fast++;
-                // }
+        while (fast < arr.length) {
+            arr[++slow] = arr[fast++];
+            if (slow - 1 >= 0 && arr[slow] == arr[slow - 1]) {
+                slow -= 2;
             }
         }
         return new String(arr, 0, slow + 1);
