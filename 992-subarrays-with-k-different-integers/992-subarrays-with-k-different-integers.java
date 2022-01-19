9e299f5fe3359ec82ef711f2ec8bfedbd79a2a2e
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-01-19 02:12:40 -0800

    Time: 49 ms (50.23%), Space: 43.5 MB (61.02%) - LeetHub

--- 992-subarrays-with-k-different-integers/992-subarrays-with-k-different-integers.java
+++ 992-subarrays-with-k-different-integers/992-subarrays-with-k-different-integers.java
@@ -3,6 +3,17 @@ class Solution {
         return atMostK(nums, k) - atMostK(nums, k - 1);
     }
     
+    /*
+    why j - i + 1?
+    suppose initial window [a] then subarrays that ends with this element are [a]--> 1
+now we expand our window [a,b] then subarrays that ends with this new element are [b], [a,b] -->2
+now we expand our window [a,b,c] then subarrays that ends with this new element are [c], [b, c], [a,b,c] -->3
+now we expand our window [a,b,c,d] and let suppose this is not valid window so we compress window from left side to make it valid window
+[b,c,d] then subarrays that ends with this new element are [d], [c,d], [b,c,d] -->3
+
+You can observe that we are only considering subarrays with new element in it which auto. eliminate the counting of duplicate subarrays that we already considered previously.
+And surprisingly the number of sub-arrays with this new element in it is equal to the length of current window.
+    */
    private int atMostK(int[] A, int K) {
         int i = 0, res = 0;
         Map<Integer, Integer> count = new HashMap<>();
