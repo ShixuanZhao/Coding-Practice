a506bf933044609366c76f94fc07524d947a8f3d
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-01-19 19:09:02 -0800

    Time: 0 ms (100.00%), Space: 55.1 MB (15.33%) - LeetHub

---
+++ 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int missingElement(int[] nums, int k) {
+        int left = 0;
+        int right = nums.length - 1;
+        //from left ro right, the number of missing ele
+        int missing = nums[right] - nums[left] - (right - left);
+        if (k > missing) {
+            return nums[right] + k - missing;
+        }
+        while (left < right - 1) {
+            int mid = left + (right - left) / 2;
+            missing = nums[mid] - nums[left] - (mid - left);
+            if (missing < k) {
+                left = mid;
+                k -= missing;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left] + k;
+    }
+}
