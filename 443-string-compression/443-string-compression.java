87d7b42b3cbcd8c999ed362a323d4952dcfdae3c
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-01-24 12:39:17 -0800

    Time: 2 ms (56.07%), Space: 40.8 MB (39.87%) - LeetHub

---
+++ 443-string-compression/443-string-compression.java
@@ -0,0 +1,22 @@
+class Solution {
+    //[0,left) is what we want to return
+    public int compress(char[] chars) {
+        int slow = 0;
+        int fast = 0;
+        while (fast < chars.length) {
+            char temp = chars[fast];
+            int cnt = 0;
+            while (fast < chars.length && chars[fast] == temp) {
+                fast++;
+                cnt++;
+            }
+            chars[slow++] = temp;
+            if (cnt != 1) {
+                for (char c : String.valueOf(cnt).toCharArray()) {
+                    chars[slow++] = c;
+                }
+            }
+        }
+        return slow;
+    }
+}
