99c42b84c17acbaa3a7d772946286a2afc418546
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-01-25 01:06:42 -0800

    Time: 0 ms (100.00%), Space: 36.6 MB (76.38%) - LeetHub

--- 993-cousins-in-binary-tree/993-cousins-in-binary-tree.java
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.java
@@ -24,7 +24,7 @@ class Solution {
     //return is the level of x or y
     private int isCousins(TreeNode root, int x, int y, boolean[] flag, int level) {
         if (root == null) {
-            return -6;
+            return 0;
         }
         //if find x or y return its level
         if (root.val == x || root.val == y) {
