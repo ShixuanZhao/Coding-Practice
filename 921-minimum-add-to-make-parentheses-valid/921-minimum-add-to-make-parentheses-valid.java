fa5454abf0284970bfc3234cf93d8e7a53792ee3
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-01-25 17:32:42 -0800

    Time: 1 ms (67.59%), Space: 38.6 MB (40.08%) - LeetHub

--- 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.java
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.java
@@ -5,16 +5,20 @@ class Solution {
     left:1   right:0
     */
     public int minAddToMakeValid(String s) {
+        //rolling state 平衡的思路，遇到左括号状态+1遇到右括号状态-1
+        //left:需要弥补的左括号的数量， right需要的右括号
         int left = 0;
         int right = 0;
-        for (char c : s.toCharArray()) {
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
             if (c == '(') {
+                //需要一个右括号来弥补
                 right++;
             } else {
                 if (right > 0) {
-                    right--;
+                    right--; //找到一个匹配的pair，还债
                 } else {
-                    left++;
+                    left++; //右括号太多了，需要左括号还弥补
                 }
             }
         }
