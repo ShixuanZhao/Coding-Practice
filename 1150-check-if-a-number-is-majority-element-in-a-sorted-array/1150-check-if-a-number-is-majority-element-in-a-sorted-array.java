14d98c26a6a79af66780c7a2b6cc219bf8aacc20
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-01-28 14:18:32 -0800

    Time: 0 ms (100.00%), Space: 40.4 MB (5.70%) - LeetHub

---
+++ 1150-check-if-a-number-is-majority-element-in-a-sorted-array/1150-check-if-a-number-is-majority-element-in-a-sorted-array.java
@@ -0,0 +1,51 @@
+class Solution {
+    public boolean isMajorityElement(int[] nums, int target) {
+        int n = nums.length;
+        int first = search1(nums, target);
+        int last = search2(nums, target);
+        if (first == -1 || last == -1) {
+            return false;
+        }
+        return last - first + 1 > n / 2;
+    }
+	
+    private int search1(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right - 1) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        if (nums[left] == target) {
+            return left;
+        } else if (nums[right] == target) {
+            return right;
+        } else {
+            return -1;
+        }
+    }
+    
+    private int search2(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right - 1) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        if (nums[right] == target) {
+            return right;
+        } else if (nums[left] == target) {
+            return left;
+        } else {
+            return -1;
+        }
+    }
+}
