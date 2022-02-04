eb0b889a3df67f057afd11c35325d60ee19a61d5
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-02-04 00:12:53 -0800

    Time: 13 ms (52.29%), Space: 57.1 MB (13.91%) - LeetHub

---
+++ 286-walls-and-gates/286-walls-and-gates.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void wallsAndGates(int[][] rooms) {
+        int m = rooms.length;
+        int n = m == 0 ? 0 : rooms[0].length;
+        int[][] dirs = {{-1,0}, {0,1}, {0,-1}, {1,0}};
+        Queue<int[]> queue = new LinkedList<>();
+        // add all gates to the queue
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (rooms[i][j] == 0) {
+                    queue.offer(new int[] {i,j});
+                }
+            }
+        }
+        // update distance from gates
+        while (!queue.isEmpty()) {
+            int[] curPos = queue.poll();
+            for (int[] dir: dirs) {
+                int X = curPos[0] + dir[0];
+                int Y = curPos[1] + dir[1];
+                if (X<0 || Y <0 || X >= m || Y >= n || rooms[X][Y] != Integer.MAX_VALUE) continue;
+                rooms[X][Y] = rooms[curPos[0]][curPos[1]]+1;
+                queue.offer(new int[] {X, Y});
+            }
+        }
+    }
+}
