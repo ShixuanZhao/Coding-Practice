1bb933f49788a710860ef849a91982f8d84ce6d4
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-02-05 04:58:46 -0800

    Time: 25 ms (68.01%), Space: 42.9 MB (54.85%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.java
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -1,27 +1,32 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-    int m=s.length(), n=p.length();
-	boolean[][] dp = new boolean[m+1][n+1];
-	dp[0][0] = true;
-	for (int i=1; i<=m; i++) {
-		dp[i][0] = false;
-	}
-	
-	for(int j=1; j<=n; j++) {
-		if(p.charAt(j-1)=='*'){
-			dp[0][j] = dp[0][j - 1];
+        int m = s.length();
+        int n = p.length();
+	    boolean[][] M = new boolean[m + 1][n + 1];
+	    M[0][0] = true;
+        for (int i = 1; i <= m; i++) {
+            M[i][0] = false;
         }
-	}
-	
-	for(int i=1; i<=m; i++) {
-		for(int j=1; j<=n; j++) {
-			if (p.charAt(j-1)!='*') {
-				dp[i][j] = dp[i-1][j-1] && (s.charAt(i-1)==p.charAt(j-1) || p.charAt(j-1)=='?');
-			} else {
-				dp[i][j] = dp[i-1][j] || dp[i][j-1];
-			}
-		}
-	}
-	return dp[m][n];
+        for (int j = 1; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                //match 0 letter
+                M[0][j] = M[0][j - 1];
+            }
+            //other case would be false
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == '*') {
+                    //case1:match 0 letter
+                    //case2:match 1 or more letter
+                    M[i][j] = M[i][j - 1] || M[i - 1][j];
+                } else if (p.charAt(j - 1) == '?' || p.charAt(j - 1) == s.charAt(i - 1)) {
+                    M[i][j] = M[i - 1][j - 1];
+                } else {
+                    M[i][j] = false;
+                }
+            }
+        }
+        return M[m][n];
     }
 }
