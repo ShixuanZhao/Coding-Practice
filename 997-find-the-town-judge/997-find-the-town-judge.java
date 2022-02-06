652a8bc86dd9de3b8a467a71d84a09d0489898c4
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-02-06 14:23:56 -0800

    Time: 3 ms (91.95%), Space: 68 MB (73.26%) - LeetHub

---
+++ 997-find-the-town-judge/997-find-the-town-judge.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findJudge(int N, int[][] trust) {
+        //becasue we label 1 to n
+        int[] Trusted = new int[N+1];
+        for(int[] person : trust){
+            Trusted[person[0]]--;
+            Trusted[person[1]]++;
+        }
+        for(int i = 1;i < Trusted.length;i++){
+            if(Trusted[i] == N-1) return i;
+        }
+        return -1;
+    }
+}
