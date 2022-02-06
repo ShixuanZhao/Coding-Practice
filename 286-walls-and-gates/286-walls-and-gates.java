625551a2ac865e6bdc60add9482c1a6f179b78e2
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-02-05 17:47:15 -0800

    Time: 11 ms (59.14%), Space: 57.1 MB (15.73%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.java
+++ 286-walls-and-gates/286-walls-and-gates.java
@@ -18,7 +18,11 @@ class Solution {
             for (int[] dir: dirs) {
                 int X = curPos[0] + dir[0];
                 int Y = curPos[1] + dir[1];
-                if (X<0 || Y <0 || X >= m || Y >= n || rooms[X][Y] != Integer.MAX_VALUE) continue;
+                //rooms[X][Y] != Integer.MAX_VALUE
+                //means we have visited the empty room
+                //bfs will ensure the shortest distance when we first time visit
+                //a empty room
+                if (X<0 || Y <0 || X >= m || Y >= n || rooms[X][Y] != Integer.MAX_VALUE || rooms[X][Y] == -1) continue;
                 rooms[X][Y] = rooms[curPos[0]][curPos[1]]+1;
                 queue.offer(new int[] {X, Y});
             }
