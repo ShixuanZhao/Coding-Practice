342ab3ac345d968b0a02f88a5b7fba5993a2a98b
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-02-06 13:22:27 -0800

    Time: 8 ms (58.86%), Space: 47.2 MB (8.61%) - LeetHub

---
+++ 723-candy-crush/723-candy-crush.java
@@ -0,0 +1,69 @@
+class Solution {
+    public int[][] candyCrush(int[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        boolean stable  = true;
+        //delete horizontally
+        for (int i = 0; i < m; i++) {
+            int j = 0;
+            while (j + 2 < n) {
+                int target = Math.abs(board[i][j]);
+                if (board[i][j] != 0 && Math.abs(board[i][j + 1]) == target && Math.abs(board[i][j + 2]) == target) {
+                    stable = false;
+                    int k = j;
+                    while (k < n && Math.abs(board[i][k]) == target) {
+                        board[i][k] = -target;
+                        k++;
+                    }
+                    j = k;
+                } else {
+                    j++;
+                }
+            }
+        }
+        //delete vertically
+        for (int j = 0; j < n; j++) {
+            int i = 0;
+            while (i + 2 < m) {
+                int target = Math.abs(board[i][j]);
+                if (board[i][j] != 0 && Math.abs(board[i + 1][j]) == target && Math.abs(board[i + 2][j]) == target) {
+                    stable = false;
+                    int k = i;
+                    while (k < m && Math.abs(board[k][j]) == target) {
+                        board[k][j] = -target;
+                        k++;
+                    }
+                    i = k;
+                } else {
+                    i++;
+                }
+            }
+        }
+        //falling
+        //botton:1 2 -3 -3 -3 1 0 0 -> 1 2 1 0 0 0 0 0
+        //botton:1 2  1 -3 -3 1 0 0 -> 1 2 1 0 0 0 0 0
+        //                      i
+        //              j
+        if (!stable) {
+            for (int j = 0; j < n; j++) {
+                //i aim to find the first 0
+                int i = m - 1;
+                //below pointer p is all the settled down ele
+                int p = m - 1;
+                //two pointers
+                while (i >= 0 && board[i][j] != 0) {
+                    if (board[i][j] > 0) {
+                        board[p][j] = board[i][j];
+                        p--;
+                    }
+                    i--;
+                }
+                while (p >= 0) {
+                    board[p][j] = 0;
+                    p--;
+                }
+            }
+        }
+        return stable ? board : candyCrush(board);
+    }
+}
