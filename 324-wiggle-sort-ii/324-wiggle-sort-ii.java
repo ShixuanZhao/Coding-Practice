4144a4c40907e4e97337bf5bd64184e3ae4d3c30
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-02-13 19:42:47 -0800

    Time: 3 ms (86.55%), Space: 45.9 MB (61.13%) - LeetHub

---
+++ 324-wiggle-sort-ii/324-wiggle-sort-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void wiggleSort(int[] nums) {
+        Arrays.sort(nums);//可以自己写快速排序.
+        int[]array=new int[nums.length];
+        int k=nums.length-1;
+        //index is odd, is peak
+        for(int i=1;i<nums.length;i+=2)
+        {
+            array[i]=nums[k--];
+        }
+        //index is even, is botton
+        for(int i=0;i<nums.length;i+=2)
+        {
+            array[i]=nums[k--];
+        }
+
+        // nums = Arrays.copyOf(array, array.length);
+        for (int i = 0; i < array.length; i++) {
+            nums[i] = array[i];
+        }
+    }
+}
