74ba91d04508b3b8c9d7f9a3e1ef8a5a027c56e7
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-02-13 19:43:52 -0800

    Time: 4 ms (25.00%), Space: 49.8 MB (7.04%) - LeetHub

--- 280-wiggle-sort/280-wiggle-sort.java
+++ 280-wiggle-sort/280-wiggle-sort.java
@@ -1,15 +1,37 @@
 class Solution {
-    public void wiggleSort(int[] nums) {
-        for(int i=0;i<nums.length;i++)
-            if(i%2==1){
-               if(nums[i-1]>nums[i]) swap(nums, i);
-            }else if(i!=0 && nums[i-1]<nums[i]) swap(nums, i);
-    }
+//     public void wiggleSort(int[] nums) {
+//         for(int i=0;i<nums.length;i++)
+//             if(i%2==1){
+//                if(nums[i-1]>nums[i]) swap(nums, i);
+//             }else if(i!=0 && nums[i-1]<nums[i]) swap(nums, i);
+//     }
     
-    //swap with prev ele
-    public void swap(int[] nums, int i){
-          int tmp=nums[i];
-          nums[i]=nums[i-1];
-          nums[i-1]=tmp;
+//     //swap with prev ele
+//     public void swap(int[] nums, int i){
+//           int tmp=nums[i];
+//           nums[i]=nums[i-1];
+//           nums[i-1]=tmp;
+//     }
+    
+    public void wiggleSort(int[] nums) {
+        Arrays.sort(nums);//可以自己写快速排序.
+        //not in place
+        int[]array=new int[nums.length];
+        int k=nums.length-1;
+        //index is odd, is peak
+        for(int i=1;i<nums.length;i+=2)
+        {
+            array[i]=nums[k--];
+        }
+        //index is even, is botton
+        for(int i=0;i<nums.length;i+=2)
+        {
+            array[i]=nums[k--];
+        }
+
+        // nums = Arrays.copyOf(array, array.length);
+        for (int i = 0; i < array.length; i++) {
+            nums[i] = array[i];
+        }
     }
 }
