d5830d1c7eb088534dba7afabc817a409ab8c2cf
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-02-15 21:22:59 -0800

    Time: 0 ms (100.00%), Space: 41.9 MB (18.90%) - LeetHub

---
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.java
@@ -0,0 +1,57 @@
+class Solution {
+    /*
+        dfs:choose 3 choices that '*' can choose
+        dfs(String s, int begin, int cnt)
+        use cnt to record the state
+        if we meet (, cnt++
+        if we meet ), cnt--
+        if we meed *, try 3 possibility
+        if at last, judge whether cnt == 0
+        T = O(3^n)
+    */
+    //TLE
+//     public boolean checkValidString(String s) {
+//         return dfs(s, 0, 0);
+//     }
+    
+//     private boolean dfs(String s, int begin, int cnt) {
+//         if (cnt < 0) {
+//             return false;
+//         }
+//         for (int i = begin; i < s.length(); i++) {
+//             if (s.charAt(i) == '(') {
+//                 cnt++;
+//             } else if (s.charAt(i) == ')') {
+//                 cnt--;
+//                 if (cnt < 0) {
+//                     return false;
+//                 }
+//             } else if (s.charAt(i) == '*') {
+//                 return dfs(s, i + 1, cnt + 1) || dfs(s, i + 1, cnt - 1) || dfs(s, i + 1, cnt);
+//             }
+//         }
+//         return cnt == 0;
+//     }
+    
+    public boolean checkValidString(String s) {
+        int cmin = 0, cmax = 0; // open parentheses count in range [cmin, cmax]
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                cmax++;
+                cmin++;
+            } else if (c == ')') {
+                cmax--;
+                cmin--;
+            } else if (c == '*') {
+                cmax++; // if `*` become `(` then openCount++
+                cmin--; // if `*` become `)` then openCount--
+                // if `*` become `` then nothing happens
+                // So openCount will be in new range [cmin-1, cmax+1]
+            }
+            if (cmax < 0) return false; // Currently, don't have enough open parentheses to match close parentheses-> Invalid
+                                        // For example: ())(
+            cmin = Math.max(cmin, 0);   // It's invalid if open parentheses count < 0 that's why cmin can't be negative
+        }
+        return cmin == 0; // Return true if can found `openCount == 0` in range [cmin, cmax]
+    }
+}
