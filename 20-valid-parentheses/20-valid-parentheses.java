6dd6395c871ffefb7b0f42fb0367e9bfb1be04bb
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-02-15 20:46:59 -0800

    Time: 1 ms (99.34%), Space: 40 MB (31.87%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -28,4 +28,34 @@ class Solution {
         //find whether all the open paranthes has been matched
         return stack.isEmpty();
     }
+    
+    /*
+    WRONG!
+    Open brackets must be closed by the same type of brackets.
+    "([)]" can not pass
+    */
+    // public boolean isValid(String s) {
+    //     int cnt1 = 0;
+    //     int cnt2 = 0;
+    //     int cnt3 = 0;
+    //     for (char c : s.toCharArray()) {
+    //         if (c == '(') {
+    //             cnt1++;
+    //         } else if (c == ')') {
+    //             cnt1--;
+    //         } else if (c == '[') {
+    //             cnt2++;
+    //         } else if (c == ']') {
+    //             cnt2--;
+    //         } else if (c == '{') {
+    //             cnt3++;
+    //         } else if (c == '}') {
+    //             cnt3--;
+    //         }
+    //         if (cnt1 < 0 || cnt2 < 0 || cnt3 < 0) {
+    //             return false;
+    //         }
+    //     }
+    //     return cnt1 == 0 && cnt2 == 0 && cnt3 == 0;
+    // }
 }
