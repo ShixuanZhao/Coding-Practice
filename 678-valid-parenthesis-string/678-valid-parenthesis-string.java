1ae32738ce185e9ce239c197f9edd73883658dfc
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-02-15 22:30:12 -0800

    Time: 0 ms (100.00%), Space: 42 MB (16.23%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.java
@@ -34,24 +34,25 @@ class Solution {
 //     }
     
     public boolean checkValidString(String s) {
-        int cmin = 0, cmax = 0; // open parentheses count in range [cmin, cmax]
+        //num of open '('
+        int min = 0;
+        int max = 0;
         for (char c : s.toCharArray()) {
             if (c == '(') {
-                cmax++;
-                cmin++;
+                min++;
+                max++;
             } else if (c == ')') {
-                cmax--;
-                cmin--;
-            } else if (c == '*') {
-                cmax++; // if `*` become `(` then openCount++
-                cmin--; // if `*` become `)` then openCount--
-                // if `*` become `` then nothing happens
-                // So openCount will be in new range [cmin-1, cmax+1]
+                min--;
+                max--;
+            } else {
+                max++;
+                min--;
             }
-            if (cmax < 0) return false; // Currently, don't have enough open parentheses to match close parentheses-> Invalid
-                                        // For example: ())(
-            cmin = Math.max(cmin, 0);   // It's invalid if open parentheses count < 0 that's why cmin can't be negative
+            if (max < 0) {
+                return false;
+            }
+            min = Math.max(min, 0);
         }
-        return cmin == 0; // Return true if can found `openCount == 0` in range [cmin, cmax]
+        return min == 0;
     }
 }
