f4ec97f69de41c73c2286fee7bb98924089c4b6a
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-02-19 18:16:06 -0800

    Time: 5 ms (91.88%), Space: 54.2 MB (10.92%) - LeetHub

--- 254-factor-combinations/254-factor-combinations.java
+++ 254-factor-combinations/254-factor-combinations.java
@@ -7,12 +7,15 @@ class Solution {
     }
     
     private void dfs(int n, int begin, List<List<Integer>> res, List<Integer> cur) {
+        //make choice. in order to avoid duplication, we impose the start point
+        //we need to begin variable
         for (int i = begin; i * i <= n; i++) {
             if (n % i == 0) {
                 cur.add(i);
                 cur.add(n / i);
                 res.add(new ArrayList<>(cur));
                 cur.remove(cur.size() - 1);
+                //i because we can use duplicate number
                 dfs(n / i, i, res, cur);
                 cur.remove(cur.size() - 1);
             }
