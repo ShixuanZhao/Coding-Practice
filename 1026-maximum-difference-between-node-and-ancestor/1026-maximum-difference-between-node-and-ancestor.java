5a1d234f6365551d805c565c876d040821b3a247
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-02-21 23:10:44 -0800

    Time: 1 ms (65.54%), Space: 42.6 MB (5.51%) - LeetHub

---
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    //preOrder直上直下的路径不带返回值的写法
+//     public int maxAncestorDiff(TreeNode root) {
+//         //在所有直上直下的路径上找到maxDiff
+//         //当前层update，max和min
+//         //base case的时候更新maxDiff
+//         int[] maxDiff = new int[1];
+//         maxAncestorDiff(root, maxDiff, Integer.MAX_VALUE, Integer.MIN_VALUE);
+//         return maxDiff[0];
+//     }
+    
+//     //dfs
+//     private void maxAncestorDiff(TreeNode root, int[] maxDiff, int min, int max) {
+//         if (root == null) {
+//             maxDiff[0] = Math.max(max - min, maxDiff[0]);
+//             return;
+//         }
+//         min = Math.min(min, root.val);
+//         max = Math.max(max, root.val);
+//         maxAncestorDiff(root.left, maxDiff, min, max);
+//         maxAncestorDiff(root.right, maxDiff, min, max);
+//     }
+    
+    //三部曲的写法
+    public int maxAncestorDiff(TreeNode root) {
+        //三部曲
+        return maxAncestorDiff(root, Integer.MAX_VALUE, Integer.MIN_VALUE);
+    }
+
+    private int maxAncestorDiff(TreeNode root, int min, int max) {
+        if (root == null) {
+            return max - min;
+        }
+        min = Math.min(min, root.val);
+        max = Math.max(max, root.val);
+        int left = maxAncestorDiff(root.left, min, max);
+        int right = maxAncestorDiff(root.right, min, max);
+        return Math.max(left, right);
+    }
+}
