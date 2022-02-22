c9448c4e0da8056ea217c7103078a641bc41288a
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-02-22 15:09:39 -0800

    Time: 0 ms (100.00%), Space: 40.9 MB (32.74%) - LeetHub

--- 168-excel-sheet-column-title/168-excel-sheet-column-title.java
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.java
@@ -3,6 +3,9 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         //26进制余数0-25转成'A'-> 0, 这样不会出现余数是26的情况
         for (int i = columnNumber; i != 0; i /= 26) {
+            //!!!!!!!
+            //because it is 26-nary problem, but we start from 1
+            //就像提取整数的每一位一样，从右往左
             i--;
             int cur = i % 26;
             sb.append((char)(cur + 'A'));
