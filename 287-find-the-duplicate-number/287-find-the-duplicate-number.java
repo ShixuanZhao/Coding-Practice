40ecde10cb3513e889acff163265616f8c971650
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-02-22 22:15:11 -0800

    Time: 12 ms (40.87%), Space: 75.3 MB (39.97%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int findDuplicate(int[] nums) {
+    // public int findDuplicate(int[] nums) {
         //有可能重复的数字出现多次
         //M1:HashSet
         // Set<Integer> set = new HashSet<>();
@@ -24,39 +24,39 @@ class Solution {
          0 1 2 3 4
         [1,2,3,4,2]
         */
-        for (int i = 0; i < nums.length; i++) {
-            while (i != nums[i] - 1) {
-                if (nums[i] == nums[nums[i] - 1]) {
-                    return nums[i];
+//         for (int i = 0; i < nums.length; i++) {
+//             while (i != nums[i] - 1) {
+//                 if (nums[i] == nums[nums[i] - 1]) {
+//                     return nums[i];
+//                 }
+//                 swap(nums, i, nums[i] - 1);
+//             }
+//         }
+//         return -1;
+//     }
+    
+//     private void swap(int[] nums, int i, int j) {
+//         int temp = nums[i];
+//         nums[i] = nums[j];
+//         nums[j] = temp;
+//     }
+        
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (slow == fast) {
+                fast = nums[0];
+                while (slow != fast) {
+                    slow = nums[slow];
+                    fast = nums[fast];
                 }
-                swap(nums, i, nums[i] - 1);
+                return slow;
             }
         }
-        return -1;
     }
-    
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-        
-        //best solution
-        //O（n） O（1）
-        //转换成find the start of circle in linkedlist
-        // int slow = nums[0];
-        // int fast = nums[0];
-        // while (true) {
-        //     slow = nums[slow];
-        //     fast = nums[nums[fast]];
-        //     if (slow == fast) {
-        //         fast = nums[0];
-        //         while (fast != slow) {
-        //             slow = nums[slow];
-        //             fast = nums[fast];
-        //         }
-        //         return slow;
-        //     }
-        // }
+       
        
 }
