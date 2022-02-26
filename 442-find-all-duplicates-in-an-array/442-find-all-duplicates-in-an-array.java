3ba2930abab16b9aca25f78b5cb5711c2c7fe9f3
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-02-25 20:27:19 -0800

    Time: 12 ms (39.82%), Space: 65.7 MB (40.96%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.java
@@ -3,29 +3,43 @@ class Solution {
     // when find a number i, flip the number at position i-1 to negative. 
     // if the number at position i-1 is already negative, i is the number that occurs twice.
      0 1 2
-    [-1,-2,2]
+    [1,-1,2]
     res:1
        0 1 2 
       [1,2,1]
     */
     public List<Integer> findDuplicates(int[] nums) {
-      Set<Integer> set = new HashSet<>();
-      for (int i = 0; i < nums.length; i++) {
-            while (i != nums[i] - 1) {
-                //must be break not continue
-                if (nums[i] == nums[nums[i] - 1]) {
-                    set.add(nums[i]);
-                    break;
-                }
-                swap(nums, i, nums[i] - 1);
+        List<Integer> res = new ArrayList<>();
+        for (int num : nums) {
+            int index = Math.abs(num) - 1;
+            //说明之前已经有人对应过index位置上的元素了， < 0
+            if (nums[index] < 0) {
+                res.add(Math.abs(num));
             }
+            //负号标记被对应的元素
+            nums[index] = -nums[index];
         }
-        return new ArrayList<>(set);
+        return res;
     }
+    //must use set因为在一直交换的过程中会重复加入重复的数，最好的方法是不交换，把num[i] - 1对应位置的值变为负数，表示visited过了
+//     public List<Integer> findDuplicates(int[] nums) {
+//       Set<Integer> set = new HashSet<>();
+//       for (int i = 0; i < nums.length; i++) {
+//             while (i != nums[i] - 1) {
+//                 //must be break not continue
+//                 if (nums[i] == nums[nums[i] - 1]) {
+//                     set.add(nums[i]);
+//                     break;
+//                 }
+//                 swap(nums, i, nums[i] - 1);
+//             }
+//         }
+//         return new ArrayList<>(set);
+//     }
     
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
+//     private void swap(int[] nums, int i, int j) {
+//         int temp = nums[i];
+//         nums[i] = nums[j];
+//         nums[j] = temp;
+//     }
 }
