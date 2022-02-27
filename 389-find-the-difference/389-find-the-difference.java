3a8200cea9eb3d704bdfe36ef75f404ea9d6358c
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-02-27 13:28:20 -0800

    Time: 2 ms (76.79%), Space: 42.3 MB (46.30%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -1,18 +1,29 @@
 class Solution {
+    // public char findTheDifference(String s, String t) {
+    //     int[] freq = new int[26];
+    //     for (char c : s.toCharArray()) {
+    //         freq[c -'a']--;
+    //     }
+    //     for (char c : t.toCharArray()) {
+    //         freq[c - 'a']++;
+    //     }
+    //     int i = 0;
+    //     for (; i < 26; i++) {
+    //         if (freq[i] == 1) {
+    //             break;
+    //         }
+    //     }
+    //     return (char)(i + 'a');
+    // }
+    
     public char findTheDifference(String s, String t) {
-        int[] freq = new int[26];
-        for (char c : s.toCharArray()) {
-            freq[c -'a']--;
+        char c = 0;
+        for(int i = 0; i < s.length(); i++){
+            c ^= s.charAt(i);
         }
-        for (char c : t.toCharArray()) {
-            freq[c - 'a']++;
+        for(int i = 0; i < t.length(); i++){
+            c ^= t.charAt(i);
         }
-        int i = 0;
-        for (; i < 26; i++) {
-            if (freq[i] == 1) {
-                break;
-            }
-        }
-        return (char)(i + 'a');
+        return c;
     }
 }
