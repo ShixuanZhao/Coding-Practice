9867bab697638ebf84c4a63e7b3f29254ebafdfd
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-02-27 00:34:34 -0800

    Time: 10 ms (82.13%), Space: 49.1 MB (21.47%) - LeetHub

--- 303-range-sum-query-immutable/303-range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/303-range-sum-query-immutable.java
@@ -1,21 +1,35 @@
+// class NumArray {
+//     int[] prefixSum;
+//     int[] num;
+//     public NumArray(int[] nums) {
+//         prefixSum = new int[nums.length];
+//         prefixSum[0] = nums[0];
+//         for (int i = 1; i < nums.length; i++) {
+//             prefixSum[i] = prefixSum[i - 1] + nums[i];
+//         }
+//        //  num = new int[nums.length];
+//        // for (int i = 0; i < nums.length; i++) {
+//        //     num[i] = nums[i];
+//        // }
+//         num = nums;
+//     }
+    
+//     public int sumRange(int left, int right) {
+//         return prefixSum[right] - prefixSum[left] + num[left];
+//     }
+// }
+
 class NumArray {
     int[] prefixSum;
-    int[] num;
     public NumArray(int[] nums) {
-        prefixSum = new int[nums.length];
-        prefixSum[0] = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            prefixSum[i] = prefixSum[i - 1] + nums[i];
+        prefixSum = new int[nums.length + 1];
+        for (int i = 1; i <= nums.length; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
         }
-       //  num = new int[nums.length];
-       // for (int i = 0; i < nums.length; i++) {
-       //     num[i] = nums[i];
-       // }
-        num = nums;
     }
     
     public int sumRange(int left, int right) {
-        return prefixSum[right] - prefixSum[left] + num[left];
+        return prefixSum[right + 1] - prefixSum[left];
     }
 }
 
