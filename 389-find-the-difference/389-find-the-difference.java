09c3f1c49c5c2c04b20e7732321398c9e7d689e1
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-02-27 13:28:33 -0800

    Time: 1 ms (99.83%), Space: 43.1 MB (5.25%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -1,29 +1,29 @@
 class Solution {
-    // public char findTheDifference(String s, String t) {
-    //     int[] freq = new int[26];
-    //     for (char c : s.toCharArray()) {
-    //         freq[c -'a']--;
-    //     }
-    //     for (char c : t.toCharArray()) {
-    //         freq[c - 'a']++;
-    //     }
-    //     int i = 0;
-    //     for (; i < 26; i++) {
-    //         if (freq[i] == 1) {
-    //             break;
-    //         }
-    //     }
-    //     return (char)(i + 'a');
-    // }
-    
     public char findTheDifference(String s, String t) {
-        char c = 0;
-        for(int i = 0; i < s.length(); i++){
-            c ^= s.charAt(i);
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) {
+            freq[c -'a']--;
         }
-        for(int i = 0; i < t.length(); i++){
-            c ^= t.charAt(i);
+        for (char c : t.toCharArray()) {
+            freq[c - 'a']++;
         }
-        return c;
+        int i = 0;
+        for (; i < 26; i++) {
+            if (freq[i] == 1) {
+                break;
+            }
+        }
+        return (char)(i + 'a');
     }
+    
+    // public char findTheDifference(String s, String t) {
+    //     char c = 0;
+    //     for(int i = 0; i < s.length(); i++){
+    //         c ^= s.charAt(i);
+    //     }
+    //     for(int i = 0; i < t.length(); i++){
+    //         c ^= t.charAt(i);
+    //     }
+    //     return c;
+    // }
 }
