c6b17cafbc79459be823911b5119a3be81422ea4
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-03-05 17:07:24 -0800

    Time: 2 ms (71.54%), Space: 41.3 MB (36.13%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.java
+++ 7-reverse-integer/7-reverse-integer.java
@@ -1,11 +1,26 @@
 class Solution {
     //负数的除法 -123 / 10 = -12.....-3
+    // public int reverse(int x) {
+    //     long res = 0;
+    //     for (int i = x; i != 0; i /= 10) {
+    //         res = res * 10 + i % 10;
+    //     }
+    //     //(int)res != res means it is overflow return 0
+    //     return (int)res != res ? 0 : (int)res;
+    // }
+    
+    //Update: Not using long
     public int reverse(int x) {
-        long res = 0;
+        int prevRes = 0;
+        int res = 0;
         for (int i = x; i != 0; i /= 10) {
             res = res * 10 + i % 10;
+            if ((res - i % 10) / 10 != prevRes) {
+                return 0;
+            }
+            prevRes = res;
         }
-        //(int)res != res means it is overflow return 0
-        return (int)res != res ? 0 : (int)res;
+        return res;
     }
+    
 }
