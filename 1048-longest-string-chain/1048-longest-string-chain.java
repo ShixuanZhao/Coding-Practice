0f836acd3abd3741bea45520e5d6e8f29a202e21
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-03-06 23:11:15 -0800

    Time: 59 ms (60.24%), Space: 45.3 MB (69.98%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,19 +1,25 @@
 class Solution {
+    //longest inscreasing subsequence
+    //逆向思维：删除一个，从后往前从长变短的一个chain
+    //M[i]:the longest chain starting from index i(begin to delete)
+    //M[i] = MAX(M[j] + 1) words[j] can be obtained by deleting a letter from words[i]
+    //sort by length, liear scan the words have smaller length
+    //i scan from left to right, j scan from i - 1
     public int longestStrChain(String[] words) {
-    Arrays.sort(words, (a, b) -> (a.length() - b.length()));
-    int[] dp = new int[words.length];
-    int maxLen = 0;
-    for (int i = 0; i < words.length; i++) {
-        dp[i] = 1;
-        for (int j = i - 1; j >= 0 && words[i].length() - words[j].length() <= 1; j--) {
-            if (isPredecessor(words[j], words[i])) {
-                dp[i] = Math.max(dp[i], dp[j] + 1);
+        Arrays.sort(words, (a, b) -> (a.length() - b.length()));
+        int[] dp = new int[words.length];
+        int maxLen = 0;
+        for (int i = 0; i < words.length; i++) {
+            dp[i] = 1;
+            for (int j = i - 1; j >= 0 && words[i].length() - words[j].length() <= 1; j--) {
+                if (isPredecessor(words[j], words[i])) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
             }
+            maxLen = Math.max(maxLen, dp[i]);
         }
-        maxLen = Math.max(maxLen, dp[i]);
+        return maxLen;
     }
-    return maxLen;
-}
 
  private boolean isPredecessor(String s1, String s2) {
     if (s2.length() == s1.length()) return false;
