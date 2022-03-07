7bc5faa0fa3eb10b23a5bbeaf3745538b766f514
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-03-06 23:42:04 -0800

    Time: 52 ms (68.17%), Space: 45.4 MB (69.71%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -7,33 +7,44 @@ class Solution {
     //i scan from left to right, j scan from i - 1
     public int longestStrChain(String[] words) {
         Arrays.sort(words, (a, b) -> (a.length() - b.length()));
-        int[] dp = new int[words.length];
-        int maxLen = 0;
-        for (int i = 0; i < words.length; i++) {
-            dp[i] = 1;
+        int res = 1;
+        int n = words.length;
+        int[] M = new int[n];
+        for (int i = 0; i < n; i++) {
+            //initialize
+            M[i] = 1;
             for (int j = i - 1; j >= 0 && words[i].length() - words[j].length() <= 1; j--) {
                 if (isPredecessor(words[j], words[i])) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    M[i] = Math.max(M[i], M[j] + 1);
                 }
             }
-            maxLen = Math.max(maxLen, dp[i]);
+            res = Math.max(res, M[i]);
         }
-        return maxLen;
+        return res;
     }
-
- private boolean isPredecessor(String s1, String s2) {
-    if (s2.length() == s1.length()) return false;
-    int diff = 0;
-    for (int i = 0, j = 0; i < s1.length(); ) {
-        if (s1.charAt(i) == s2.charAt(j)) {
-            i++;
-            j++;
-        } else {
-            diff++;
-            if (diff > 1) return false;
-            j++;
+    
+    private boolean isPredecessor(String s1, String s2) {
+        int n1 = s1.length();
+        int n2 = s2.length();
+        if (n1 == n2) {
+            return false;
         }
+        int i = 0;
+        int j = 0;
+        //number of diff letter
+        int cnt = 0;
+        while (i < n1) {
+            if (s1.charAt(i) == s2.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                cnt++;
+                if (cnt > 1) {
+                    return false;
+                }
+                j++;
+            }
+        }
+        return true;
     }
-    return true;
-}
 }
