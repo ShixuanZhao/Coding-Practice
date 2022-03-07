65adea1df204a11c91de76601755429f77872390
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-03-06 19:51:19 -0800

    Time: 443 ms (22.44%), Space: 85.5 MB (21.65%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -26,7 +26,7 @@ class Solution {
         expand from the center and check the edge
         T = O(n^2) S = O(n^2)
     */
-    public String longestPalindrome(String s) {
+    // public String longestPalindrome(String s) {
         // if (s == null || s.length() == 0) {
         //     return "";
         // }
@@ -58,30 +58,65 @@ class Solution {
         // }
         // return s.substring(begin, begin + maxLen);
         
+//         int n = s.length();
+//         if (n == 1) {
+//             return s;
+//         }
+//         StringBuilder sb = new StringBuilder();
+//         int[] maxLen = new int[1];
+//         for (int i = 0; i < n; i++) {
+//             checkP(s, i, i, sb, maxLen);
+//             checkP(s, i, i + 1, sb, maxLen);
+//         }
+//         return sb.toString();
+//     }
+    
+//     private void checkP(String s, int left, int right, StringBuilder sb, int[] maxLen) {
+//         while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+//             left--;
+//             right++;
+//         }
+//         //(left, right) not include boundary
+//         if (maxLen[0] < right - left - 1) {
+//             maxLen[0] = right - left - 1;
+//             sb.setLength(0);
+//             sb.append(s.substring(left + 1, right));
+//         }
+    
+//     }
+    
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
         int n = s.length();
-        if (n == 1) {
-            return s;
-        }
-        StringBuilder sb = new StringBuilder();
-        int[] maxLen = new int[1];
+        boolean[][] M = new boolean[n][n];
         for (int i = 0; i < n; i++) {
-            checkP(s, i, i, sb, maxLen);
-            checkP(s, i, i + 1, sb, maxLen);
+            M[i][i] = true;
         }
-        return sb.toString();
-    }
-    
-    private void checkP(String s, int left, int right, StringBuilder sb, int[] maxLen) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
+        int begin = 0;
+        //the minimum len is 1
+        int maxLen = 1;
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+                if (s.charAt(i) == s.charAt(j)) {
+                    //guarantee at least 3 ele
+                    //only two ele
+                    if (j - i < 3) {
+                        M[i][j] = true;
+                    } else {
+                        M[i][j] = M[i + 1][j - 1];
+                    }
+                } else {
+                    M[i][j] = false;
+                }
+                if (M[i][j] && j - i + 1 > maxLen) {
+                    begin = i;
+                    maxLen = j - i + 1;
+                }
+            }
         }
-        //(left, right) not include boundary
-        if (maxLen[0] < right - left - 1) {
-            maxLen[0] = right - left - 1;
-            sb.setLength(0);
-            sb.append(s.substring(left + 1, right));
-        }
-    
+        return s.substring(begin, begin + maxLen);
     }
 }
