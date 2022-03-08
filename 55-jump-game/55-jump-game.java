8ba9ee32af311972a2181a223b92bb8309aafe35
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-03-07 19:07:19 -0800

    Time: 1205 ms (5.95%), Space: 68.9 MB (11.69%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,37 +1,43 @@
 class Solution {
     //M[i] whether can reach last position from index i
     public boolean canJump(int[] nums) {
-        //make a diff from leetcode746
+//         //make a diff from leetcode746
+//         if (nums.length == 1) {
+//             return true;
+//         }
+//         int n = nums.length;
+//         boolean[] M = new boolean[n];
+//         M[n - 1] = true;
+//         for (int i = n - 2; i >= 0; i--) {
+//             if (i + nums[i] >= n - 1) {
+//                 M[i] = true;
+//             } else {
+//                 for (int j = 1; j <= nums[i]; j++) {
+//                     if (M[i + j]) {
+//                         M[i] = true;
+//                         break;
+//                     }
+//                 }
+//             }
+//         }
+//         return M[0];
+        
+        //M2 is wrong!! we must traverse from right to left
+    //M[i] whether index i is reachable from index0
         if (nums.length == 1) {
             return true;
         }
         int n = nums.length;
         boolean[] M = new boolean[n];
-        M[n - 1] = true;
-        for (int i = n - 2; i >= 0; i--) {
-            if (i + nums[i] >= n - 1) {
-                M[i] = true;
-            } else {
-                for (int j = 1; j <= nums[i]; j++) {
-                    if (M[i + j]) {
-                        M[i] = true;
-                        break;
-                    }
+        M[0] = true;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (M[j] && j + nums[j] >= i) {
+                    M[i] = true;
+                    break;
                 }
             }
         }
-        return M[0];
-        
-        //M2 is wrong!! we must traverse from right to left
-        // M[0] = true;
-        // for (int i = 1; i < n; i++) {
-        //     for (int j = 1; j <= nums[i]; j++) {
-        //         if (i - j >= 0 && M[i - j]) {
-        //             M[i] = true;
-        //             break;
-        //         }
-        //     }
-        // }
-        // return M[n - 1];
+        return M[n - 1];
     }
 }
