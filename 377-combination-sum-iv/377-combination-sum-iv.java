d3aa929efcf17daf048a0766e074b5b7014b13d9
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-03-07 22:20:32 -0800

    Time: 2 ms (63.03%), Space: 40.6 MB (51.34%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -2,7 +2,11 @@ class Solution {
     public int combinationSum4(int[] nums, int target) {
         int[] M = new int[target + 1];
         M[0] = 1;
+        /*
+        T = 3 (1, 2)  (2, 1)
+        */
         for (int i = 1; i < M.length; i++) {
+            //j 是右小段，右小段是一个数字，这样算的话肯定会有重读其实是permutation问题
             for (int j = 0; j < nums.length; j++) {
                 if (i - nums[j] >= 0) {
                     M[i] += M[i - nums[j]];
