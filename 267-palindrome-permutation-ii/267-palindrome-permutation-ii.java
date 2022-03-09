f2ce1898f2909777f47ade94e3c2944166db88e9
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-03-08 23:47:40 -0800

    Time: 24 ms (70.63%), Space: 63.9 MB (63.59%) - LeetHub

---
+++ 267-palindrome-permutation-ii/267-palindrome-permutation-ii.java
@@ -0,0 +1,46 @@
+class Solution {
+     public List<String> generatePalindromes(String s) {
+        List<String> res = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();        
+        
+        // count all chars
+        int[] c = new int[256];
+        for(int i=0;i<s.length();i++) c[s.charAt(i)]++;
+
+        // count all odd chars        
+        for(int i=0;i<c.length;i++) {
+            if(c[i]%2!=0) sb.append((char) i);
+        }
+        
+        // if there is more then 1 odd char, return the empty list
+        if(sb.length()>1) return res; 
+
+        
+        permute(sb, res, c, s.length());
+        return res;
+    }
+    
+    
+    void permute(StringBuilder sb, List<String> res, int[] c, int len){
+        if(sb.length()==len){
+            res.add(sb.toString());
+            return;
+        }
+        
+        for(int i=0;i<c.length;i++){
+            if(c[i]>1){
+                c[i]-=2;
+                sb.insert(0, (char) i);
+                sb.append((char) i);
+                
+                permute(sb, res, c, len);
+                
+                // backtrack
+                sb.deleteCharAt(0);
+                sb.deleteCharAt(sb.length()-1);
+                c[i]+=2;                
+            }
+        }
+        
+    }
+}
