b4bb8e3a56d487836bb7ff27b82938be416e4df4
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-03-19 10:06:30 -0700

    Time: 1 ms (99.97%), Space: 42.3 MB (76.24%) - LeetHub

--- 243-shortest-word-distance/243-shortest-word-distance.java
+++ 243-shortest-word-distance/243-shortest-word-distance.java
@@ -22,20 +22,23 @@ class Solution {
     //     return res;
     // }
     
+    //not use hashmap
     public int shortestDistance(String[] words, String word1, String word2) {
-    int p1 = -1, p2 = -1, min = Integer.MAX_VALUE;
-    
-    for (int i = 0; i < words.length; i++) {
-        if (words[i].equals(word1)) 
-            p1 = i;
-
-        if (words[i].equals(word2)) 
-            p2 = i;
-            
-        if (p1 != -1 && p2 != -1)
-            min = Math.min(min, Math.abs(p1 - p2));
+        //index for word1
+        int p1 = -1;
+        int p2 = -1;
+        int res = Integer.MAX_VALUE;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word1)) {
+                p1 = i;
+            }
+            if (words[i].equals(word2)) {
+                p2 = i;
+            }
+            if (p1 != -1 && p2 != -1) {
+                res = Math.min(res, Math.abs(p1 - p2));
+            }
+        }
+        return res;
     }
-    
-    return min;
-}
 }
