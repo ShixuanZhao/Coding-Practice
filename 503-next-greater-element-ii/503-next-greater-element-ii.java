d753e3ca908b6bb1e0e8c5fe75f3df8ff6bd5d2f
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-03-20 09:45:42 -0700

    Time: 10 ms (90.54%), Space: 55.6 MB (10.27%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.java
+++ 503-next-greater-element-ii/503-next-greater-element-ii.java
@@ -3,16 +3,17 @@ class Solution {
         //想象成把原有的array copy了一遍放在后面形成一个大的array，size=2n，遍历index到2n-1，每次取模来获取元素
         //stack store the index
         //do not forget to initialize tp -1
-        
+//         Loop once, we can get the Next Greater Number of a normal array.
+// Loop twice, we can get the Next Greater Number of a circular array
         Deque<Integer> stack = new ArrayDeque<>();
-        int[] res = new int[nums.length];
-        Arrays.fill(res, -1);
         int n = nums.length;
-        for (int i = 0; i <= 2 * n - 1; i++) {
-            while (!stack.isEmpty() && nums[i % n] > nums[stack.peekFirst()]) {
+        int[] res = new int[n];
+        Arrays.fill(res, -1);
+        for (int i = 0; i < 2 * n; i++) {
+            while (!stack.isEmpty() && nums[stack.peekFirst()] < nums[i % n]) {
                 res[stack.pollFirst()] = nums[i % n];
-            }  
-                stack.offerFirst(i % n);
+            }
+            stack.offerFirst(i % n);
         }
         return res;
     }
