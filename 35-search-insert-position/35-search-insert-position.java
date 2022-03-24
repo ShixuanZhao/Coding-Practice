47c3598cceb853b5cb3bfa1a60ee795cd2184271
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-03-23 17:57:14 -0700

    Time: 0 ms (100.00%), Space: 42 MB (74.62%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.java
@@ -0,0 +1,44 @@
+class Solution {
+    /*
+    1 3 5 6 T = 5
+    l mid r
+      1 3   5     6   case1   return 2
+         l mid     r   
+         
+     1 3   4     6   T = 5  return 3
+       l   mid    r
+      1 3   4     6  A[l] < T < A[r]: return l + 1;  
+             l     r
+                         T < A[l]: return l;
+                         T > A[r]: return r + 1
+    */
+    public int searchInsert(int[] nums, int target) {
+        //nums contains distinct values sorted in ascending order.
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+        if (nums[left] == target) {
+            return left;
+        }
+        if (nums[right] == target) {
+            return right;
+        }
+        while (left < right - 1) {
+            int mid = left + (right - left) / 2;
+            if (target < nums[mid]) {
+                right = mid;
+            } else if (target == nums[mid]) {
+                return mid;
+            } else {
+                left = mid;
+            }
+        }
+        if (nums[left] < target && target < nums[right]) {
+            return right;
+        } else if (target < nums[left]) {
+            return left;
+        } else {
+            return right + 1;
+        }    
+    }
+}
