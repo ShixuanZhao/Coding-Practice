70554cdf71ba5942de1d3a342eaabc94e462f983
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-03-30 14:02:05 -0700

    Time: 1 ms (54.60%), Space: 42.3 MB (43.28%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -3,22 +3,26 @@ class Solution {
         if (obstacleGrid == null || obstacleGrid.length == 0) {
             return 0;
         }
+        // 定义 dp 数组并初始化第 1 行和第 1 列。
         int m = obstacleGrid.length, n = obstacleGrid[0].length;
-        //M[i][j]:how many ways from (0, 0) to (i, j)
-        int[][] M = new int[m][n];
-        for (int i = 0; i < n && obstacleGrid[0][i] != 1; i++) {
-            M[0][i] = 1;
+        int[][] dp = new int[m][n];
+        //first col
+        for (int i = 0; i < m && obstacleGrid[i][0] == 0; i++) {
+            dp[i][0] = 1;
         }
-        for (int i = 0; i < m && obstacleGrid[i][0] != 1; i++) {
-            M[i][0] = 1;
+        //first row
+        for (int j = 0; j < n && obstacleGrid[0][j] == 0; j++) {
+            dp[0][j] = 1;
         }
+
+        // 根据状态转移方程 dp[i][j] = dp[i - 1][j] + dp[i][j - 1] 进行递推。
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (obstacleGrid[i][j] == 0) {
-                    M[i][j] = M[i][j - 1] + M[i - 1][j];
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
                 }
             }
         }
-        return M[m - 1][n - 1];
+        return dp[m - 1][n - 1];
     }
 }
