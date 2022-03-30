4ba18fa52936c21d2ada76d0e4c3e42736b4b390
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-03-29 17:20:32 -0700

    Time: 17 ms (86.93%), Space: 42.4 MB (98.79%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,6 +1,8 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        Set<Integer> set = new HashSet<>();
+        //store the index of () to be removed(invalid)
+        Set<Integer> indexToRemove = new HashSet<>();
+        //store the index of () '(' push ')' pop
         Deque<Integer> stack = new ArrayDeque<>();
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
@@ -8,18 +10,19 @@ class Solution {
                 stack.offerFirst(i);
             } else if (c == ')') {
                 if (stack.isEmpty()) {
-                    set.add(i);
+                    indexToRemove.add(i);
                 } else {
                     stack.pollFirst();
                 }
             }
         }
+        //if there are element in the stack, it would be unmatched '('
         while (!stack.isEmpty()) {
-            set.add(stack.pollFirst());
+            indexToRemove.add(stack.pollFirst());
         }
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
-            if (!set.contains(i)) {
+            if (!indexToRemove.contains(i)) {
                 sb.append(s.charAt(i));
             }
         }
