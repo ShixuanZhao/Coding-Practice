5469579f02adda1914d6804819483ea450dfba45
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-03-30 13:44:35 -0700

    Time: 33 ms (42.75%), Space: 54.7 MB (33.51%) - LeetHub

--- 697-degree-of-an-array/697-degree-of-an-array.java
+++ 697-degree-of-an-array/697-degree-of-an-array.java
@@ -11,8 +11,8 @@ class Solution {
     public int findShortestSubArray(int[] nums) {
         Map<Integer, Integer> freq = new HashMap<>();
         Map<Integer, Integer> firstIndex = new HashMap<>();
+        int res = Integer.MAX_VALUE;
         int degree = 0;
-        int res = 0;
         for (int i = 0; i < nums.length; i++) {
             freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
             firstIndex.putIfAbsent(nums[i], i);
@@ -20,7 +20,7 @@ class Solution {
                 degree = freq.get(nums[i]);
                 res = i - firstIndex.get(nums[i]) + 1;
             } else if (freq.get(nums[i]) == degree) {
-                res = Math.min(res, i - firstIndex.get(nums[i]) + 1);
+                res = Math.min(res, res = i - firstIndex.get(nums[i]) + 1);
             }
         }
         return res;
