055edc52a64b771d0bc663f004b5470c8077a503
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-04-02 20:52:13 -0700

    Time: 0 ms (100.00%), Space: 41.9 MB (72.80%) - LeetHub

--- 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.java
+++ 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.java
@@ -7,7 +7,10 @@ class Node {
     public Node child;
 };
 */
-
+// Start form the head , move one step each time to the next node
+// When meet with a node with child, say node p, follow its child chain to the end and connect the tail node with p.next, by doing this we merged the child chain back to the main thread
+// Return to p and proceed until find next node with child.
+// Repeat until reach null
 class Solution {
     public Node flatten(Node head) {
         if (head == null) {
@@ -15,17 +18,21 @@ class Solution {
         }
         Node cur = head;
         while (cur != null) {
+            /* CASE 1: if no child, proceed */
             if (cur.child == null) {
                 cur = cur.next;
             } else {
+                 /* CASE 2: got child, find the tail of the child and link it to p.next */
                 Node temp = cur.child;
                 while (temp.next != null) {
                     temp = temp.next;
                 }
+                // Connect tail with p.next, if it is not null
                 temp.next = cur.next;
                 if (cur.next != null) {
                     cur.next.prev = temp;
                 }
+                // Connect p with p.child, and remove p.child
                 cur.next = cur.child;
                 cur.child.prev = cur;
                 cur.child = null;
