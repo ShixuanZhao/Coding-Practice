2c3d397938f2057f4aa66f28234fd9165c568e2a
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-04-03 13:35:22 -0700

    Time: 0 ms (100.00%), Space: 42 MB (12.58%) - LeetHub

--- 671-second-minimum-node-in-a-binary-tree/671-second-minimum-node-in-a-binary-tree.java
+++ 671-second-minimum-node-in-a-binary-tree/671-second-minimum-node-in-a-binary-tree.java
@@ -14,27 +14,55 @@
  * }
  */
 class Solution {
+    // public int findSecondMinimumValue(TreeNode root) {
+    //     if(root==null || root.right==null || root.left==null) return -1;
+    // Queue<TreeNode> q = new LinkedList<>();
+    // q.offer(root);
+    //     //int second = Integer.MAX_VALUE is wrong, 因为corner case：第二小的就是Integer.MAX_VALUE
+    //     //所以Integer null来处理这个corner case
+    //     Integer second = null;
+    //     while (!q.isEmpty()) {
+    //         TreeNode cur = q.poll();
+    //         if (cur.val != root.val) {
+    //             if (second == null) {
+    //                 second = cur.val;
+    //             } else if (cur.val < second){
+    //                 second = cur.val;
+    //                 //no need to add it's children into queue
+    //                 continue;
+    //             }
+    //         }
+    //         if(cur.right!=null) q.offer(cur.right);
+    // if(cur.left!=null) q.offer(cur.left);
+    //     }
+    //     return second == null ? -1 : second;
+    // }
+    
     public int findSecondMinimumValue(TreeNode root) {
-        if(root==null || root.right==null || root.left==null) return -1;
-	    Queue<TreeNode> q = new LinkedList<>();
-	    q.offer(root);
-        //int second = Integer.MAX_VALUE is wrong, 因为corner case：第二小的就是Integer.MAX_VALUE
-        //所以Integer null来处理这个corner case
-        Integer second = null;
-        while (!q.isEmpty()) {
-            TreeNode cur = q.poll();
-            if (cur.val != root.val) {
-                if (second == null) {
-                    second = cur.val;
-                } else if (cur.val < second){
-                    second = cur.val;
-                    //no need to add it's children into queue
-                    continue;
-                }
-            }
-            if(cur.right!=null) q.offer(cur.right);
-		    if(cur.left!=null) q.offer(cur.left);
+        if (root == null) {
+            return -1;
         }
-        return second == null ? -1 : second;
+        return dfs(root, root.val);
+    }
+    
+    private int dfs(TreeNode root, int min) {
+        if (root == null) {
+            //not found the second min
+            return -1;
+        }
+        if (root.val > min) {
+            //return to upper layer
+            return root.val;
+        }
+        //root.val == min
+        int left = dfs(root.left, min);
+        int right = dfs(root.right, min);
+        if (left == -1) {
+            return right;
+        } else if (right == -1) {
+            return left;
+        } else {
+            return Math.min(left, right);
+        }
     }
 }
