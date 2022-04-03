f03eac1af4c1b78855a4b67d9dabcc3545364023
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-04-03 15:55:29 -0700

    Time: 0 ms (100.00%), Space: 41.7 MB (99.23%) - LeetHub

---
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+//     public int kthSmallest(TreeNode root, int k) {
+//         //inOrder traverse
+//         int[] cnt = new int[1];
+//         int[] res = new int[1];
+//         helper(root, k, cnt, res);
+//         return res[0];
+//     }
+    
+//     private void helper(TreeNode root, int k, int[] cnt, int[] res) {
+//         if (root == null) {
+//             return;
+//         }
+//         helper(root.left, k, cnt, res);
+//         cnt[0]++;
+//         if (cnt[0] == k) {
+//             res[0] = root.val;
+//         }
+//         helper(root.right, k, cnt, res);
+//     }
+    
+    //iterative
+    public int kthSmallest(TreeNode root, int k) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.offerFirst(root);
+                root = root.left;
+            }
+            root = stack.pollFirst();
+            if (--k == 0) {
+                break;
+            }
+            root = root.right;
+        }
+        return root.val;
+    }
+}
