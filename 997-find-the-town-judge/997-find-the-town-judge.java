37d9c776159973595a1a03dd0dc0d962fb4a1b33
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-04-04 23:12:26 -0700

    Time: 2 ms (99.97%), Space: 75.1 MB (7.55%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.java
+++ 997-find-the-town-judge/997-find-the-town-judge.java
@@ -1,4 +1,6 @@
 class Solution {
+//     Consider trust as a graph, all pairs are directed edge.
+// The point with in-degree - out-degree = N - 1 become the judge.
     public int findJudge(int N, int[][] trust) {
         //becasue we label 1 to n
         int[] Trusted = new int[N+1];
