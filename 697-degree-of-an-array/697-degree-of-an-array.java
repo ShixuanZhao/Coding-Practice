c2d4f69cce9ae38a8d41a2393f364405cf82442b
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-04-06 11:37:46 -0700

    Time: 51 ms (14.87%), Space: 54.1 MB (60.43%) - LeetHub

--- 697-degree-of-an-array/697-degree-of-an-array.java
+++ 697-degree-of-an-array/697-degree-of-an-array.java
@@ -1,25 +1,30 @@
 class Solution {
-//     One pass on A,
-// For each different number a in A,
-// we need to count its frequency and it first occurrence index.
-
-// If a has the maximum frequency,
-// update the degree = count[a] and res = i - first[A[i]] + 1.
-
-// If a is one of the numbers that has the maximum frequency,
-// update the res = min(res, i - first[A[i]] + 1)
+    /*
+    input: int[]
+    output: int 
+    clarify:num == null || length == 0 return 0
+    freMap <num, freq>
+    degree
+    first_index_map <num, first index>
+    [1,2,2,3,1]
+         i
+     freqMap:<1, 1> <2, 2>
+     indexMap:<1, 0> <2, 1>
+     indegree:2
+     res:2
+    */
     public int findShortestSubArray(int[] nums) {
-        Map<Integer, Integer> freq = new HashMap<>();
+        Map<Integer, Integer> freqMap = new HashMap<>();
         Map<Integer, Integer> firstIndex = new HashMap<>();
-        int res = Integer.MAX_VALUE;
+        int res = 0;
         int degree = 0;
         for (int i = 0; i < nums.length; i++) {
-            freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
+            freqMap.put(nums[i], freqMap.getOrDefault(nums[i], 0) + 1);
             firstIndex.putIfAbsent(nums[i], i);
-            if (freq.get(nums[i]) > degree) {
-                degree = freq.get(nums[i]);
+            if (freqMap.get(nums[i]) > degree) {
+                degree = freqMap.get(nums[i]);
                 res = i - firstIndex.get(nums[i]) + 1;
-            } else if (freq.get(nums[i]) == degree) {
+            } else if (freqMap.get(nums[i]) == degree) {
                 res = Math.min(res, i - firstIndex.get(nums[i]) + 1);
             }
         }
