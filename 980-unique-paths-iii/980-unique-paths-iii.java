66c453b69340e471deae274ec9021e60cc43c4bb
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-04-05 17:59:56 -0700

    Time: 1 ms (85.49%), Space: 42.2 MB (27.09%) - LeetHub

---
+++ 980-unique-paths-iii/980-unique-paths-iii.java
@@ -0,0 +1,42 @@
+class Solution {
+    /*
+    First find out where the start and the end is.
+Also We need to know the number of empty cells.
+
+We we try to explore a cell,
+it will change 0 to -2 and do a dfs in 4 direction.
+    */
+    int res = 0, empty = 1, sx, sy, ex, ey;
+    public int uniquePathsIII(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 0) empty++;
+                else if (grid[i][j] == 1) {
+                    sx = i;
+                    sy = j;
+                }
+            }
+        }
+        dfs(grid, sx, sy);
+        return res;
+    }
+    
+    public void dfs(int[][] grid, int x, int y) {
+        if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length || grid[x][y] < 0)
+            return;
+        if (grid[x][y] == 2) {
+            if (empty == 0) res++;
+            return;
+        }
+        //have visited
+        grid[x][y] = -2;
+        empty--;
+        dfs(grid, x + 1, y);
+        dfs(grid, x - 1, y);
+        dfs(grid, x, y + 1);
+        dfs(grid, x, y - 1);
+        grid[x][y] = 0;
+        empty++;
+    }
+}
