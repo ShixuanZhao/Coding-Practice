a5b33e4e0fabb6304fc5d2cfaaa8f22b32c21412
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-04-05 21:11:50 -0700

    Time: 14 ms (57.54%), Space: 39.6 MB (92.24%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.java
+++ 37-sudoku-solver/37-sudoku-solver.java
@@ -3,6 +3,7 @@ class Solution {
         dfs(board, 0, 0);
     }
     
+    //define the return type is boolean is important
     private boolean dfs(char[][] board, int i, int j) {
         int m = board.length;
         int n = board[0].length;
@@ -18,6 +19,7 @@ class Solution {
         for (char c = '1'; c <= '9'; c++) {
             if (isValid(board, i, j, c)) {
                 board[i][j] = c;
+                //!!
                 if (dfs(board, i, j + 1)) {
                     return true;
                 }
@@ -38,6 +40,10 @@ class Solution {
                 return false;
             }
             //same block
+            // 判断 3 x 3 方框是否存在重复
+            //(r/3)*3代表9个block中每个block的起始行号0 3 6 i/3就是递增量从0到2.   列同理
+            //其实就是先固定每个block最左上角元素的坐标，再递增
+            //注意行不变的时候，列要递增，所以列的时候是i%3
             if (board[(r / 3) * 3 + i / 3][(c / 3) * 3 + i % 3] == n) {
                 return false;
             }
