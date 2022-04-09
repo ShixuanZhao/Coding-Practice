39c2b7dbe4462e592fd86c2b1212ab7b6c23e26d
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-04-09 13:06:19 -0700

    Time: 2 ms (47.18%), Space: 44.1 MB (41.40%) - LeetHub

--- 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.java
+++ 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.java
@@ -14,24 +14,40 @@
  * }
  */
 class Solution {
+//     public int findBottomLeftValue(TreeNode root) {
+//         if (root == null) return 0;
+        
+//         int result = 0;
+//         Queue<TreeNode> queue = new LinkedList<>();
+//         queue.add(root);
+        
+//         while (!queue.isEmpty()) {
+//             int size = queue.size();
+//             for (int i = 0; i < size; i++) {
+//                 TreeNode node = queue.poll();
+//                 //Only additional step is to remember the first element of each level.
+//                 if (i == 0) result = node.val;
+//                 if (node.left != null) queue.add(node.left);
+//                 if (node.right != null) queue.add(node.right);
+//             }
+//         }
+        
+//         return result;
+//     }
+    
+    //bfs先遍历right node再left node
     public int findBottomLeftValue(TreeNode root) {
-        if (root == null) return 0;
-        
-        int result = 0;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                //Only additional step is to remember the first element of each level.
-                if (i == 0) result = node.val;
-                if (node.left != null) queue.add(node.left);
-                if (node.right != null) queue.add(node.right);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            root = q.poll();
+            if (root.right != null) {
+                q.offer(root.right);
+            }
+            if (root.left != null) {
+                q.offer(root.left);
             }
         }
-        
-        return result;
+        return root.val;
     }
 }
