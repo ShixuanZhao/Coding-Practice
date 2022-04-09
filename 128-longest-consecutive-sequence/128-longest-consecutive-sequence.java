f51fb2a1bf38c9adccb7a7a575bbe18e1bb6b9a3
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-04-09 12:32:42 -0700

    Time: 524 ms (21.05%), Space: 147.3 MB (23.22%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,24 +1,26 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
+        //算法的关键：找出连续序列的的起点，设起点为x，看x+1,x+2...
+        //怎么筛选起点？如果在set里有x-1，那可以忽略，因为他肯定不是起点
+        //每个element遍历一遍，判断在不在set是O（1），总的T=O（n）
         if (nums.length == 0) {
             return 0;
         }
         Set<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            set.add(num);
+        for (int val : nums) {
+            set.add(val);
         }
-        int res = 1;
+        int globalMax = 1;
         for (int i = 0; i < nums.length; i++) {
-            int curLen = 1;
-            int temp = nums[i];
-            if (!set.contains(temp - 1)) {
-                while (set.contains(temp + 1)) {
-                    temp = temp + 1;
-                    curLen++;
+            //说明是起点
+            int curMax = 1;
+            if (!set.contains(nums[i] - 1)) {
+                while (set.contains(++nums[i])) {
+                    curMax++;
                 }
-            }
-            res = Math.max(res, curLen);
+            globalMax = Math.max(globalMax, curMax);    
+            }           
         }
-        return res;
+        return globalMax;
     }
 }
