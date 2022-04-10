d8f9fcc2e1694f038aded24e85ed38234219038a
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-04-10 00:53:26 -0700

    Time: 6 ms (95.74%), Space: 73.8 MB (69.94%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.java
+++ 904-fruit-into-baskets/904-fruit-into-baskets.java
@@ -1,19 +1,47 @@
 class Solution {
     //hashmap sliding window
+    // public int totalFruit(int[] fruits) {
+    //     Map<Integer, Integer> map = new HashMap<>();
+    //     int left = 0;
+    //     int res = 0;
+    //     for (int i = 0; i < fruits.length; i++) {
+    //         map.put(fruits[i], map.getOrDefault(fruits[i], 0) + 1);
+    //         while (map.size() > 2) {
+    //             map.put(fruits[left], map.get(fruits[left]) - 1);
+    //             if (map.get(fruits[left]) == 0) {
+    //                 map.remove(fruits[left]);
+    //             }
+    //             left++;
+    //         }
+    //         res = Math.max(res, i - left + 1);
+    //     }
+    //     return res;
+    // }
+    
+    
+    //another solution without hashmap
     public int totalFruit(int[] fruits) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int left = 0;
+        //last kind of fruit
+        int last = -1;
+        int second = -1;
+        int lastCnt = 0;
+        int curMax = 0;
         int res = 0;
-        for (int i = 0; i < fruits.length; i++) {
-            map.put(fruits[i], map.getOrDefault(fruits[i], 0) + 1);
-            while (map.size() > 2) {
-                map.put(fruits[left], map.get(fruits[left]) - 1);
-                if (map.get(fruits[left]) == 0) {
-                    map.remove(fruits[left]);
-                }
-                left++;
+        for (int fruit : fruits) {
+            if (fruit == last || fruit == second) {
+                curMax++;
+            } else {
+                curMax = lastCnt + 1;
             }
-            res = Math.max(res, i - left + 1);
+            
+            if (fruit == last) {
+                lastCnt++;
+            } else {
+                lastCnt = 1;
+                second = last;
+                last = fruit;
+            }
+            res = Math.max(res, curMax);
         }
         return res;
     }
