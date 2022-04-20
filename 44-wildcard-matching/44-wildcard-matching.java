11ae3c612da8e5e033085e55109901beddddb6bf
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-04-20 08:41:25 -0700

    Time: 45 ms (40.33%), Space: 48.6 MB (42.58%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.java
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -3,25 +3,25 @@ class Solution {
         int m = s.length();
         int n = p.length();
 	    boolean[][] M = new boolean[m + 1][n + 1];
-	    M[0][0] = true;
+        M[0][0] = true;
         for (int i = 1; i <= m; i++) {
             M[i][0] = false;
         }
-        for (int j = 1; j <= n; j++) {
-            if (p.charAt(j - 1) == '*') {
-                //match 0 letter
-                M[0][j] = M[0][j - 1];
+        //"" ***
+        for (int i = 1; i <= n; i++) {
+            if (p.charAt(i - 1) == '*') {
+                M[0][i] = M[0][i - 1];
             }
-            //other case would be false
         }
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                if (p.charAt(j - 1) == '*') {
-                    //case1:match 0 letter
-                    //case2:match 1 or more letter
-                    M[i][j] = M[i][j - 1] || M[i - 1][j];
-                } else if (p.charAt(j - 1) == '?' || p.charAt(j - 1) == s.charAt(i - 1)) {
+                //case1: aa aa | ab a?
+                //compare the ending letter of s and p
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?') {
                     M[i][j] = M[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    //abc * M[i - 1][j]| ab a* M[i - 1][j - 1] | ab ab* M[i][j - 1] 
+                    M[i][j] = M[i - 1][j] || M[i - 1][j - 1] || M[i][j - 1];
                 } else {
                     M[i][j] = false;
                 }
