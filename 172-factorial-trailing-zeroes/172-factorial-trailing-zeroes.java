0287901df32b6b38d6399c28722118afaf39db9e
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-04-22 01:27:13 -0700

    Time: 0 ms (100.00%), Space: 39.1 MB (93.86%) - LeetHub

---
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int trailingZeroes(int n) {
+//         Because all trailing 0 is from factors 5 * 2.
+// But sometimes one number may have several 5 factors, for example, 25 have two 5 factors, 125 have three 5 factors. In the n! operation, factors 2 is always ample. So we just count how many 5 factors in all number from 1 to n.
+        int cnt = 0;
+        //counter the number of 5
+        for (int i = 5; i <= n; i *= 5) {
+            cnt += n / i;
+        }
+        return cnt;
+    }
+}
