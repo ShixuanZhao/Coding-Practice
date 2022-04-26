4d42f337513f1e7584f72abc6e9063a25df80874
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-04-26 05:37:15 -0700

    Time: 146 ms (70.99%), Space: 99.7 MB (65.66%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.java
+++ 1396-design-underground-system/1396-design-underground-system.java
@@ -1,59 +1,63 @@
 class UndergroundSystem {
-    static class Event {
-        int id;
-        String name;
-        int time;
-        public Event(int id, String name, int time) {
+    //<id, event>
+    private Map<Integer, Event> arrivals;
+    //<"start + end", Average>
+    private Map<String, Average> averages;
+    
+    public UndergroundSystem() {
+        arrivals = new HashMap<>();
+        averages = new HashMap<>();
+    }
+    
+    //checkIn->arrival
+    public void checkIn(int id, String stationName, int t) {
+        arrivals.put(id, new Event(id, stationName, t));
+    }
+    
+    //update average
+    public void checkOut(int id, String stationName, int t) {
+        Event arrivalEvent = arrivals.get(id);
+        arrivals.remove(id);
+        int diff = t - arrivalEvent.time;
+        String key = arrivalEvent.stationName + "," + stationName;
+        Average average = averages.getOrDefault(key, new Average());
+        average.updateAverage(diff);
+        averages.put(key, average);
+    }
+    
+    public double getAverageTime(String startStation, String endStation) {
+        String key = startStation + "," + endStation;
+        return averages.get(key).getAverage();
+    }
+    
+    class Event {
+        public int id;
+        public String stationName;
+        public int time;
+        
+        public Event(int id, String stationName, int time) {
             this.id = id;
-            this.name = name;
+            this.stationName = stationName;
             this.time = time;
         }
     }
     
-    static class Average {
-        double totalTime;
-        int num; 
+    class Average {
+        //total time 
+        public double total = 0;
+        //the number of passengers
+        public int cnt = 0;
         
-        public Average() {
-            this.totalTime = 0;
-            this.num = 0;
-        }
-        
-        public void update(int duration) {
-            totalTime += duration;
-            num++;
+        public void updateAverage(int diff) {
+            cnt++;
+            total += diff;
         }
         
         public double getAverage() {
-            return totalTime / num;
+            return total / cnt;
         }
     }
-    Map<Integer, Event> arrival;
-    Map<String, Average> average;
-    public UndergroundSystem() {
-        arrival = new HashMap<>();
-        average = new HashMap<>();
-    }
-    
-    public void checkIn(int id, String stationName, int t) {
-        arrival.put(id, new Event(id, stationName, t));
-    }
-    
-    public void checkOut(int id, String stationName, int t) {
-        Event e = arrival.get(id);
-        arrival.remove(id);
-        int duration = t - e.time;
-        String key = e.name + "+" + stationName;
-        average.putIfAbsent(key, new Average());
-        average.get(key).update(duration);
-    }
-    
-    public double getAverageTime(String startStation, String endStation) {
-        String key = startStation + "+" + endStation;
-        return average.get(key).getAverage();
-    }
 }
-
 /**
  * Your UndergroundSystem object will be instantiated and called as such:
  * UndergroundSystem obj = new UndergroundSystem();
