e0d5cf4cdf12c79c95a6c50bfcc484729fc50e4e
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-04-27 19:22:21 -0700

    Time: 184 ms (47.35%), Space: 124.4 MB (52.35%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.java
@@ -1,40 +1,42 @@
 class MedianFinder {
-    //small is maxHeap, large is a minHeap, small <= large the size of small and large is balanced
-    //1 2 3 small: 1 2 large: 3
-    //the size of small - the size of large <= 1
-    //最大堆里面放小的元素，栈顶是2。最小堆放大的，3
-    private PriorityQueue<Integer> small = new PriorityQueue<>(Collections.reverseOrder());
-    private PriorityQueue<Integer> large = new PriorityQueue<>();
-    private boolean even = true;
-    
+    /*
+    2  3  |     4
+maxHeap       minHeap
+if size1 == size2 add to maxHeap
+else add to minHeap
+we should also guarantee the top of maxHeap <= top of minHeap
+
+find median:if size1 == size2 (a + b) / 2
+            else   maxHeap.peek
+    */
+    PriorityQueue<Integer> maxHeap;
+    PriorityQueue<Integer> minHeap;
     public MedianFinder() {
-        
+        maxHeap = new PriorityQueue<>((a, b) -> (b - a));
+        minHeap = new PriorityQueue<>();
     }
-//     Time:
-// Constructor: O(1)
-// addNum: O(logN)
-// findMedian: O(1)
-// Space: O(N)
+    
     public void addNum(int num) {
-        if (even) {
-            //before:small 1 large:2   3
-            //after:small: 1 2 large: 3
-            //make sure small have more ele
-            large.offer(num);
-            small.offer(large.poll());
-            
+        if (maxHeap.size() == minHeap.size()) {
+            maxHeap.offer(num);
+            if (!minHeap.isEmpty() && maxHeap.peek() > minHeap.peek()) {
+                minHeap.offer(maxHeap.poll());
+                maxHeap.offer(minHeap.poll());
+            }
         } else {
-            small.offer(num);
-            large.offer(small.poll());
+            minHeap.offer(num);
+            if (maxHeap.peek() > minHeap.peek()) {
+                minHeap.offer(maxHeap.poll());
+                maxHeap.offer(minHeap.poll());
+            }
         }
-        even = !even;
     }
     
     public double findMedian() {
-        if (even) {
-            return (small.peek() + large.peek()) / 2.0;
+        if (maxHeap.size() == minHeap.size()) {
+            return (double) (maxHeap.peek() + minHeap.peek()) / 2;
         } else {
-            return small.peek();
+            return (double) (maxHeap.peek());
         }
     }
 }
