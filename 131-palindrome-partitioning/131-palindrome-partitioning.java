d71dfb4cb8f4f4a86f21b78df9a499cd5845ed6a
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-04-27 23:59:40 -0700

    Time: 38 ms (8.02%), Space: 228.9 MB (5.02%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -1,32 +1,33 @@
 class Solution {
+    /*
+          root(i = 0) i is the start pos
+       a    aa   aab(is not pali)
+     a ab   b
+    */
     public List<List<String>> partition(String s) {
-        List<List<String>> res = new ArrayList<>();
-        if (s == null || s.length() == 0) {
-            return res;
-        }
-        List<String> cur = new ArrayList<>();
-        char[] array = s.toCharArray();
-        partition(array, 0, res, cur);
+        List<List<String>> res = new LinkedList<>();
+        List<String> cur = new LinkedList<>();
+        dfs(res, cur, 0, s);
         return res;
     }
-
-    //dfs
-    private void partition(char[] array, int index, List<List<String>> res, List<String> cur) {
-        if (index == array.length) {
+    
+    private void dfs(List<List<String>> res, List<String> cur, int index, String s) {
+        if (index == s.length()) {
             res.add(new LinkedList<>(cur));
+            return;
         }
-        for (int i = index; i < array.length; i++) {
-            if (isPalidrome(array, index, i)) {
-                cur.add(new String(array, index, i - index + 1));
-                partition(array, i + 1, res, cur);
+        for (int i = index; i < s.length(); i++) {
+            if (isP(s, index, i)) {
+                cur.add(s.substring(index, i + 1));
+                dfs(res, cur, i + 1, s);
                 cur.remove(cur.size() - 1);
             }
         }
     }
-
-    private boolean isPalidrome(char[] array, int i, int j) {
+    
+    private boolean isP(String s, int i, int j) {
         while (i < j) {
-            if (array[i] != array[j]) {
+            if (s.charAt(i) != s.charAt(j)) {
                 return false;
             }
             i++;
