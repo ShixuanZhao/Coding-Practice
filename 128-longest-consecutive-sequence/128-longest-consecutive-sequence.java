1bd23425de3343627788feb250de1dd47958b99c
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-04-28 20:42:56 -0700

    Time: 525 ms (19.41%), Space: 147.4 MB (20.27%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,26 +1,34 @@
 class Solution {
+    /*
+    [100,4,200,1,3,2]
+               i
+    check for each possible start, use a set to record all the ele
+    keep checking for cur + 1 in the set 
+    case1:cur - 1 is in the set || cur + 1 is not in the set, it means cur is  not the start
+        ignore and check next ele
+    case2:repeat and update the maxLen
+    */
     public int longestConsecutive(int[] nums) {
-        //算法的关键：找出连续序列的的起点，设起点为x，看x+1,x+2...
-        //怎么筛选起点？如果在set里有x-1，那可以忽略，因为他肯定不是起点
-        //每个element遍历一遍，判断在不在set是O（1），总的T=O（n）
-        if (nums.length == 0) {
+        if (nums == null || nums.length == 0) {
             return 0;
         }
+        int res = 1;
         Set<Integer> set = new HashSet<>();
-        for (int val : nums) {
-            set.add(val);
+        for (int num : nums) {
+            set.add(num);
         }
-        int globalMax = 1;
         for (int i = 0; i < nums.length; i++) {
-            //说明是起点
-            int curMax = 1;
-            if (!set.contains(nums[i] - 1)) {
-                while (set.contains(++nums[i])) {
-                    curMax++;
-                }
-            globalMax = Math.max(globalMax, curMax);    
-            }           
+            int cur = nums[i];
+            if (set.contains(cur - 1) || !set.contains(cur + 1)) {
+                continue;
+            }
+            int curLen = 1;
+            while (i < nums.length && set.contains(cur + 1)) {
+                cur = cur + 1;
+                curLen++;
+            }
+            res = Math.max(res, curLen);
         }
-        return globalMax;
+        return res;
     }
 }
