dd07dd1d45cc4af9e384fde9c7ce29b44924d68f
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-04-28 19:26:29 -0700

    Time: 2 ms (56.58%), Space: 74 MB (39.61%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,40 +1,27 @@
 class Solution {
     /*
-        WRONG:because there are negative number in the array
-        // two pointer: left and right
-        // define a sliding window [left, right]
-        // use a variable sum to record the sum in the window
-        input:int[] num output: int
-        clarify: num == null || num.length == 0
-        return Integer.Min_VALUE;
-        primitive: sunstring [left, right]
-        for left 
-           for right
-              getSum: calculate the sum and update the max
-       T = O(n^3)
-       //dp 
-       M[i]: the maxSum of subarray from index0 to index i(including)
-       M[i] = if M[i - 1] > 0, M[i - 1] + nums[i]
-               else ,          nums[i]
-       inti:M[0] = nums[i];
-        optimization:use a var prev to represent M[i - 1]
-        T = O(n) S = O(1)
-       
+    M[i]:the max sum ending at index i
+    M[i] = M[i - 1] + nums[i] M[i - 1] > 0
+            nums[i]           else
+    keep a globalMax, update for each M[i]
+    [-2,1,-3,4,-1,2,1,-5,4]
     */
     public int maxSubArray(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return Integer.MIN_VALUE;
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0];
         }
-        int prev = nums[0];
-        int globalMax = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            if (prev < 0) {
-                prev = nums[i];
+        int[] M = new int[n];
+        M[0] = nums[0];
+        int res = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (M[i - 1] > 0) {
+                M[i] = M[i - 1] + nums[i];
             } else {
-                prev += nums[i];
+                M[i] = nums[i];
             }
-            globalMax = Math.max(globalMax, prev);
+            res = Math.max(res, M[i]);
         }
-        return globalMax;
+        return res;
     }
 }
