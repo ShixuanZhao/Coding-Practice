697d388d2d4cb97fe0833a5b03493faa4d854e69
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-04-29 04:57:16 -0700

    Time: 11 ms (95.29%), Space: 96.7 MB (67.46%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -1,23 +1,43 @@
 class Solution {
+    //top-down easy to understand
+//     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+//         //build the map<Manager, list of Employee>
+//         Map<Integer, List<Integer>> map = new HashMap<>();
+//         for (int i = 0; i < manager.length; i++) {
+//             map.putIfAbsent(manager[i], new LinkedList<>());
+//             map.get(manager[i]).add(i);
+//         }
+//         return dfs(map, headID, informTime);
+//     }
+    
+//     private int dfs(Map<Integer, List<Integer>> map, int manager, int[] informTime) {
+//         int max = 0;
+//         //leaf node
+//         if (!map.containsKey(manager)) {
+//             return max;
+//         }
+//         for (int employee : map.get(manager)) {
+//             max = Math.max(max, dfs(map, employee, informTime));
+//         }
+//         return max + informTime[manager];
+//     }
+    
+    //bottom-up 
     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
-        //build the map<Manager, list of Employee>
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        for (int i = 0; i < manager.length; i++) {
-            map.putIfAbsent(manager[i], new LinkedList<>());
-            map.get(manager[i]).add(i);
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            res = Math.max(res, dfs(i, manager, informTime));
         }
-        return dfs(map, headID, informTime);
+        return res;
     }
     
-    private int dfs(Map<Integer, List<Integer>> map, int manager, int[] informTime) {
-        int max = 0;
-        //leaf node
-        if (!map.containsKey(manager)) {
-            return max;
+    //the time need for the employee i be informed
+    //informTime: the time need for the employee i be informed from the root for each node
+    private int dfs(int i, int[] manager, int[] informTime) {
+        if (manager[i] != -1) {
+            informTime[i] += dfs(manager[i], manager, informTime);
+            manager[i] = -1;
         }
-        for (int employee : map.get(manager)) {
-            max = Math.max(max, dfs(map, employee, informTime));
-        }
-        return max + informTime[manager];
+        return informTime[i];
     }
 }
