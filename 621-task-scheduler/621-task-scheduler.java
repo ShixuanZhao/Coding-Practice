e10af468620226aa9ab5f95629b3ccc80427919e
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-04-29 20:47:08 -0700

    Time: 2 ms (98.69%), Space: 43.3 MB (88.55%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.java
+++ 621-task-scheduler/621-task-scheduler.java
@@ -1,4 +1,12 @@
 class Solution {
+    /*
+    ["A","A","A","B","B","B"], n = 2
+    [3,3,0,0....]
+    [0,0,...3,3]
+    idle = 2 * 2 = 4
+    idle = 4 - 2 = 2
+    2 + 6 = 8
+    */
     public int leastInterval(char[] tasks, int n) {
         //char_map
         int[] map = new int[26];
@@ -8,10 +16,13 @@ class Solution {
         Arrays.sort(map);
         //the last ele not need to wait for cool down
         int max_val = map[25] - 1;
+        //after fill out the most freq ele, suppose all other are idle
         int idle_slots = max_val * n;
+        //fill out the idles 
         for (int i = 24; i >= 0; i--) {
             idle_slots -= Math.min(map[i], max_val);
         }
+        //remaining idles are idles that can not be filled up
         return idle_slots > 0 ? idle_slots + tasks.length : tasks.length;
     }
 }
