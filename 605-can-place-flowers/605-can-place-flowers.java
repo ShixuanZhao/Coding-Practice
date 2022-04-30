8fb88bfcfbb201e6897aa9c3764e138add4c3865
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-04-30 09:54:44 -0700

    Time: 1 ms (97.94%), Space: 52.2 MB (46.06%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.java
+++ 605-can-place-flowers/605-can-place-flowers.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
         int count = 0;
-        
+        //0 is empty
         for(int i = 0; i < flowerbed.length; i++){
             if(flowerbed[i] == 0){
                 int prev = (i == 0 || flowerbed[i - 1] == 0) ? 0 : 1;
