b8c7cabc4f76c6507ec646cb8d6bbbd101309b7f
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-04-30 02:09:55 -0700

    Time: 5 ms (95.48%), Space: 42.4 MB (89.46%) - LeetHub

--- 694-number-of-distinct-islands/694-number-of-distinct-islands.java
+++ 694-number-of-distinct-islands/694-number-of-distinct-islands.java
@@ -1,43 +1,34 @@
 class Solution {
     public int numDistinctIslands(int[][] grid) {
-        // 首先，对于形状相同的岛屿，如果从同一起点出发，dfs函数遍历的顺序肯定是一样的
-        // 相当于是岛屿序列化的结果，只要每次使用dfs遍历岛屿的时候生成这串数字进行比较，就可以计算到底有多少个不同的岛屿了。
-        int m = grid.length, n = grid[0].length;
-    // 记录所有岛屿的序列化结果
-    HashSet<String> islands = new HashSet<>();
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            if (grid[i][j] == 1) {
-                // 淹掉这个岛屿，同时存储岛屿的序列化结果
-                StringBuilder sb = new StringBuilder();
-                // 初始的方向可以随便写，不影响正确性
-                dfs(grid, i, j, sb, 666);
-                islands.add(sb.toString());
+        int m = grid.length;
+        int n = grid[0].length;
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    StringBuilder sb = new StringBuilder();
+                    dfs(grid, sb, i, j, -1);
+                    set.add(sb.toString());
+                }
             }
         }
-    }
-    // 不相同的岛屿数量
-    return islands.size();
+        return set.size();
     }
     
-    // 改造dfs函数，添加一些函数参数以便记录遍历顺序：
-    // dir记录方向，dfs函数递归结束后，sb记录着整个遍历顺序
-    void dfs(int[][] grid, int i, int j, StringBuilder sb, int dir) {
-    int m = grid.length, n = grid[0].length;
-    if (i < 0 || j < 0 || i >= m || j >= n 
-        || grid[i][j] == 0) {
-        return;
+    //dir:1 2 3 4 for 4 direction
+    private void dfs(int[][] grid, StringBuilder sb, int i, int j, int dir) {
+        int m = grid.length;
+        int n = grid[0].length;
+        //if we have visited one position, mark it as 0
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0) {
+            return;
+        }
+        grid[i][j] = 0;
+        sb.append(dir);
+        dfs(grid, sb, i - 1, j, 1);
+        dfs(grid, sb, i + 1, j, 2);
+        dfs(grid, sb, i, j - 1, 3);
+        dfs(grid, sb, i, j + 1, 4);
+        sb.append(-dir);
     }
-    // 前序遍历位置：进入 (i, j)
-    grid[i][j] = 0;
-    sb.append(dir).append(',');
-
-    dfs(grid, i - 1, j, sb, 1); // 上
-    dfs(grid, i + 1, j, sb, 2); // 下
-    dfs(grid, i, j - 1, sb, 3); // 左
-    dfs(grid, i, j + 1, sb, 4); // 右
-
-    // 后序遍历位置：离开 (i, j) !!!
-    sb.append(-dir).append(',');
-    }   
 }
