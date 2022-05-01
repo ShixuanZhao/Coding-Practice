7ceb428527aa41df620ad9e98dfe10abcea23064
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-01 00:01:15 -0700

    Time: 48 ms (37.54%), Space: 59.1 MB (59.56%) - LeetHub

--- 244-shortest-word-distance-ii/244-shortest-word-distance-ii.java
+++ 244-shortest-word-distance-ii/244-shortest-word-distance-ii.java
@@ -1,49 +1,29 @@
 class WordDistance {
-    //compare with wordDistance1 using two pointers
-    //in this question, use hashmap is faster because when calling shortest we do not need to interate
-    //over the String arr
-    private Map<String, List<Integer>> map;
+    //key is the word, value is all the appearing index
+    Map<String, List<Integer>> map;
     public WordDistance(String[] wordsDict) {
-        map = new HashMap<String, List<Integer>>();
-        for(int i = 0; i < wordsDict.length; i++) {
-            String w = wordsDict[i];
-            if(map.containsKey(w)) {
-                map.get(w).add(i);
-            } else {
-                List<Integer> list = new ArrayList<Integer>();
-                list.add(i);
-                map.put(w, list);
-            }
+        map = new HashMap<>();
+        for (int i = 0; i < wordsDict.length; i++) {
+            map.putIfAbsent(wordsDict[i], new ArrayList<>());
+            map.get(wordsDict[i]).add(i);
         }
     }
     
-    /*
-        q:two ascending arr, find the pair which has the min diff, return the diff
-        [1, 3, 4]
-               i
-        [2, 5]
-            j
-        min = 1
-        [2]
-           i
-        [3 5 6]
-         j
-         min = 1
-    */
     public int shortest(String word1, String word2) {
         List<Integer> list1 = map.get(word1);
         List<Integer> list2 = map.get(word2);
+        //two sorted list find the min distance
+        //move the pointer that point to smaller element
         int i = 0;
         int j = 0;
         int res = Integer.MAX_VALUE;
-        //if one pointer out of boundary, we should termionate because other pairs diff would be larger
         while (i < list1.size() && j < list2.size()) {
-            int index1 = list1.get(i), index2 = list2.get(j);
+            int index1 = list1.get(i);
+            int index2 = list2.get(j);
+            res = Math.min(res, Math.abs(index1 - index2));
             if (index1 < index2) {
-                res = Math.min(res, index2 - index1);
                 i++;
             } else {
-                res = Math.min(res, index1 - index2);
                 j++;
             }
         }
