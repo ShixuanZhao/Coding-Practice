a308e5064793b9f1fe0655f8be8645037f12bb44
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-01 04:02:37 -0700

    Time: 1 ms (86.88%), Space: 47.7 MB (49.98%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
@@ -29,4 +29,46 @@ class Solution {
         max[0] = Math.max(max[0], left + right + root.val);
         return Math.max(left, right) + root.val;
     }
+
+    
+//     public int maxPathSum(TreeNode root) {
+//         int[] max = new int[] {Integer.MIN_VALUE};
+//         String[] path = new String[1];
+//         maxPathSum(root, max, path);
+//         System.out.println(path[0]);
+//         return max[0];
+//     }
+    
+//     private Pair maxPathSum(TreeNode root, int[] max, String[] path) {
+//         if (root == null) {
+//             return new Pair(0, "");
+//         }
+//         Pair res = new Pair(0, "");
+//         int left = Math.max(0, maxPathSum(root.left, max, path).sum);
+//         int right = Math.max(0, maxPathSum(root.right, max, path).sum);
+//         StringBuilder sb = new StringBuilder();
+//         if (left + right + root.val > max[0]) {
+//             max[0] = left + right + root.val;
+//             path[0] = sb.append(maxPathSum(root.left, max, path).s).append("-").append(root.val).append("-").append(maxPathSum(root.right, max, path).s).toString();
+//             //System.out.println(path);
+//         }
+//         if (left > right) {
+//             res.sum = left + root.val;
+//             res.s = maxPathSum(root.left, max, path).s + "-" + String.valueOf(root.val);
+//         } else {
+//             res.sum = right + root.val;
+//             res.s = String.valueOf(root.val) + "-" + maxPathSum(root.right, max, path).s;
+//         }
+//         return res;
+//     }
+    
+//     class Pair {
+//         int sum;
+//         String s;
+        
+//         Pair(int sum, String s) {
+//             this.sum = sum;
+//             this.s = s;
+//         }
+//     }
 }
