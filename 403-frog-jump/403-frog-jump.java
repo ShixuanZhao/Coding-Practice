d8d1e6e5e43614674c599f9627257851402d15e6
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-01 02:17:45 -0700

    Time: 59 ms (67.61%), Space: 71.5 MB (26.12%) - LeetHub

--- 403-frog-jump/403-frog-jump.java
+++ 403-frog-jump/403-frog-jump.java
@@ -20,7 +20,11 @@ class Solution {
         for (int i = 2; i < n; i++) {
             for (int j = 1; j < i; j++) {
                 int k = stones[i] - stones[j];
-               if (j + 1 >= k) {
+                // 我们知道从位置 j 到位置 i 是需要步长为 k 的跳跃
+                // 而从位置 j 发起的跳跃最多不超过 j + 1
+                // 因为每次跳跃，下标至少增加 1，而步长最多增加 1 
+                //从i跳到j需要跳k步，实际最多k+1（假设每次step+1）
+               if (k <= j + 1) {
                     M[i][k] = M[j][k - 1] || M[j][k] || M[j][k + 1];
                }
             }
