62dbaf38d190593a2e1bb4b3b6edf63d21a5fab6
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-04-30 22:34:57 -0700

    Time: 2 ms (70.49%), Space: 44.3 MB (68.67%) - LeetHub

--- 243-shortest-word-distance/243-shortest-word-distance.java
+++ 243-shortest-word-distance/243-shortest-word-distance.java
@@ -1,47 +1,28 @@
 class Solution {
-    // public int shortestDistance(String[] wordsDict, String word1, String word2) {
-    //     //without hashmap, we can also use two list
-    //     Map<String, List<Integer>> map = new HashMap<>();
-    //     for (int i = 0; i < wordsDict.length; i++) {
-    //         map.putIfAbsent(wordsDict[i], new ArrayList<>());
-    //         map.get(wordsDict[i]).add(i);
-    //     }
-    //     List<Integer> list1 = map.get(word1);
-    //     List<Integer> list2 = map.get(word2);
-    //     int i = 0;
-    //     int j = 0;
-    //     int res = Integer.MAX_VALUE;
-    //     while (i < list1.size() && j < list2.size()) {
-    //         res = Math.min(res, Math.abs(list2.get(j) - list1.get(i)));
-    //         if (list1.get(i) < list2.get(j)) {
-    //             i++;
-    //         } else {
-    //             j++;
-    //         }
-    //     }
-    //     return res;
-    // }
-    
-    //not use hashmap
-    public int shortestDistance(String[] words, String word1, String word2) {
-        //the latest index for word1 and word2
-        /*
-        ***@**#**@@**#
-        */
+    /*
+    int i: the latest position of word1 in string arr
+    int j: the latest position of word2
+    initial: i = -1 j = -1
+    update the dis when either is updated
+    ["practice", "makes", "perfect", "coding", "makes"], word1 = "makes", word2 = "coding"
+                                       j        i
+    dis:1
+    */
+    public int shortestDistance(String[] wordsDict, String word1, String word2) {
         int p1 = -1;
         int p2 = -1;
         int res = Integer.MAX_VALUE;
-        for (int i = 0; i < words.length; i++) {
-            if (words[i].equals(word1)) {
+        for (int i = 0; i < wordsDict.length; i++) {
+            if (wordsDict[i].equals(word1)) {
                 p1 = i;
             }
-            if (words[i].equals(word2)) {
+            if (wordsDict[i].equals(word2)) {
                 p2 = i;
             }
             if (p1 != -1 && p2 != -1) {
                 res = Math.min(res, Math.abs(p1 - p2));
             }
-        } 
+        }
         return res;
     }
 }
