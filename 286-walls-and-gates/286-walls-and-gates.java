9f72151bac83434b7e2d8fa6f12fbfa87a45082f
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-02 02:45:35 -0700

    Time: 16 ms (30.92%), Space: 57.2 MB (33.00%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.java
+++ 286-walls-and-gates/286-walls-and-gates.java
@@ -1,30 +1,29 @@
 class Solution {
+    public static final int[][] dirs = new int[][] {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
     public void wallsAndGates(int[][] rooms) {
+        Queue<int[]> q = new ArrayDeque<>();
         int m = rooms.length;
-        int n = m == 0 ? 0 : rooms[0].length;
-        int[][] dirs = {{-1,0}, {0,1}, {0,-1}, {1,0}};
-        Queue<int[]> queue = new LinkedList<>();
-        // add all gates to the queue
-        for (int i=0; i<m; i++) {
-            for (int j=0; j<n; j++) {
+        int n = rooms[0].length;
+        boolean[][] visited = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (rooms[i][j] == 0) {
-                    queue.offer(new int[] {i,j});
+                    q.offer(new int[] {i, j});
+                    visited[i][j] = true;
                 }
             }
         }
-        // update distance from gates
-        while (!queue.isEmpty()) {
-            int[] curPos = queue.poll();
-            for (int[] dir: dirs) {
-                int X = curPos[0] + dir[0];
-                int Y = curPos[1] + dir[1];
-                //rooms[X][Y] != Integer.MAX_VALUE
-                //means we have visited the empty room
-                //bfs will ensure the shortest distance when we first time visit
-                //a empty room
-                if (X<0 || Y <0 || X >= m || Y >= n || rooms[X][Y] != Integer.MAX_VALUE || rooms[X][Y] == -1) continue;
-                rooms[X][Y] = rooms[curPos[0]][curPos[1]]+1;
-                queue.offer(new int[] {X, Y});
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            for (int[] dir : dirs) {
+                int x = cur[0] + dir[0];
+                int y = cur[1] + dir[1];
+                if (x < 0 || x >= m || y < 0 || y >= n || visited[x][y] || rooms[x][y] == -1) {
+                    continue;
+                }
+                q.offer(new int[] {x, y});
+                visited[x][y] = true;
+                rooms[x][y] = rooms[cur[0]][cur[1]] + 1;
             }
         }
     }
