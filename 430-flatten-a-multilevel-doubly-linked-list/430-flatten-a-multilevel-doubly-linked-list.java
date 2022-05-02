c265897e893e0c7c6b74be82e1fcdf651d6679ff
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-02 05:36:00 -0700

    Time: 0 ms (100.00%), Space: 40.6 MB (76.92%) - LeetHub

--- 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.java
+++ 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.java
@@ -25,7 +25,15 @@ class Solution {
         return head;
     }
     
-    //flatten the list and return the tail
+    //flatten the list and return the tail after flatten
+    //the tail means the last node after flattening "head"
+    
+    // Five situations:
+    // 1. null - no need to flatten, just return it
+    // 2. no child, no next - no need to flatten, it is the last element, just return it
+    // 3. no child, next - no need to flatten, go next
+    // 4. child, no next - flatten the child and done
+    // 5. child, next - flatten the child, connect it with the next, go next
     private Node flattenTail(Node head) {
         if (head == null) {
             return head;
@@ -52,3 +60,38 @@ class Solution {
         }
     }
 }
+
+// Start form the head , move one step each time to the next node
+// When meet with a node with child, say node p, follow its child chain to the end and connect the tail node with p.next, by doing this we merged the child chain back to the main thread
+// Return to p and proceed until find next node with child.
+// Repeat until reach null
+// class Solution {
+//     public Node flatten(Node head) {
+//         if (head == null) {
+//             return head;
+//         }
+//         Node cur = head;
+//         while (cur != null) {
+//             /* CASE 1: if no child, proceed */
+//             if (cur.child == null) {
+//                 cur = cur.next;
+//             } else {
+//                  /* CASE 2: got child, find the tail of the child and link it to p.next */
+//                 Node temp = cur.child;
+//                 while (temp.next != null) {
+//                     temp = temp.next;
+//                 }
+//                 // Connect tail with p.next, if it is not null
+//                 temp.next = cur.next;
+//                 if (cur.next != null) {
+//                     cur.next.prev = temp;
+//                 }
+//                 // Connect p with p.child, and remove p.child
+//                 cur.next = cur.child;
+//                 cur.child.prev = cur;
+//                 cur.child = null;
+//             }
+//         }
+//         return head;
+//     }
+// }
