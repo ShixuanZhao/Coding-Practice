1d85f5ad4333470d9ad5d583e0628e0e02512e86
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-02 06:51:11 -0700

    Time: 165 ms (7.57%), Space: 47.3 MB (62.31%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,49 +1,57 @@
 class Solution {
-    //longest inscreasing subsequence + helper fuc 判断predecessor
-    //逆向思维：删除一个，从后往前从长变短的一个chain
-    //M[i]:the longest chain starting from index i(begin to delete) 以i结尾的最长chain
-    //M[i] = MAX(M[j] + 1) words[j] can be obtained by deleting a letter from words[i]
-    //sort by length, liear scan the words have smaller length
-    //i scan from left to right, j scan from i - 1
+    /*
+    ["a","b","ba","bca","bda","bdca"]
+                                 i
+    1.sort by length, use a set to record all the words
+    2.delete oen letter, check all possible word after deletion whether in the set
+    */
+    //M[i]: the longest chain ending at index i(including)
+    //M[i] = Math.max(M[j]) words[j] and words[i] are predecessor
     public int longestStrChain(String[] words) {
-        Arrays.sort(words, (a, b) -> (a.length() - b.length()));
-        int res = 1;
         int n = words.length;
         int[] M = new int[n];
-        for (int i = 0; i < n; i++) {
-            //initialize
-            M[i] = 1;
-            //长度相同的word相邻，所以保证相邻重复的可以跳过
+        int res = 1;
+        Arrays.fill(M, 1);
+        Arrays.sort(words, (a, b) -> (a.length() - b.length()));
+        for (int i = 1; i < n; i++) {
             for (int j = i - 1; j >= 0 && words[i].length() - words[j].length() <= 1; j--) {
-                if (isPredecessor(words[j], words[i])) {
+                if (isP(words[i], words[j])) {
                     M[i] = Math.max(M[i], M[j] + 1);
                 }
             }
+            System.out.println(M[i]);
             res = Math.max(res, M[i]);
         }
         return res;
     }
     
-    private boolean isPredecessor(String s1, String s2) {
+    /*
+    bda
+       i
+    bdca
+        j
+     cnt:1
+    */
+    private boolean isP(String s1, String s2) {
         int n1 = s1.length();
         int n2 = s2.length();
         if (n1 == n2) {
             return false;
         }
+        //s1 is the longer one
         int i = 0;
         int j = 0;
-        //number of diff letter
+        //the number of letter in s2 has been ignored
         int cnt = 0;
-        while (i < n1) {
+        while (i < n1 && j < n2) {
             if (s1.charAt(i) == s2.charAt(j)) {
                 i++;
                 j++;
             } else {
-                cnt++;
-                if (cnt > 1) {
+                i++;
+                if (++cnt > 1) {
                     return false;
                 }
-                j++;
             }
         }
         return true;
