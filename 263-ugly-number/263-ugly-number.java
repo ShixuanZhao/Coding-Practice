aa27be9f350944ff31bcd651dcc4001c1f9c9a6a
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-03 02:36:49 -0700

    Time: 1 ms (95.66%), Space: 39.3 MB (94.82%) - LeetHub

--- 263-ugly-number/263-ugly-number.java
+++ 263-ugly-number/263-ugly-number.java
@@ -1,14 +1,11 @@
 class Solution {
     public boolean isUgly(int n) {
-        if (n == 0) {
+        if (n == 1) {
+            return true;
+        }
+        if (n <= 0) {
             return false;
         }
-        int[] factor = new int[] {2, 3, 5};
-        for (int num : factor) {
-            while (n % num == 0) {
-                n /= num;
-            }
-        }
-        return n == 1;
+        return (n % 2 == 0 && isUgly(n / 2)) || (n % 3 == 0 && isUgly(n / 3)) || (n % 5 == 0 && isUgly(n / 5));
     }
 }
