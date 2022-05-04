8676a12eca1e4b64778cfda7c8b946170932154a
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-04 06:25:13 -0700

    Time: 6 ms (42.48%), Space: 96.2 MB (42.72%) - LeetHub

--- 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.java
+++ 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.java
@@ -10,25 +10,42 @@ class Solution {
     min of right: 0 0 3 6 6
     
     */
-    public int partitionDisjoint(int[] nums) {
-        int n = nums.length;
-        int[] max = new int[n];
-        int prevMax = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++) {
-            max[i] = Math.max(prevMax, nums[i]);
-            prevMax = max[i];
-        }
-        int prevMin = Integer.MAX_VALUE;
-        int[] min = new int[n];
-        for (int i = n - 1; i >= 0; i--) {
-            min[i] = Math.min(prevMin, nums[i]);
-            prevMin = min[i];
-        }
-        for (int i = 0; i < n - 1; i++) {
-            if (max[i] <= min[i + 1]) {
-                return i + 1;
+    // public int partitionDisjoint(int[] nums) {
+    //     int n = nums.length;
+    //     int[] max = new int[n];
+    //     int prevMax = Integer.MIN_VALUE;
+    //     for (int i = 0; i < n; i++) {
+    //         max[i] = Math.max(prevMax, nums[i]);
+    //         prevMax = max[i];
+    //     }
+    //     int prevMin = Integer.MAX_VALUE;
+    //     int[] min = new int[n];
+    //     for (int i = n - 1; i >= 0; i--) {
+    //         min[i] = Math.min(prevMin, nums[i]);
+    //         prevMin = min[i];
+    //     }
+    //     for (int i = 0; i < n - 1; i++) {
+    //         if (max[i] <= min[i + 1]) {
+    //             return i + 1;
+    //         }
+    //     }
+    //     return n;
+    // }
+    
+    public int partitionDisjoint(int[] A) {
+        int cmax=A[0]; //cmax:current maximum
+        int nmax=A[0];//nmax: next maximum
+        int ans=0;
+        
+        for(int i=1;i<A.length;i++){
+            int val=A[i];
+          nmax=Math.max(val,nmax); //next max for case if i exist in left Part
+            
+            if(val<cmax){
+                ans=i;         //now left Part is till here
+                cmax=nmax; //maximum of left array is nmax(as we are maintaining maximum for split Part)
             }
         }
-        return n;
+        return ans+1; // we have to return length not idx so ans+1.
     }
 }
