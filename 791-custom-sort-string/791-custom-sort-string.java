4f02e48ea48e026fdeb65a9cfa0534f66b56e204
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-04 04:35:30 -0700

    Time: 1 ms (76.23%), Space: 42.3 MB (40.38%) - LeetHub

--- 791-custom-sort-string/791-custom-sort-string.java
+++ 791-custom-sort-string/791-custom-sort-string.java
@@ -1,38 +1,26 @@
 class Solution {
-    public String customSortString(String S, String T) {
-        int[] count = new int[26];
-        for (char c : T.toCharArray()) { ++count[c - 'a']; }  // count each char in T.
-        StringBuilder sb = new StringBuilder();
-        for (char c : S.toCharArray()) {                            
-            while (count[c - 'a']-- > 0) { sb.append(c); }    // sort chars both in T and S by the order of S.
+    /*
+    1.append the common letter according to the order in order string
+    2append the uncommon letter at tail
+    order = "cba", s = "abcd"
+    */
+    public String customSortString(String order, String s) {
+        //letter and freq in s
+        int[] cnt = new int[26];
+        for (char c : s.toCharArray()) {
+            cnt[c - 'a']++;
         }
-        for (char c = 'a'; c <= 'z'; ++c) {
-            while (count[c - 'a']-- > 0) { sb.append(c); }    // group chars in T but not in S.
+        StringBuilder sb = new StringBuilder();
+        for (char c : order.toCharArray()) {
+            while (cnt[c - 'a']-- > 0) {
+                sb.append(c);
+            }
+        }
+        for (char c = 'a'; c <= 'z'; c++) {
+            while (cnt[c - 'a']-- > 0) {
+                sb.append(c);
+            }
         }
         return sb.toString();
-   }
-    // public String customSortString(String order, String s) {
-    //     Map<Character, Integer> map = new HashMap<>();
-    //     for (char c : s.toCharArray()) {
-    //         map.put(c, map.getOrDefault(c, 0) + 1);
-    //     }
-    //     StringBuilder sb = new StringBuilder();
-    //     for (char c : order.toCharArray()) {
-    //         if (map.containsKey(c)) {
-    //             for (int i = 0; i < map.get(c); i++) {
-    //                 sb.append(c);
-    //             }
-    //             map.put(c, 0);
-    //         }
-    //     }
-    //     //add non-common ele to the tail
-    //     for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-    //         if (entry.getValue() != 0) {
-    //             for (int i = 0; i < entry.getValue(); i++) {
-    //                 sb.append(entry.getKey());
-    //             }
-    //         }
-    //     }
-    //     return sb.toString();
-    // }
+    }
 }
