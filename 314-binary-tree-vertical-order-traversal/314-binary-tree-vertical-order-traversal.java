7c7ab8b687bc6fa3ce0bad5078f506c823b01a16
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-04 00:45:01 -0700

    Time: 5 ms (44.92%), Space: 43.6 MB (45.29%) - LeetHub

--- 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.java
+++ 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.java
@@ -14,35 +14,77 @@
  * }
  */
 class Solution {
+    //T = O(nlogn) because we need to sorting in the for loop
+    // public List<List<Integer>> verticalOrder(TreeNode root) {
+    //     List<List<Integer>> res = new ArrayList<>();
+    //     //col to a list of ele which have diff level and val, so we need a int[] to record the level and val for each node
+    //     //key:col value:the node which have same col, int[]A A[0]:level A[1]:val
+    //     Map<Integer, List<int[]>> map = new TreeMap<>();
+    //     dfs(root, map, 0, 0);
+    //     for (List<int[]> cur : map.values()) {
+    //         //sort by level
+    //         //because dfs we use preOrder which can ensure traverse left node first when same col and level
+    //         Collections.sort(cur, (a, b) -> (a[0] - b[0]));
+    //         List<Integer> temp = new ArrayList<>();
+    //         for (int[] arr : cur) {
+    //             temp.add(arr[1]);
+    //         }
+    //         res.add(temp);
+    //     }
+    //     return res;
+    // }
+    // //for the treemap, key is col, value is a pair<level, val>
+    // private void dfs(TreeNode root, Map<Integer, List<int[]>> map, int col, int level) {
+    //     if (root == null) {
+    //         return;
+    //     }
+    //     if (!map.containsKey(col)) {
+    //         map.put(col, new LinkedList<int[]>());
+    //     }
+    //     map.get(col).add(new int[] {level, root.val});
+    //     //preOrder traversal, must be traverse left subtree first because for same level and col, we need the left one first 
+    //     dfs(root.left, map, col - 1, level + 1);
+    //     dfs(root.right, map, col + 1, level + 1);
+    // }
+    
+    //better solution T = O(n) S = O(n)
+    //BFS, we do not sorting for each col
     public List<List<Integer>> verticalOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<>();
-        //col to a list of ele which have diff level and val, so we need a int[] to record the level and val for each node
-        //key:col value:the node which have same col, int[]A A[0]:level A[1]:val
-        Map<Integer, List<int[]>> map = new TreeMap<>();
-        dfs(root, map, 0, 0);
-        for (List<int[]> cur : map.values()) {
-            //sort by level
-            //because dfs we use preOrder which can ensure traverse left node first when same col and level
-            Collections.sort(cur, (a, b) -> (a[0] - b[0]));
-            List<Integer> temp = new ArrayList<>();
-            for (int[] arr : cur) {
-                temp.add(arr[1]);
+        List<List<Integer>> res = new LinkedList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.offer(root);
+        //key is the col number, val is a list ele in this col
+        //add the map, when we pop the node from q
+        //the root, col is 0 left:-1 right:+1
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        //key is the node, val is the col number
+        //the mapping between node and col
+        //update when we offer child node into the queue
+        Map<TreeNode, Integer> colMap = new HashMap<>();
+        colMap.put(root, 0);
+        //the mininum col
+        int min = 0;
+        while (!q.isEmpty()) {
+            TreeNode cur = q.poll();
+            int col = colMap.get(cur);
+            map.putIfAbsent(col, new LinkedList<>());
+            map.get(col).add(cur.val);
+            if (cur.left != null) {
+                q.offer(cur.left);
+                colMap.put(cur.left, col - 1);
             }
-            res.add(temp);
+            if (cur.right != null) {
+                q.offer(cur.right);
+                colMap.put(cur.right, col + 1);
+            }
+            min = Math.min(min, col);
+        }
+        while (map.containsKey(min)) {
+            res.add(map.get(min++));
         }
         return res;
-    }
-    
-    private void dfs(TreeNode root, Map<Integer, List<int[]>> map, int col, int level) {
-        if (root == null) {
-            return;
-        }
-        if (!map.containsKey(col)) {
-            map.put(col, new LinkedList<int[]>());
-        }
-        map.get(col).add(new int[] {level, root.val});
-        //preOrder traversal, must be traverse left subtree first because for same level and col, we need the left one first 
-        dfs(root.left, map, col - 1, level + 1);
-        dfs(root.right, map, col + 1, level + 1);
     }
 }
