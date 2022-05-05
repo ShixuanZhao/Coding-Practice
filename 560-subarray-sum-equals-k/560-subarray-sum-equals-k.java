f166d4739198dca2d6758970c80ad634edc05dd6
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-05 02:19:38 -0700

    Time: 37 ms (48.55%), Space: 60.3 MB (54.80%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -1,18 +1,18 @@
 class Solution {
+    /*
+    interate the array, record the curSum, and store the prefixSum and its freq into HashMap
+    for each ele, check whether we have k - curSum in map
+    if have, update the res
+    */
     public int subarraySum(int[] nums, int k) {
-        //similiar to 2SUM, prefixSum
-        //------------
-        //     p1  T   p2
-        //p2 - p1 = target,找在map里面是否存在p2 - target
-        //map： key ：prefixSum, value:有几个值是这样的prefixSum
-        Map<Integer, Integer> map = new HashMap<>();
-        int prefixSum = 0;
-        map.put(0, 1);
         int res = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0;
         for (int num : nums) {
-            prefixSum = prefixSum + num;
-            res += map.getOrDefault(prefixSum - k, 0);
-            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+            sum += num;
+            res = res + map.getOrDefault(sum - k, 0);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
         return res;
     }
