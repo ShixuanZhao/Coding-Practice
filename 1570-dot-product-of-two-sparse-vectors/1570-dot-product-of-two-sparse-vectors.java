b3baaf9f9f4928e8e2ee6e1f1c28df2a9f110e41
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-05 11:11:18 -0700

    Time: 15 ms (36.83%), Space: 120.6 MB (31.52%) - LeetHub

--- 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.java
+++ 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.java
@@ -1,19 +1,55 @@
 class SparseVector {
     //use a HashMap to record the ele that is not zero
     //key is index, val is val
-    Map<Integer, Integer> map;
+//     Map<Integer, Integer> map;
+//     SparseVector(int[] nums) {
+//         map = new HashMap<>();
+//         for (int i = 0; i < nums.length; i++) {
+//             if (nums[i] != 0) {
+//                 map.put(i, nums[i]);
+//             }
+//         }
+//     }
+    
+// 	// Return the dotProduct of two sparse vectors
+//     public int dotProduct(SparseVector vec) {
+//         if (this.map.size() < vec.map.size()) {
+//             return dotProduct(this, vec);
+//         } else {
+//             return dotProduct(vec, this);
+//         }
+//     }
+    
+//     private int dotProduct(SparseVector small, SparseVector large) {
+//         int res = 0;
+//         for (int i : small.map.keySet()) {
+//             if (large.map.containsKey(i)) {
+//                 res += small.map.get(i) * large.map.get(i);
+//             }
+//         }
+//         return res;
+//     }
+    class Pair {
+        int key;
+        int val;
+        public Pair(int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+    //record the pair which key is not zero
+    List<Pair> list;
     SparseVector(int[] nums) {
-        map = new HashMap<>();
+        list = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                map.put(i, nums[i]);
+                list.add(new Pair(i, nums[i]));
             }
         }
     }
     
-	// Return the dotProduct of two sparse vectors
     public int dotProduct(SparseVector vec) {
-        if (this.map.size() < vec.map.size()) {
+        if (this.list.size() < vec.list.size()) {
             return dotProduct(this, vec);
         } else {
             return dotProduct(vec, this);
@@ -22,12 +58,30 @@ class SparseVector {
     
     private int dotProduct(SparseVector small, SparseVector large) {
         int res = 0;
-        for (int i : small.map.keySet()) {
-            if (large.map.containsKey(i)) {
-                res += small.map.get(i) * large.map.get(i);
-            }
+        for (Pair p : small.list) {
+            if (search(large.list, p.key) != -1) {
+                res += search(large.list, p.key) * p.val;
+            } 
         }
         return res;
+    }
+    
+    //search the val in arraylist which index is t
+    //if not found, return -1
+    private int search(List<Pair> list, int t) {
+        int left = 0;
+        int right = list.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (list.get(mid).key == t) {
+                return list.get(mid).val;
+            } else if (list.get(mid).key < t) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
     }
 }
 
