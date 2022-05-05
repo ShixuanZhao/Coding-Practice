21f58164f57947f3f6d4c46532a3122b8c93e91b
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-05 02:03:23 -0700

    Time: 1371 ms (5.00%), Space: 68.9 MB (15.64%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,38 +1,16 @@
 class Solution {
-    //M[i] whether can reach last position from index i
-    //scanning from right to left
+    /*
+    M[i] whether we can jump from index0 to index i 
+    M[i] = true if we can find a index j(j < i) M[j] = true && j + nums[j] >= i
+    else = false
+    M[0] = true;
+    */
     public boolean canJump(int[] nums) {
-//         //make a diff from leetcode746
-//         if (nums.length == 1) {
-//             return true;
-//         }
-//         int n = nums.length;
-//         boolean[] M = new boolean[n];
-//         M[n - 1] = true;
-//         for (int i = n - 2; i >= 0; i--) {
-//             if (i + nums[i] >= n - 1) {
-//                 M[i] = true;
-//             } else {
-//                 for (int j = 1; j <= nums[i]; j++) {
-//                     if (M[i + j]) {
-//                         M[i] = true;
-//                         break;
-//                     }
-//                 }
-//             }
-//         }
-//         return M[0];
-        
-        //M2:scanning from left to right, the difference with M1 is the definition of M[i]
-    //M[i] whether index i is reachable from index0
-        if (nums.length == 1) {
-            return true;
-        }
         int n = nums.length;
         boolean[] M = new boolean[n];
         M[0] = true;
         for (int i = 1; i < n; i++) {
-            for (int j = i - 1; j >= 0; j--) {
+            for (int j = 0; j < i; j++) {
                 if (M[j] && j + nums[j] >= i) {
                     M[i] = true;
                     break;
