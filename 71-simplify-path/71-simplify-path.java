bfa9326926a2657b449b9ab9d9297274a69ce42a
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-06 01:40:21 -0700

    Time: 5 ms (84.14%), Space: 43.7 MB (66.45%) - LeetHub

--- 71-simplify-path/71-simplify-path.java
+++ 71-simplify-path/71-simplify-path.java
@@ -1,4 +1,16 @@
 class Solution {
+    /*
+        use stack to store the filename that we want to return
+        stack:c  
+        "", a, ., b.., .., c
+        case1:skip when we meet "" || "."
+        case2:pop when we meet ".."
+        case3:push to stack when we meet filename
+        
+        /a/./b/../../c/
+                  i
+        iterate the ele in the stack
+    */
     public String simplifyPath(String path) {
         Deque<String> stack = new ArrayDeque<>();
         String[] data = path.split("/");
@@ -14,6 +26,9 @@ class Solution {
                 stack.offerFirst(s);
             }
         }
+        //corner case
+        //input:"/../"
+        //we should return "/" other than ""
         if (stack.isEmpty()) {
             return "/";
         }
@@ -25,6 +40,7 @@ class Solution {
         app/home/ reverse not right because file name is also be reversed
         */
         while (!stack.isEmpty()) {
+            //no tailing '/'
             sb.insert(0, stack.pollFirst());
             sb.insert(0, "/");
         }
