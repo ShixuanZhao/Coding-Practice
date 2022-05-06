fa16c52891515570ee74bd5850c95cb57c24d4e6
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-06 01:39:44 -0700

    Time: 6 ms (75.38%), Space: 43.6 MB (66.45%) - LeetHub

--- 71-simplify-path/71-simplify-path.java
+++ 71-simplify-path/71-simplify-path.java
@@ -1,40 +1,30 @@
 class Solution {
-    /*
-        use stack to store the filename that we want to return
-        stack:c  
-        "", a, ., b.., .., c
-        case1:skip when we meet "" || "."
-        case2:pop when we meet ".."
-        case3:push to stack when we meet filename
-        
-        /a/./b/../../c/
-                  i
-        iterate the ele in the stack
-    */
     public String simplifyPath(String path) {
         Deque<String> stack = new ArrayDeque<>();
-        String[] name  = path.split("/");
-        for (String s : name) {
-            if (s.isEmpty() || s.equals(".")) {
+        String[] data = path.split("/");
+        for (String s : data) {
+            if (s.equals("") || s.equals(".")) {
                 continue;
             } else if (s.equals("..")) {
                 if (!stack.isEmpty()) {
                     stack.pollFirst();
                 }
-                
             } else {
+                //filename
                 stack.offerFirst(s);
             }
         }
-        StringBuilder sb = new StringBuilder();
-        //corner case
-        //input:"/../"
-        //we should return "/" other than ""
         if (stack.isEmpty()) {
             return "/";
         }
+        StringBuilder sb = new StringBuilder();
+        /*
+        "/home/app/"
+        "/home/app"
+        stack:home, app
+        app/home/ reverse not right because file name is also be reversed
+        */
         while (!stack.isEmpty()) {
-            //no tailing '/'
             sb.insert(0, stack.pollFirst());
             sb.insert(0, "/");
         }
