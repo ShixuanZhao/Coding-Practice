b8f331ba69ebc3c49502e26f8bbf8b00b8eb61c4
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-06 21:28:43 -0700

    Time: 1 ms (96.24%), Space: 45.2 MB (58.17%) - LeetHub

---
+++ 272-closest-binary-search-tree-value-ii/272-closest-binary-search-tree-value-ii.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /*
+    1 2 3 4 5 k = 2
+        - -
+    */
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        List<Integer> res = new LinkedList<>();
+        dfs(root, target, k, res);
+        return res;
+    }
+    
+    private void dfs(TreeNode root, double target, int k, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left, target, k, res);
+        if (res.size() < k) {
+            res.add(root.val);
+        } else if (Math.abs(target - root.val) < Math.abs(target - res.get(0))){
+            res.remove(0);
+            res.add(root.val);
+        } else {
+            return;
+        }
+        dfs(root.right, target, k, res);
+    }
+}
