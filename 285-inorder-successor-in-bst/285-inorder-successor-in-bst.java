0801c76356566faa7d528b9b1e4c866878c5fbd5
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-07 02:25:10 -0700

    Time: 2 ms (91.37%), Space: 42.8 MB (98.70%) - LeetHub

--- 285-inorder-successor-in-bst/285-inorder-successor-in-bst.java
+++ 285-inorder-successor-in-bst/285-inorder-successor-in-bst.java
@@ -8,7 +8,7 @@
  * }
  */
 class Solution {
-    //WRONG
+    //WRONG must have global var cnt[]
 //     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
 //         TreeNode[] res = new TreeNode[1];
 //         inOrder(root, p, res);
@@ -27,25 +27,40 @@ class Solution {
 //         inOrder(root.right, p, res);
 //     }
     
+//     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+//         TreeNode[] res = new TreeNode[1];
+//         int[] cnt = new int[1];
+//         inOrder(root, p, res, cnt);
+//         return res[0];
+//     }
+    
+//     private void inOrder(TreeNode root, TreeNode p, TreeNode[] res, int[] cnt) {
+//         if (root == null) {
+//             return;
+//         }
+//         inOrder(root.left, p, res, cnt);
+//         if (cnt[0] == 0 && root.val > p.val) {
+//             res[0] = root;
+//             cnt[0]++;
+//             return;
+//         }
+//         inOrder(root.right, p, res, cnt);
+//     }
+    
+    //another recursion:top-down
     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
-        TreeNode[] res = new TreeNode[1];
-        int[] cnt = new int[1];
-        inOrder(root, p, res, cnt);
-        return res[0];
+        if (root == null) {
+            return null;
+        }
+        if (root.val <= p.val) {
+            return inorderSuccessor(root.right, p);
+        } else {
+            TreeNode res = inorderSuccessor(root.left, p);
+            //左边没有了，root就是该返回的
+            return res != null ? res : root;
+        }
     }
     
-    private void inOrder(TreeNode root, TreeNode p, TreeNode[] res, int[] cnt) {
-        if (root == null) {
-            return;
-        }
-        inOrder(root.left, p, res, cnt);
-        if (cnt[0] == 0 && root.val > p.val) {
-            res[0] = root;
-            cnt[0]++;
-            return;
-        }
-        inOrder(root.right, p, res, cnt);
-    }
     //like binary search
 //     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
 //     TreeNode succ = null;
