c72b4b322b16f8ab3a81539c8dd06bf83cfb3bca
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-07 23:05:38 -0700

    Time: 29 ms (76.17%), Space: 50.7 MB (45.29%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,23 +1,17 @@
 class Solution {
-//     Use Stack(s) to save adjacent duplicate chars and the corresponding occurrences, pop them out when the occurrences reach k;
-// Build String by the items remaining in Stack(s).
     class Node {
         char c;
         int cnt;
-        Node(char c) {
+        public Node(char c) {
             this.c = c;
             cnt = 1;
         }
     }
     public String removeDuplicates(String s, int k) {
-        int n = s.length();
-        if (k > n) {
-            return s;
-        }
         Deque<Node> stack = new ArrayDeque<>();
-        for (char ch : s.toCharArray()) {
-            if (stack.isEmpty() || ch != stack.peekFirst().c) {
-                stack.offerFirst(new Node(ch));
+        for (char c : s.toCharArray()) {
+            if(stack.isEmpty() || stack.peekFirst().c != c) {
+                stack.offerFirst(new Node(c));
             } else {
                 if (stack.peekFirst().cnt + 1 == k) {
                     stack.pollFirst();
@@ -28,10 +22,11 @@ class Solution {
         }
         StringBuilder sb = new StringBuilder();
         while (!stack.isEmpty()) {
-            Node node = stack.pollFirst();
-            int times = node.cnt;
-            for (int i = 0; i < times; i++) {
-                sb.append(node.c);
+            Node cur = stack.pollFirst();
+            char c = cur.c;
+            int cnt = cur.cnt;
+            for (int i = 0; i < cnt; i++) {
+                sb.append(c);
             }
         }
         return sb.reverse().toString();
