775cc4531918adea4be8cd4da9f7ea23fc5f6d18
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-08 05:42:13 -0700

    Time: 0 ms (100.00%), Space: 43.1 MB (7.90%) - LeetHub

--- 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.java
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.java
@@ -14,23 +14,50 @@
  * }
  */
 class Solution {
+//     public List<List<Integer>> findLeaves(TreeNode root) {
+//         List<List<Integer>> res = new ArrayList<>();
+//         getHeight(root, res);
+//         return res;
+//     }
+    
+//     private int getHeight(TreeNode root, List<List<Integer>> res) {
+//         if (root == null) {
+//             return -1;
+//         }
+//         int left = getHeight(root.left, res);
+//         int right = getHeight(root.right, res);
+//         int height = Math.max(left, right) + 1;
+//         if (height == res.size()) {
+//             res.add(new ArrayList<>());
+//         }
+//         res.get(height).add(root.val);
+//         return height;
+//     }
+    
     public List<List<Integer>> findLeaves(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
-        getHeight(root, res);
+        while (root != null) {
+            List<Integer> temp = new ArrayList<>();
+            //root = 
+            root = deleteNode(root, temp);
+            res.add(temp);
+        }
         return res;
     }
     
-    private int getHeight(TreeNode root, List<List<Integer>> res) {
+    //delete leave node, and return the root after deletion, and put the leaf node into temp list 
+    private TreeNode deleteNode(TreeNode root, List<Integer> temp) {
         if (root == null) {
-            return -1;
+            return root;
         }
-        int left = getHeight(root.left, res);
-        int right = getHeight(root.right, res);
-        int height = Math.max(left, right) + 1;
-        if (height == res.size()) {
-            res.add(new ArrayList<>());
+        TreeNode left = deleteNode(root.left, temp);
+        TreeNode right = deleteNode(root.right, temp);
+        if (root.left == null && root.right == null) {
+            temp.add(root.val);
+            return null;
         }
-        res.get(height).add(root.val);
-        return height;
+        root.left = left;
+        root.right = right;
+        return root;
     }
 }
