a407230a3ca02ef63384aa9a784e0dcc26fb825b
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-07 21:22:19 -0700

    Time: 3 ms (60.44%), Space: 52.8 MB (78.62%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.java
+++ 896-monotonic-array/896-monotonic-array.java
@@ -1,22 +1,18 @@
 class Solution {
     /*
-    nums = [1,2,2,3]
-    nums = [1,4,2,3]
-    isA:t
-    isD:t
-    is ==, we treat as ascending
     */
     public boolean isMonotonic(int[] nums) {
-        boolean hasA = true;
-        boolean hasD = true;
+        //at first set true is i,portant
+        boolean isIncreasing = true;
+        boolean isDecreasing = true;
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] >  nums[i - 1]) {
-                hasD = false;
-            } 
-           if (nums[i] <  nums[i - 1]) {
-                hasA = false;
-            } 
+            if (nums[i] > nums[i - 1]) {
+                isDecreasing = false;
+            }
+            if (nums[i] < nums[i - 1]) {
+                isIncreasing = false;
+            }
         }
-        return hasD || hasA;
+        return isIncreasing || isDecreasing;
     }
 }
