a903b2e9f52038dbdecef3d76a1f15e1c6a11e17
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-11 08:00:31 -0700

    Time: 107 ms (64.51%), Space: 124.2 MB (42.39%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.java
@@ -0,0 +1,43 @@
+class Solution {
+    //BFS, three cases for generating neighboors
+    public int minJumps(int[] arr) {
+        //use a map to record the all the index for the same value
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.putIfAbsent(arr[i], new ArrayList<>());
+            map.get(arr[i]).add(i);
+        }
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(arr.length - 1);
+        boolean[] visited = new boolean[arr.length];
+        visited[arr.length - 1] = true;
+        int step = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int j = 0; j < size; j++) {
+                int i = q.poll();
+                if (i == 0) {
+                    return step;
+                }
+                if (i + 1 < arr.length && !visited[i + 1]) {
+                    visited[i + 1] = true;
+                    q.offer(i + 1);
+                }
+                if (i - 1 >= 0 && !visited[i - 1]) {
+                    visited[i - 1] = true;
+                    q.offer(i - 1);
+                }
+                List<Integer> next = map.getOrDefault(arr[i], new ArrayList<>());
+                for (int cur : next) {
+                    if (!visited[cur]) {
+                        visited[cur] = true;
+                        q.offer(cur);
+                    }
+                }
+                next.clear();
+            }
+            step++;
+        }
+        return -1;
+    }
+}
