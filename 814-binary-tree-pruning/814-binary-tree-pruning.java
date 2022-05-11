5d7a9d89c76d4318e5decfd94656cab06c700edf
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-11 03:43:44 -0700

    Time: 0 ms (100.00%), Space: 39.9 MB (83.73%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.java
+++ 814-binary-tree-pruning/814-binary-tree-pruning.java
@@ -14,33 +14,17 @@
  * }
  */
 class Solution {
-//     public TreeNode pruneTree(TreeNode root) {
-//         if (root == null) {
-//             return root;
-//         }
-//         containOne(root);
-//         return root;
-//     }
-    
-//     private boolean containOne(TreeNode root) {
-//         if (root == null) {
-//             return false;
-//         }
-//         boolean left = containOne(root.left);
-//         boolean right = containOne(root.right);
-//         if (!left) {
-//             root.left = null;
-//         }
-//         if (!right) {
-//             root.right = null;
-//         }
-//         return root.val == 1 || left || right;
-//     }
-    // This is a kind of divide and conquer process. Beginning from the bottom, for null nodes we just return null. If the node is 1, or if there is any child of it is not null, we return itself, otherwise we return null. In this way, only when a node has two null children, and itself is also 0, we return null and the null will be assigned to its parent’s left/right, so we pruned the tree.
     public TreeNode pruneTree(TreeNode root) {
-        if (root == null) return null;
-        root.left = pruneTree(root.left);
-        root.right = pruneTree(root.right);
-        return (root.val == 1 || root.left != null || root.right != null) ? root : null;
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = pruneTree(root.left);
+        TreeNode right = pruneTree(root.right);
+        if (left == null && right == null && root.val != 1) {
+            return null;
+        }
+        root.left = left;
+        root.right = right;
+        return root;
     }
 }
