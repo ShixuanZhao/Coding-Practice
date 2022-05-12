c93d9de6defe1a5aa3bf73ff022139d6822075f3
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-12 00:27:07 -0700

    Time: 4 ms (66.45%), Space: 45 MB (60.58%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.java
+++ 946-validate-stack-sequences/946-validate-stack-sequences.java
@@ -1,11 +1,16 @@
 class Solution {
+    /*
+    stack:1   
+    [4,5,3,2,1]
+             i 
+    */
     public boolean validateStackSequences(int[] pushed, int[] popped) {
         //use stack to simulate
         Deque<Integer> stack = new ArrayDeque<>();
         int i = 0;
-        for (int p : pushed) {
-            stack.offerFirst(p);
-            while (!stack.isEmpty() && popped[i] == stack.peekFirst()) {
+        for (int cur : pushed) {
+            stack.offerFirst(cur);
+            while (!stack.isEmpty() && stack.peekFirst() == popped[i]) {
                 stack.pollFirst();
                 i++;
             }
