fc6c3b0416d2c2a8f8d61e4b9b972f73a8861d87
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-14 05:36:00 -0700

    Time: 0 ms (100.00%), Space: 41.7 MB (52.84%) - LeetHub

--- 369-plus-one-linked-list/369-plus-one-linked-list.java
+++ 369-plus-one-linked-list/369-plus-one-linked-list.java
@@ -9,41 +9,65 @@
  * }
  */
 class Solution {
-    public ListNode plusOne(ListNode head) {
-        head = reverse(head);
-        //use dummy node to avoid the corner case of onlt one node
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode x = dummy;
-        int carry = 1;
-        while (carry > 0 || x.next != null) {
-            if (x.next != null) {
-                x = x.next;
-                carry += x.val;
-                x.val = carry % 10;
-                carry /= 10;
-            }
-            else {
-                x.next = new ListNode(carry);
-                x = x.next;
-                carry = 0;
-            }
-        }
-        return reverse(dummy.next);
-    }
+//     public ListNode plusOne(ListNode head) {
+//         head = reverse(head);
+//         //use dummy node to avoid the corner case of onlt one node
+//         ListNode dummy = new ListNode(0);
+//         dummy.next = head;
+//         ListNode x = dummy;
+//         int carry = 1;
+//         while (carry > 0 || x.next != null) {
+//             if (x.next != null) {
+//                 x = x.next;
+//                 carry += x.val;
+//                 x.val = carry % 10;
+//                 carry /= 10;
+//             }
+//             else {
+//                 x.next = new ListNode(carry);
+//                 x = x.next;
+//                 carry = 0;
+//             }
+//         }
+//         return reverse(dummy.next);
+//     }
     
-    private ListNode reverse(ListNode head) {
-        if (head == null || head.next == null) {
+//     private ListNode reverse(ListNode head) {
+//         if (head == null || head.next == null) {
+//             return head;
+//         }
+//         ListNode cur = head;
+//         ListNode prev = null;
+//         while (cur != null) {
+//             ListNode next = cur.next;
+//             cur.next = prev;
+//             prev = cur;
+//             cur = next;
+//         }
+//         return prev;
+//     }
+    
+    //recursion
+    public ListNode plusOne(ListNode head) {
+        if (head == null) {
             return head;
         }
-        ListNode cur = head;
-        ListNode prev = null;
-        while (cur != null) {
-            ListNode next = cur.next;
-            cur.next = prev;
-            prev = cur;
-            cur = next;
+        if (addWithCarry(head) == 0) {
+            return head;
+        } else {
+            ListNode newNode = new ListNode(1);
+            newNode.next = head;
+            return newNode;
         }
-        return prev;
+    }
+    
+    //adding one from tail and return the carry
+    private int addWithCarry(ListNode head) {
+        if (head == null) {
+            return 1;
+        }
+        int res = head.val + addWithCarry(head.next);
+        head.val = res % 10;
+        return res / 10;
     }
 }
