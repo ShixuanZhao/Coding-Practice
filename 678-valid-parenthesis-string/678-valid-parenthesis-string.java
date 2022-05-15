dbc16ddefe399067ee98cd103d107eeb9624e9a2
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-15 04:29:46 -0700

    Time: 14 ms (5.11%), Space: 42.4 MB (13.00%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.java
@@ -34,30 +34,58 @@ class Solution {
 //     }
     
     //best solution:counting
+    // public boolean checkValidString(String s) {
+    //     int cmin = 0, cmax = 0; // open parentheses count in range [cmin, cmax]
+    //     for (char c : s.toCharArray()) {
+    //         if (c == '(') {
+    //             cmax++;
+    //             cmin++;
+    //         } else if (c == ')') {
+    //             cmax--;
+    //             cmin--;
+    //         } else if (c == '*') {
+    //             cmax++; // if `*` become `(` then openCount++
+    //             cmin--; // if `*` become `)` then openCount--
+    //             // if `*` become `` then nothing happens
+    //             // So openCount will be in new range [cmin-1, cmax+1]
+    //         }
+    //         if (cmax < 0) return false; // Currently, don't have enough open parentheses to match close parentheses-> Invalid
+    //                                     // For example: ())(
+    //         cmin = Math.max(cmin, 0);   // It's invalid if open parentheses count < 0 that's why cmin can't be negative
+    //     }
+    //     return cmin == 0; // Return true if can found `openCount == 0` in range [cmin, cmax]
+    // }
+
+    //DP
+    //case1:L()R  L:( or * R:) or * M[i][j] depend on M[i + 1][j - 1] 
+    //case2:()|()
+    //        k    M[i][k] && M[k + 1][j]
     public boolean checkValidString(String s) {
-        int cmin = 0;
-        int cmax = 0;
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                cmin++;
-                cmax++;
-            } else if (c == ')') {
-                cmin--;
-                cmax--;
-            } else {
-                //c == '*'
-                cmax++;
-                cmin--;
+        int n = s.length();
+        boolean[][] M = new boolean[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == '*') {
+                M[i][i] = true;
             }
-            //too many )
-            if (cmax < 0) {
-                return false;
-            }
-            //can choose to * to empty
-            if (cmin < 0) {
-                cmin = 0;
+            for (int j = i + 1; j < n; j++) {
+                //case1:
+                char left = s.charAt(i);
+                char right = s.charAt(j);
+                if (left != ')' && right != '(') {
+                    if (j - i < 2 || M[i + 1][j - 1]) {
+                        M[i][j] = true;
+                        continue;
+                    }
+                }
+                //enumerate k
+                for (int k = i; k < j; k++) {
+                    if (M[i][k] && M[k + 1][j]) {
+                        M[i][j] = true;
+                        break;
+                    }
+                }
             }
         }
-        return cmin == 0;
+        return M[0][n - 1];
     }
 }
