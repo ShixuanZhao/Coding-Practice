c2cf696bf05a065e706a97bd0b6c7c3f7b32546e
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-16 06:55:42 -0700

    Time: 28 ms (47.20%), Space: 45.4 MB (37.60%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,18 +1,17 @@
 class Solution {
+    //M[i]: the min coin to make up amount i
+    //M[i] = Min(M[j]) + 1 j = i - all the possible coins value, and we can reach j
     public int coinChange(int[] coins, int amount) {
-        //jump game similiar
-        //M[i]凑i的钱数最少需要的coin数量
         int[] M = new int[amount + 1];
         Arrays.fill(M, Integer.MAX_VALUE);
         M[0] = 0;
         for (int i = 1; i <= amount; i++) {
-            //右小段是一枚硬币
             for (int coin : coins) {
-                if (i >= coin && M[i - coin] != Integer.MAX_VALUE) {
+                if (i - coin >= 0 && M[i - coin] != Integer.MAX_VALUE) {
                     M[i] = Math.min(M[i], M[i - coin] + 1);
                 }
             }
         }
-        return M[amount] == Integer.MAX_VALUE ? -1 : M[amount];
+        return M[amount] != Integer.MAX_VALUE ? M[amount] : -1;
     }
 }
