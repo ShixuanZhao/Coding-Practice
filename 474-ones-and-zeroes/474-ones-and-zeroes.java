7877dc6fe0ded032a9df4c15dc4b224b63afabd1
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-16 03:56:07 -0700

    Time: 20 ms (96.78%), Space: 43 MB (62.79%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.java
+++ 474-ones-and-zeroes/474-ones-and-zeroes.java
@@ -1,24 +1,24 @@
 class Solution {
     public int findMaxForm(String[] strs, int m, int n) {
-        int[][] dp = new int[m + 1][n + 1]; //M[m][n]是我们剩余空间为m n的时候
-        //我们最多可以装多少个物品
+        //M[i][j]:the max size of subset of whole strs using at most m 0 and n 1
+        int[][] M = new int[m + 1][n + 1];
         for (String s : strs) {
-            int one = 0;
             int zero = 0;
+            int one = 0;
             for (char c : s.toCharArray()) {
-                if (c == '1') {
-                    one++;
-                } else {
+                if (c == '0') {
                     zero++;
+                } else {
+                    one++;
                 }
             }
-            //只要目前剩余的空间比这个物品的空间大 我们可以选择拿，牺牲空间得到count + 1
+            //M[i][j] = Math.max(M[i][j], M[i - zero][j - one] + 1)
             for (int i = m; i >= zero; i--) {
                 for (int j = n; j >= one; j--) {
-                    dp[i][j] = Math.max(dp[i][j], dp[i - zero][j - one] + 1);
+                    M[i][j] = Math.max(M[i][j], M[i - zero][j - one] + 1);
                 }
             }
         }
-        return dp[m][n];
+        return M[m][n];
     }
 }
