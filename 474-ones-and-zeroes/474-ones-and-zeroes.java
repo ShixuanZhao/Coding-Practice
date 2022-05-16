db3ddf9d158f61597a71021265f8e55de938036d
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-16 04:00:15 -0700

    Time: 33 ms (77.13%), Space: 42.3 MB (82.85%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.java
+++ 474-ones-and-zeroes/474-ones-and-zeroes.java
@@ -1,4 +1,7 @@
 class Solution {
+    /*
+    我们有一个背包，背包的容量为m个1和n个0。我们有一堆物品可以拿，每个物品空间为一些0和一些1.我们可以选择讲每个物品放入包中或者不放。求最后我们能最多放入多少个物品。
+    */
     public int findMaxForm(String[] strs, int m, int n) {
         //M[i][j]:the max size of subset of whole strs using at most m 0 and n 1
         int[][] M = new int[m + 1][n + 1];
@@ -13,6 +16,9 @@ class Solution {
                 }
             }
             //M[i][j] = Math.max(M[i][j], M[i - zero][j - one] + 1)
+            ////只要目前剩余的空间比这个物品的空间大 我们可以选择拿，牺牲空间得到count + 1
+            //注意遍历顺序，因为利用滚动数组牺牲了str轮数这个维度，左边相当于M[l][][],右边相当于M[l - 1][i][j]
+            //所以当前轮不应该依赖于当前轮的结果，而是依赖于上一轮的结果，所以空间容量从大到小遍历，不存在当前轮的依赖关系
             for (int i = m; i >= zero; i--) {
                 for (int j = n; j >= one; j--) {
                     M[i][j] = Math.max(M[i][j], M[i - zero][j - one] + 1);
