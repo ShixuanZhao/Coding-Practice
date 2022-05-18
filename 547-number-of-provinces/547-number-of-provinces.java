7eb270c0e1806eb2da824b1d1f3a03d28953a498
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-18 00:24:58 -0700

    Time: 1 ms (97.50%), Space: 54.5 MB (18.31%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.java
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -1,24 +1,35 @@
 class Solution {
+    /*
+    adjancency matrix
+    isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+    110
+    110
+    001
+    第一行：1和1连着，1和2连着，1和3不连
+    第二行：2和1连着，2和2连着，2和3不连
+    */
     public int findCircleNum(int[][] isConnected) {
+        //number of city
         int n = isConnected.length;
         boolean[] visited = new boolean[n];
         int res = 0;
-        //for each city
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
                 res++;
-                dfs(i, isConnected, visited);
+                dfs(isConnected, visited, i);
             }
         }
         return res;
     }
     
-    private void dfs(int i, int[][] isConnected, boolean[] visited) {
+    //mark all connected city as visited
+    //i is the start city
+    private void dfs(int[][] isConnected, boolean[] visited, int i) {
         visited[i] = true;
-        //mark nei
+        //all other city
         for (int j = 0; j < isConnected.length; j++) {
             if (!visited[j] && isConnected[i][j] == 1) {
-                dfs(j, isConnected, visited);
+                dfs(isConnected, visited, j);
             }
         }
     }
