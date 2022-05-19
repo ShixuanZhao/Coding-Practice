24993cfa3f70bcfad5bd513915a1c188dfbd5b2d
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-19 09:40:54 -0700

    Time: 3 ms (36.35%), Space: 41.5 MB (32.27%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -1,20 +1,19 @@
 class Solution {
-    //The idea is to use one hash set to record sum of every digit square of every number occurred. Once the current sum cannot be added to set, return false; once the current sum equals 1, return true;
     public boolean isHappy(int n) {
         Set<Integer> set = new HashSet<>();
+        set.add(n);
         while (true) {
-            if (!set.add(n)) {
+            int cur = 0;
+            for (int i = n; i > 0; i /= 10) {
+                cur += (i % 10) * (i % 10);
+            }
+            if (cur == 1) {
+                return true;
+            }
+            if (!set.add(cur)) {
                 return false;
             }
-            int squareSum = 0;
-            for (int i = n; i != 0; i /= 10) {
-                squareSum += (i % 10) * (i % 10);
-            }
-            if (squareSum == 1) {
-                return true;
-            } else {
-                n = squareSum;
-            }
+            n = cur;
         }
     }
 }
