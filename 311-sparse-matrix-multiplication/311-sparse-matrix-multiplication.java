28604c70ccd5d67ed04e41bc800340d64ef8664d
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-18 20:34:53 -0700

    Time: 5 ms (19.72%), Space: 44.7 MB (50.09%) - LeetHub

---
+++ 311-sparse-matrix-multiplication/311-sparse-matrix-multiplication.java
@@ -0,0 +1,22 @@
+class Solution {
+    /*
+    For brute force solution, for each C[ i ] [ j ], it uses C[ i ] [ j ] += A[ i ] [ k ] * B[ k ] [ j ] where k = [ 0, n].Note: even A[ i ] [ k ] or B[ k ] [ j ] is 0, the multiplication is still executed.
+
+For the above smart solution, if A[ i ] [ k ] == 0 or B[ k ] [ j ] == 0, it just skip the multiplication . This is achieved by moving for-loop" for ( k = 0; k < n; k++ ) " from inner-most loop to middle loop, so that we can use if-statement to tell whether A[ i ] [ k ] == 0 or B[ k ] [ j ] == 0. This is really smart.
+    */
+    //brute force
+    public int[][] multiply(int[][] mat1, int[][] mat2) {
+        int m = mat1.length;
+        int k = mat1[0].length;
+        int n = mat2[0].length;
+        int[][] res = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int temp = 0; temp < k; temp++) {
+                    res[i][j] += mat1[i][temp] * mat2[temp][j];
+                }
+            }
+        }
+        return res;
+    }
+}
