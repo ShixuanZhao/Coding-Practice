7bd4a01afa18412e40fc3ac1f193a5d678b97323
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-18 20:44:22 -0700

    Time: 1 ms (66.23%), Space: 45.5 MB (7.61%) - LeetHub

--- 311-sparse-matrix-multiplication/311-sparse-matrix-multiplication.java
+++ 311-sparse-matrix-multiplication/311-sparse-matrix-multiplication.java
@@ -5,18 +5,37 @@ class Solution {
 For the above smart solution, if A[ i ] [ k ] == 0 or B[ k ] [ j ] == 0, it just skip the multiplication . This is achieved by moving for-loop" for ( k = 0; k < n; k++ ) " from inner-most loop to middle loop, so that we can use if-statement to tell whether A[ i ] [ k ] == 0 or B[ k ] [ j ] == 0. This is really smart.
     */
     //brute force
-    public int[][] multiply(int[][] mat1, int[][] mat2) {
-        int m = mat1.length;
-        int k = mat1[0].length;
-        int n = mat2[0].length;
-        int[][] res = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int temp = 0; temp < k; temp++) {
-                    res[i][j] += mat1[i][temp] * mat2[temp][j];
-                }
-            }
-        }
-        return res;
-    }
+    // public int[][] multiply(int[][] mat1, int[][] mat2) {
+    //     int m = mat1.length;
+    //     int k = mat1[0].length;
+    //     int n = mat2[0].length;
+    //     int[][] res = new int[m][n];
+    //     for (int i = 0; i < m; i++) {
+    //         for (int j = 0; j < n; j++) {
+    //             for (int temp = 0; temp < k; temp++) {
+    //                 res[i][j] += mat1[i][temp] * mat2[temp][j];
+    //             }
+    //         }
+    //     }
+    //     return res;
+    // }
+    //smarter
+     public int[][] multiply(int[][] mat1, int[][] mat2) {
+         int m = mat1.length;
+         int k = mat1[0].length;
+         int n = mat2[0].length;
+         int[][] res = new int[m][n];
+         for (int i = 0; i < m; i++) {
+             for (int k1 = 0; k1 < k; k1++) {
+                 if (mat1[i][k1] != 0) {
+                     for (int j = 0; j < n; j++) {
+                         if (mat2[k1][j] != 0) {
+                             res[i][j] += mat1[i][k1] * mat2[k1][j];
+                         }
+                     }
+                 }
+             }
+         }
+         return res;
+     }
 }
