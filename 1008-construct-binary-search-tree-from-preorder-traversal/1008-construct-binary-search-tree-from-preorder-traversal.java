0ca3b039f270df5702a3cf8a6bf0ae7e8f180b28
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-20 04:33:01 -0700

    Time: 0 ms (100.00%), Space: 41.7 MB (66.44%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -14,22 +14,42 @@
  * }
  */
 class Solution {
+//     public TreeNode bstFromPreorder(int[] preorder) {
+//         return helper(preorder, 0, preorder.length - 1);
+//     }
+    
+//     private TreeNode helper(int[] preorder, int left, int right) {
+//         if (left > right) {
+//             return null;
+//         }
+//         TreeNode root = new TreeNode(preorder[left]);
+//         int index = left + 1;
+//         //boundary index == right is ok, means all left node no right node
+//         while (index <= right && preorder[index] < preorder[left]) {
+//             index++;
+//         }
+//         root.left = helper(preorder, left + 1, index - 1);
+//         root.right = helper(preorder, index, right);
+//         return root;
+//     }
+    int index = 0;
     public TreeNode bstFromPreorder(int[] preorder) {
-        return helper(preorder, 0, preorder.length - 1);
+        return build(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
     
-    private TreeNode helper(int[] preorder, int left, int right) {
-        if (left > right) {
+    private TreeNode build(int[] preorder, int low, int high) {
+        if (index == preorder.length) {
             return null;
         }
-        TreeNode root = new TreeNode(preorder[left]);
-        int index = left + 1;
-        //boundary index == right is ok, means all left node no right node
-        while (index <= right && preorder[index] < preorder[left]) {
-            index++;
+        int val = preorder[index];
+        if (val < low || val > high) {
+            return null;
         }
-        root.left = helper(preorder, left + 1, index - 1);
-        root.right = helper(preorder, index, right);
+        //index++ must be here
+        index++;
+        TreeNode root = new TreeNode(val);
+        root.left = build(preorder, low, val);
+        root.right = build(preorder, val, high);
         return root;
     }
 }
