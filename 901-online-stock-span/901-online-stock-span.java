969437a8315962206a040b4e205906e7471bb795
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-20 01:36:53 -0700

    Time: 41 ms (88.31%), Space: 74.9 MB (30.67%) - LeetHub

---
+++ 901-online-stock-span/901-online-stock-span.java
@@ -0,0 +1,34 @@
+class StockSpanner {
+    //maintain a descreasing stack
+    /*
+    initial res to 1
+    [100,80,60,70,60,75,85]
+                      i
+     stack:100 85 
+     1 1 1 2 1 4(1 + 1 + 2) 1 + 4 + 1 = 6
+                     60  70
+                     
+     so we also need to store the previous res in the stack
+    */
+    Deque<int[]> stack;
+    public StockSpanner() {
+        stack = new ArrayDeque<>();
+    }
+    
+    public int next(int price) {
+        //pair <price, res>
+        int res = 1;
+        while (!stack.isEmpty() && stack.peekFirst()[0] <= price) {
+            res += stack.peekFirst()[1];
+            stack.pollFirst();
+        }
+        stack.offerFirst(new int[] {price, res});
+        return res;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
