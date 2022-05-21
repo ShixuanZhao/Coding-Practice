0719606758b669d1b7ccb0e515802bd251414869
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-21 07:03:24 -0700

    Time: 63 ms (5.19%), Space: 42.5 MB (92.21%) - LeetHub

---
+++ 536-construct-binary-tree-from-string/536-construct-binary-tree-from-string.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode str2tree(String s) {
+        System.out.println(s);
+        if (s == null || s.length() == 0) {
+            return null;
+        }
+        //base case:only one number (5) leaf node
+        // if (s.charAt(0) == '(' && s.charAt(s.length() - 1) == ')') {
+        //     s = s.substring
+        // }
+        //find the root
+        TreeNode root = null;
+        int index = s.indexOf("(");
+        if (index == -1) {
+            root = new TreeNode(Integer.valueOf(s.substring(0, s.length())));
+            return root;
+        } else {
+            root = new TreeNode(Integer.valueOf(s.substring(0, index)));
+        }
+        int left_bound = index;
+        //parenthensis matching
+        int leftCnt = 0;
+        int i = index;
+        for (; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                leftCnt++;
+            }
+            if (s.charAt(i) == ')') {
+                leftCnt--;
+            }
+            if (leftCnt == 0) {
+                break;
+            }
+        }
+        root.left = str2tree(s.substring(left_bound + 1, i));
+        if (i != s.length() - 1) {
+            root.right = str2tree(s.substring(i + 2, s.length() - 1));
+        }
+        return root;
+    }
+}
