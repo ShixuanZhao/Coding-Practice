71c0a0043a8e19e5aadb05500910c7d6e52d96e2
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-22 11:23:03 -0700

    Time: 7 ms (81.62%), Space: 48.2 MB (51.85%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.java
+++ 735-asteroid-collision/735-asteroid-collision.java
@@ -1,28 +1,33 @@
 class Solution {
-//     overall, there are totally 4 scenarios will happen: 1.+ + 2.- - 3.+ - 4.- +
-// when collision happens: only 3 which is + -
-// use a stack to keep track of the previous and compare current value with previous ones
+    /*
+    stack:5 10
+    if positive, push into stack
+    if negative, case by case
+    pop: 5 -6 prev is positive and keep poping
+    if prev < 0, push
+    if prev > 0, cur < 0, prev == cur, pop once
+    */
     public int[] asteroidCollision(int[] asteroids) {
-        if (asteroids == null) {
-            return null;
-        } else if (asteroids.length <= 1) {
-            return asteroids;
-        }
-        Stack<Integer> stack = new Stack<>();
+        Deque<Integer> stack = new ArrayDeque<>();
         for (int cur : asteroids) {
-            if (cur > 0) { // previous one does not matter, no collision forever
-                stack.push(cur);
+            if (cur > 0) {
+                stack.offerFirst(cur);
             } else {
-                while (!stack.isEmpty() && stack.peek() > 0 && -cur > stack.peek()) { // destroy the previous positive one(s) 
-                    stack.pop();
+                while (!stack.isEmpty() && stack.peekFirst() > 0 && -cur > stack.peekFirst()) {
+                    stack.pollFirst();
                 }
-                if (stack.isEmpty() || stack.peek() < 0) {
-                    stack.push(cur);
-                } else if (stack.peek() == -cur) {
-                    stack.pop();
+                if (stack.isEmpty() || stack.peekFirst() < 0) {
+                    stack.offerFirst(cur);
+                } else if (stack.peekFirst() == -cur) {
+                    stack.pollFirst();
                 }
             }
         }
-        return stack.stream().mapToInt(i->i).toArray();
+        int[] res = new int[stack.size()];
+        int index = stack.size() - 1;
+        while (!stack.isEmpty()) {
+            res[index--] = stack.pollFirst();
+        }
+        return res;
     }
 }
