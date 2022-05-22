635f6f7655c3011bcd73f956641a5d65396b7c5d
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-22 11:46:32 -0700

    Time: 0 ms (100.00%), Space: 41.5 MB (10.09%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -1,21 +1,24 @@
 class Solution {
     public boolean divisorGame(int n) {
-        if (n == 1) {
-            return false;
-        }
-        boolean[] M = new boolean[n + 1];
-        M[1] = false;
-        M[2] = true;
-        //i - j 必输
-        //那么j的时候一定win
-        for (int i = 3; i <= n; i++) {
-            for (int j = 1; j < i; j++) {
-                //both players play optimally.
-                if (i % j == 0 && !M[i - j]) {
-                    M[i] = true;
-                }
-            }
-        }
-        return M[n];
+        return n % 2 == 0;
     }
+    // public boolean divisorGame(int n) {
+    //     if (n == 1) {
+    //         return false;
+    //     }
+    //     boolean[] M = new boolean[n + 1];
+    //     M[1] = false;
+    //     M[2] = true;
+    //     //i - j 必输
+    //     //那么j的时候一定win
+    //     for (int i = 3; i <= n; i++) {
+    //         for (int j = 1; j < i; j++) {
+    //             //both players play optimally.
+    //             if (i % j == 0 && !M[i - j]) {
+    //                 M[i] = true;
+    //             }
+    //         }
+    //     }
+    //     return M[n];
+    // }
 }
