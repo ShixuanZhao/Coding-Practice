45d0c030ac4f8d5a73e56996e1de8179199cb33d
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-22 04:46:35 -0700

    Time: 0 ms (100.00%), Space: 40.8 MB (60.29%) - LeetHub

---
+++ 780-reaching-points/780-reaching-points.java
@@ -0,0 +1,16 @@
+class Solution {
+    /*
+    sx = 1, sy = 1, tx = 3, ty = 5
+    (3, 5) (3, 2) (1, 2) (1, 1)
+    	 It looks like a tree, if you know one node, 
+	 it's always easy to get the root because you have exact one way to get the parent node.
+    */
+    public boolean reachingPoints(int sx, int sy, int tx, int ty) {
+        while (sx < tx && sy < ty)
+            if (tx < ty) ty %= tx;
+            else tx %= ty;
+        //check if we reduce target points to (x, y+kx) or (x+ky, y)
+        return sx == tx && sy <= ty && (ty - sy) % sx == 0 ||
+               sy == ty && sx <= tx && (tx - sx) % sy == 0;
+    }
+}
