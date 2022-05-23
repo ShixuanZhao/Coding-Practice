88e9f3ab88101e641b779afd9465d490d282e975
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-23 07:48:55 -0700

    Time: 5 ms (71.72%), Space: 43.1 MB (30.42%) - LeetHub

--- 763-partition-labels/763-partition-labels.java
+++ 763-partition-labels/763-partition-labels.java
@@ -6,6 +6,13 @@ Eventually, if we reached a position that satisfied everybody in the bus at the 
 
 1 traverse the string record the last index of each char.
 2 using pointer to record end of the current sub string.
+
+first build lastIndex arr
+"ababcbacadefegdehijhklij"
+     i 
+ end: 7
+ end(the end of each part):
+ 
     */
     public List<Integer> partitionLabels(String s) {
         List<Integer> res = new ArrayList<>();
@@ -13,18 +20,16 @@ Eventually, if we reached a position that satisfied everybody in the bus at the 
             return res;
         }
         //record the last occurence position for each letter
-        int[] lastPos = new int[26];
-        int n = s.length();
-        for (int i = 0; i < n; i++) {
-            lastPos[s.charAt(i) - 'a'] = i;
+       int[] last = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            last[s.charAt(i) - 'a'] = i;
         }
-        //find the most further distance
         int start = 0;
         int end = 0;
-        for (int i = 0; i < n; i++) {
-            end = Math.max(end, lastPos[s.charAt(i) - 'a']);
+        for (int i = 0; i < s.length(); i++) {
+            end = Math.max(end, last[s.charAt(i) - 'a']);
             if (i == end) {
-                res.add(end -start + 1);
+                res.add(end - start + 1);
                 start = end + 1;
             }
         }
