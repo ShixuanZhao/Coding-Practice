314f02ede37ab17e22e1306405bd5a2be405e557
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-23 02:24:25 -0700

    Time: 5 ms (42.86%), Space: 40.8 MB (69.77%) - LeetHub

---
+++ 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.java
@@ -0,0 +1,41 @@
+class Solution {
+    /*
+    If negative: sort descending, excluding the sign.
+If positive: sort ascending
+Find first non-zero number (e.g. 000237)
+Swap it with the first number (e.g. 200037)
+    */
+    public long smallestNumber(long num) {
+        if(num == 0){
+            return 0;
+        }
+        boolean isNegative = num < 0;
+        num  = num < 0 ? num * -1 : num;
+        
+        char[] c = String.valueOf(num).toCharArray();
+        Arrays.sort(c);
+        /*
+        310->sorting:013
+                     ij
+        swap:103
+        */
+        if (!isNegative) {
+            //find the first non-zero
+            int i = 0;
+            while (i < c.length && c[i] == '0') {
+                i++;
+            }
+            char temp = c[0];
+            c[0] = c[i];
+            c[i] = temp;
+            //System.out.println(new String(c));
+            return Long.valueOf(new String(c));
+        } else {
+            //-7605 -> 0567 ->reverse and add"-"
+            StringBuilder sb = new StringBuilder(new String(c));
+            sb.reverse().insert(0, "-");
+            //System.out.println(sb.toString());
+            return Long.valueOf(sb.toString());
+        }
+    }
+}
