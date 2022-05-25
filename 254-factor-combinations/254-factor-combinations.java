2f5d13414d5e7b222e1027c23978b23000e16fc0
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-25 07:23:32 -0700

    Time: 6 ms (78.15%), Space: 52.9 MB (69.23%) - LeetHub

--- 254-factor-combinations/254-factor-combinations.java
+++ 254-factor-combinations/254-factor-combinations.java
@@ -6,6 +6,7 @@ class Solution {
         return res;
     }
     
+    //去重2点：1，i * i <= n 2.每次从begin开始，前面选择的就不再选择了
     private void dfs(int n, int begin, List<List<Integer>> res, List<Integer> cur) {
         //make choice. in order to avoid duplication, we impose the start point
         //we need to begin variable
@@ -16,6 +17,7 @@ class Solution {
                 res.add(new ArrayList<>(cur));
                 cur.remove(cur.size() - 1);
                 //i because we can use duplicate number
+                //把大的n /i继续拆
                 dfs(n / i, i, res, cur);
                 cur.remove(cur.size() - 1);
             }
