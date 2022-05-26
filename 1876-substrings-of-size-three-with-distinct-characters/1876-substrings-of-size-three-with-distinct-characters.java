a811dd25342c858ea5733a557fd15cc09decebd5
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-26 09:21:00 -0700

    Time: 8 ms (12.16%), Space: 41.8 MB (73.88%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -16,26 +16,49 @@ class Solution {
     //     return res;
     // }
     
-   public int countGoodSubstrings(String s) {
-       int[] map = new int[26];
-       int n = s.length();
-       int dup = 0;
-       int res = 0;
-       for (int i = 0; i < n; i++) {
-           //must be ==2 not > 1, because multiple duplicate number, we will only add dup once
-           if (++map[s.charAt(i) - 'a'] == 2) {
-               dup++;
-           }
-           if (i < 2) {
-               continue;
-           }
-           if (dup == 0) {
-               res++;
-           }
-           if (--map[s.charAt(i - 2) - 'a'] == 1) {
-               dup--;
-           }
-       }
-       return res;
-   }
+   // public int countGoodSubstrings(String s) {
+   //     int[] map = new int[26];
+   //     int n = s.length();
+   //     int dup = 0;
+   //     int res = 0;
+   //     for (int i = 0; i < n; i++) {
+   //         //must be ==2 not > 1, because multiple duplicate number, we will only add dup once
+   //         if (++map[s.charAt(i) - 'a'] == 2) {
+   //             dup++;
+   //         }
+   //         if (i < 2) {
+   //             continue;
+   //         }
+   //         if (dup == 0) {
+   //             res++;
+   //         }
+   //         if (--map[s.charAt(i - 2) - 'a'] == 1) {
+   //             dup--;
+   //         }
+   //     }
+   //     return res;
+   // }
+    
+    public int countGoodSubstrings(String s) {
+        int n = s.length();
+        Map<Character, Integer> map = new HashMap<>();
+        // for (int i = 0; i < 2; i++) {
+        //     map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        // }
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            if (i < 2) {
+                continue;
+            }
+            if (map.size() == 3) {
+                res++;
+            }
+            map.put(s.charAt(i - 2), map.get(s.charAt(i - 2)) - 1);
+            if (map.get(s.charAt(i - 2)) == 0) {
+                map.remove(s.charAt(i - 2));
+            }
+        }
+        return res;
+    }
 }
