1730115f146cf80fedefa38736aa7d68c58f4e68
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-26 02:20:16 -0700

    Time: 5 ms (55.26%), Space: 41.8 MB (73.28%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.java
+++ 264-ugly-number-ii/264-ugly-number-ii.java
@@ -1,21 +1,10 @@
 class Solution {
-//     The ugly-number sequence is 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, …
-// because every number can only be divided by 2, 3, 5, one way to look at the sequence is to split the sequence to three groups as below:
-//1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15
-    //NOTICE 1 2 3 are not 连续的，他们来源于我们的DP，表示前面的丑数
-// (1) 1×2, 2×2, 3×2, 4×2, 5×2, …
-//             i
-// (2) 1×3, 2×3, 3×3, 4×3, 5×3, …
-//             j
-// (3) 1×5, 2×5, 3×5, 4×5, 5×5, …(not have 7 * 5 because 7 is not ugly number)
-//        k
-//    dp[]:1,2 3
-//    maintain 3 var represent the val that i j k point to
-// We can find that every subsequence is the ugly-sequence itself (1, 2, 3, 4, 5, …) multiply 2, 3, 5.
-
-// Then we use similar merge method as merge sort, to get every ugly number from the three subsequence.
-
-// Every step we choose the smallest one, and move one step after,including nums with same value.
+    /*
+    dp[i]:1 2 3 4 5 6 8
+    for2: 2 4 6 8 10
+    for 4:3 8
+    for 4:5 10
+    */
     public int nthUglyNumber(int n) {
         int[] M = new int[n];
         //the index+1 th ugly number
