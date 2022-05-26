6028840da6d81bb5a8d21d783be81cf5af2a8b44
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-26 02:46:25 -0700

    Time: 3 ms (85.46%), Space: 41.5 MB (84.23%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.java
+++ 264-ugly-number-ii/264-ugly-number-ii.java
@@ -1,26 +1,23 @@
 class Solution {
     /*
     dp[i]:1 2 3 4 5 6 8
-    for2: 2 4 6 8 10
-    for 4:3 8
-    for 4:5 10
+    for 2:2 4 6 8 10
+    for 4:3 8 16
+    for 5:5 10
     */
     public int nthUglyNumber(int n) {
         int[] M = new int[n];
-        //the index+1 th ugly number
-        M[0] = 1;
         int index2 = 0;
         int index3 = 0;
         int index5 = 0;
         int val2 = 2;
         int val3 = 3;
         int val5 = 5;
+        M[0] = 1;
         for (int i = 1; i < n; i++) {
-            int min = Math.min(Math.min(val2, val3), val5);
+            int min = Math.min(val2, Math.min(val3, val5));
             M[i] = min;
             if (min == val2) {
-                //!!important, index must plus first
-                //update the index, and update the next ugly number that need to be compare
                 val2 = 2 * M[++index2];
             }
             if (min == val3) {
