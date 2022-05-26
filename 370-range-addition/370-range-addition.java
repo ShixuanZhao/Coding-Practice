20db515cba43dcc42f0d2e5950c5f25552cf07e2
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-05-25 21:45:46 -0700

    Time: 2 ms (82.90%), Space: 56.1 MB (17.49%) - LeetHub

--- 370-range-addition/370-range-addition.java
+++ 370-range-addition/370-range-addition.java
@@ -9,21 +9,23 @@ class Solution {
     */
     public int[] getModifiedArray(int length, int[][] updates) {
         int[] res = new int[length];
+        //lazy computation,在开始和结束的后一个位置加上num，再run rolling sum， T = O(update.length + length)
+        //Update only the first and end element is sufficient.
         for (int[] update : updates) {
+            int value = update[2];
             int start = update[0];
             int end = update[1];
-            //+=
-            res[start] += update[2];
-            if (end + 1 < length) {
-                res[end + 1] -= update[2];
+            res[start] += value;
+            //到最后了，说明全部都包括了，所以我们就不需要再开一个减法区域了，因为后面没有数字了
+            if (end < length - 1) {
+                res[end + 1] -= value;
             }
         }
-        //prefixSum
         int sum = 0;
         for (int i = 0; i < length; i++) {
-            res[i] = sum + res[i];
-            sum = res[i];
+            sum += res[i];
+            res[i] = sum;
         }
-        return res;
+       return res; 
     }
 }
