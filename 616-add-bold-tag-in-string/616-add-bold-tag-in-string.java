9ac56986a8b212feba80ba301c70db58a6455390
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-06-02 01:05:01 -0700

    Time: 8 ms (87.84%), Space: 44.2 MB (65.59%) - LeetHub

--- 616-add-bold-tag-in-string/616-add-bold-tag-in-string.java
+++ 616-add-bold-tag-in-string/616-add-bold-tag-in-string.java
@@ -3,42 +3,27 @@ class Solution {
     "abcxyz123abc", words = ["abc","123"]
     */
     public String addBoldTag(String s, String[] words) {
-        //use a boolean[] to mark the char that need to be bold
         int n = s.length();
         boolean[] bold = new boolean[n];
-        //the start index of word in s
         for (String word : words) {
             int start = 0;
-            // while (s.indexOf(word, start) != -1) {
-            //     int index = s.indexOf(word, start);
-            //     int end = index + word.length();
-            //     for (int i = index; i < end; i++) {
-            //         bold[i] = true;
-            //     }
-            //     start++;
-            // }
-            while (start >= 0) {
-                start = s.indexOf(word, start);
-                if (start == -1) {
-                    break;
-                }
-                int end = start + word.length();
-                for (int i = start; i < end; i++) {
+            while (s.indexOf(word, start) != -1) {
+                int index = s.indexOf(word, start);
+                //not include
+                int end = index + word.length();
+                for (int i = index; i < end; i++) {
                     bold[i] = true;
                 }
-                start++;
+                start = index + 1;
             }
         }
-        //bold like this[111000111]
-        //add <b> and </b>
+        //[11100110]
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            //add open tag before the char in s, [00<b>1...]
             if (bold[i] && (i == 0 || !bold[i - 1])) {
                 sb.append("<b>");
             }
             sb.append(s.charAt(i));
-            //add close tag after the cur char in s [11</b>0...]
             if (bold[i] && (i == n - 1 || !bold[i + 1])) {
                 sb.append("</b>");
             }
