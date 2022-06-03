4c879748a095bf599b14eef431ac69b5835031a3
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-06-03 02:04:02 -0700

    Time: 7 ms (49.57%), Space: 44.7 MB (46.31%) - LeetHub

---
+++ 988-smallest-string-starting-from-leaf/988-smallest-string-starting-from-leaf.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<String> res = new ArrayList<>();
+    public String smallestFromLeaf(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        dfs(root, sb);
+        Collections.sort(res);
+        return res.get(0);
+    }
+    
+    private void dfs(TreeNode root, StringBuilder sb) {
+        if (root == null) {
+            return;
+        }
+        char c = (char)(root.val + 'a');
+        sb.append(c);
+        if (root.left == null && root.right == null) {
+            //this is wrong because we do not want to change sb, we need it backtrack!!
+            //so we need to keep sb unchange
+            //String s = sb.reverse().toString();
+            String s = new StringBuilder(sb).reverse().toString();
+            res.add(s);
+        } else {
+            dfs(root.left, sb);
+            dfs(root.right, sb);
+        }
+        sb.setLength(sb.length() - 1);
+    }
+    
+}
