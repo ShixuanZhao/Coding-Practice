038236e452bcb8799158fbce71ff6f0731eceab3
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-06-03 23:27:00 -0700

    Time: 1 ms (50.31%), Space: 41.3 MB (64.50%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.java
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.java
@@ -1,17 +1,41 @@
 class Solution {
-    //D1 and D2 refer to main and skew diagonals respectively;
-//Use r == c and r + c == 2 to judge main and skew diagonals.
+    int[] row = new int[3];
+    int[] col = new int[3];
+    int dia = 0;
+    int antiDia = 0;
     public String tictactoe(int[][] moves) {
-        int[] aRow = new int[3], aCol = new int[3], bRow = new int[3], bCol = new int[3];
-        int  aD1 = 0, aD2 = 0, bD1 = 0, bD2 = 0;
-        for (int i = 0; i < moves.length; ++i) {
-            int r = moves[i][0], c = moves[i][1];
-            if (i % 2 == 1) {
-                if (++bRow[r] == 3 || ++bCol[c] == 3 || r == c && ++bD1 == 3 || r + c == 2 && ++bD2 == 3) return "B";
-            }else {
-                if (++aRow[r] == 3 || ++aCol[c] == 3 || r == c && ++aD1 == 3 || r + c == 2 && ++aD2 == 3) return "A";
+        for (int i = 0; i < moves.length; i++) {
+            if (i % 2 == 0) {
+                //A plays, add 1
+                row[moves[i][0]]++;
+                col[moves[i][1]]++;
+                if (moves[i][0] == moves[i][1]) {
+                    dia++;
+                }
+                if (moves[i][0] == 2 - moves[i][1]) {
+                    antiDia++;
+                }
+                if (row[moves[i][0]] == 3 || col[moves[i][1]] == 3 || dia == 3 || antiDia == 3) {
+                    return "A";
+                }
+            } else {
+                //B plays, minus 1
+                row[moves[i][0]]--;
+                col[moves[i][1]]--;
+                if (moves[i][0] == moves[i][1]) {
+                    dia--;
+                }
+                if (moves[i][0] == 2 - moves[i][1]) {
+                    antiDia--;
+                }
+                if (row[moves[i][0]] == -3 || col[moves[i][1]] == -3 || dia == -3 || antiDia == -3) {
+                    return "B";
+                }
             }
         }
-        return moves.length == 9 ? "Draw" : "Pending";        
+        if (moves.length < 9) {
+            return "Pending";
+        }
+        return "Draw";
     }
 }
