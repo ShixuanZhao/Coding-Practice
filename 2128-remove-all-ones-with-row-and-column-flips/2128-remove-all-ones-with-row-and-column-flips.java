8f75498418f340d740ea1ccea0ba36a782f59cbb
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-06-04 08:03:19 -0700

    Time: 2 ms (58.64%), Space: 66 MB (28.70%) - LeetHub

--- 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.java
+++ 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.java
@@ -2,6 +2,7 @@ class Solution {
     public boolean removeOnes(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
+        //Scan the first row and flip if required
         for (int j = 0; j < n; j++) {
             if (grid[0][j] == 1) {
                 for (int i = 0; i < m; i++) {
@@ -11,6 +12,10 @@ class Solution {
             }
         }
         //check whether other rows are same
+        // we are done with the first row
+        // now we can't filp columns again; that would mess up our first row
+        // we can only filp rows now onwards; 
+        // that means now following rows should have the same value in the whole row
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (grid[i][j] != grid[i][j - 1]) {
