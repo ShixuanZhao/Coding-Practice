dc8d91d49687e262be1539ebe548bda04161b260
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-06-08 22:06:45 +0800

    Time: 15 ms (8.92%), Space: 49.3 MB (10.52%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.java
+++ 261-graph-valid-tree/261-graph-valid-tree.java
@@ -1,97 +1,35 @@
 class Solution {
-//     public boolean validTree(int n, int[][] edges) {
-//         // initialize adjacency list
-//         List<List<Integer>> adjList = new ArrayList<List<Integer>>(n);
-        
-//         // initialize vertices
-//         for (int i = 0; i < n; i++)
-//             adjList.add(i, new ArrayList<Integer>());
-        
-//         // add edges    
-//         for (int i = 0; i < edges.length; i++) {
-//             int u = edges[i][0], v = edges[i][1];
-//             adjList.get(u).add(v);
-//             adjList.get(v).add(u);
-//         }
-//         Set<Integer> visited = new HashSet<>();
-//         dfs(adjList, 0, visited);
-//         return visited.size() == n && edges.length == n - 1;
-//     }
-    
-//     // check if an undirected graph has cycle started from vertex u
-//     //因为是无向图所以要记录一下前一个recursion访问的node
-//     //因为建图的时候是，u里面有v，v里面有u，记录一下dfs的方向因为dfs会触底反弹
-//     //如果是有向图判断有没有坏就不用记录prev node
-//     //模板，dfs with parent
-//     //记录每个node是从哪来的，如果他的爸爸和他的邻居是一个说明走了回头路，我们不管
-//     //但是如果二者不相同就说明有环
-//     //   1
-//     //  2 0
-//     //从1开始，2是从1来的他的邻居是0，这样就是有环
-//      private void dfs(List<List<Integer>> adjList, int i, Set<Integer> visited) {
-//          visited.add(i);
-//          for (int nei : adjList.get(i)) {
-//              if (!visited.contains(nei)) {
-//                  dfs(adjList, nei, visited);
-//              }
-//          }
-//      }
-    
     public boolean validTree(int n, int[][] edges) {
-        // initialize adjacency list
-        List<List<Integer>> adjList = new ArrayList<List<Integer>>(n);
-        
-        // initialize vertices
-        for (int i = 0; i < n; i++)
-            adjList.add(i, new ArrayList<Integer>());
-        
-        // add edges    
-        for (int i = 0; i < edges.length; i++) {
-            int u = edges[i][0], v = edges[i][1];
-            adjList.get(u).add(v);
-            adjList.get(v).add(u);
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int[] e : edges) {
+            map.putIfAbsent(e[0], new ArrayList<>());
+            map.get(e[0]).add(e[1]);
+            map.putIfAbsent(e[1], new ArrayList<>());
+            map.get(e[1]).add(e[0]);
         }
-        
         boolean[] visited = new boolean[n];
-        
-        // make sure there's no cycle
-        if (hasCycle(adjList, 0, visited, -1))
+        if (dfs(0, -1, visited, map)) {
             return false;
-        
-        // make sure all vertices are connected
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) 
-                return false;
         }
-        
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                return false;
+            }
+        }
         return true;
     }
     
-    // check if an undirected graph has cycle started from vertex u
-    //因为是无向图所以要记录一下前一个recursion访问的node
-    //因为建图的时候是，u里面有v，v里面有u，记录一下dfs的方向因为dfs会触底反弹
-    //如果是有向图判断有没有坏就不用记录prev node
-    //模板，dfs with parent
-    //记录每个node是从哪来的，如果他的爸爸和他的邻居是一个说明走了回头路，我们不管
-    //但是如果二者不相同就说明有环
-    //   1
-    //  2 0
-    //从1开始，2是从1来的他的邻居是0，这样就是有环
-     boolean hasCycle(List<List<Integer>> adjList, int u, boolean[] visited, int parent) {
-         //从u开始又回来了
-        if (visited[u]) {
+    //if has cycle return true
+    private boolean dfs(int cur, int prev, boolean[] visited, Map<Integer, List<Integer>> map) {
+        if (visited[cur]) {
             return true;
         }
-
-        visited[u] = true;
-
-        for(int v: adjList.get(u)){
-            //v != parent 不走回头路遍历
-            if(v != parent && hasCycle(adjList, v, visited, u)){
+        visited[cur] = true;
+        for (int nei : map.getOrDefault(cur, new ArrayList<>())) {
+            if (nei != prev && dfs(nei, cur, visited, map)) {
                 return true;
             }
         }
-
         return false;
     }
 }
