50d5e15ff668e7325ef3b32abb1532c928284bea
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-06-08 22:09:24 +0800

    Time: 13 ms (12.18%), Space: 48.8 MB (13.98%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.java
+++ 261-graph-valid-tree/261-graph-valid-tree.java
@@ -8,9 +8,11 @@ class Solution {
             map.get(e[1]).add(e[0]);
         }
         boolean[] visited = new boolean[n];
+        //// make sure there's no cycle
         if (dfs(0, -1, visited, map)) {
             return false;
         }
+        // make sure all vertices are connected
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
                 return false;
@@ -20,12 +22,14 @@ class Solution {
     }
     
     //if has cycle return true
+    ////因为是无向图所以要记录一下前一个recursion访问的node
     private boolean dfs(int cur, int prev, boolean[] visited, Map<Integer, List<Integer>> map) {
         if (visited[cur]) {
             return true;
         }
         visited[cur] = true;
         for (int nei : map.getOrDefault(cur, new ArrayList<>())) {
+            //prev node就不在generate了
             if (nei != prev && dfs(nei, cur, visited, map)) {
                 return true;
             }
