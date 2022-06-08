35512eae63675e198cd56407205b9a54c88dd1b6
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-06-08 22:23:08 +0800

    Time: 25 ms (96.69%), Space: 42.5 MB (77.54%) - LeetHub

--- 133-clone-graph/133-clone-graph.java
+++ 133-clone-graph/133-clone-graph.java
@@ -20,46 +20,47 @@ class Node {
 
 class Solution {
     //dfs
-//     public Node cloneGraph(Node node) {
-//         Map<Node, Node> map = new HashMap<>();
-//         return dfs(node, map);
-//     }
-    
-//     private Node dfs(Node node, Map<Node, Node> map) {
-//         if (node == null) {
-//             return node;
-//         }
-//         if (!map.containsKey(node)) {
-//             Node newNode = new Node(node.val);
-//             map.put(node, newNode);
-//             for (Node nei : node.neighbors) {
-//                 newNode.neighbors.add(dfs(nei, map));
-//             }
-//         }
-//         return map.get(node);
-//     }
-    
-    //bfs
     public Node cloneGraph(Node node) {
+        Map<Node, Node> map = new HashMap<>();
+        return dfs(node, map);
+    }
+    
+    private Node dfs(Node node, Map<Node, Node> map) {
         if (node == null) {
             return node;
         }
-        Map<Node, Node> map = new HashMap<>();
-        Queue<Node> q = new LinkedList<>();
-        q.offer(node);
+        if (map.containsKey(node)) {
+            return map.get(node);
+        }
         Node newNode = new Node(node.val);
         map.put(node, newNode);
-        while (!q.isEmpty()) {
-            Node cur = q.poll();
-            for (Node nei : cur.neighbors) {
-                if (!map.containsKey(nei)) {
-                    Node copied = new Node(nei.val);
-                    map.put(nei, copied);
-                    q.offer(nei);
-                }
-                map.get(cur).neighbors.add(map.get(nei));
-            }
+        for (Node nei : node.neighbors) {
+            newNode.neighbors.add(dfs(nei, map));
         }
         return newNode;
     }
+    
+    //bfs
+    // public Node cloneGraph(Node node) {
+    //     if (node == null) {
+    //         return node;
+    //     }
+    //     Map<Node, Node> map = new HashMap<>();
+    //     Queue<Node> q = new LinkedList<>();
+    //     q.offer(node);
+    //     Node newNode = new Node(node.val);
+    //     map.put(node, newNode);
+    //     while (!q.isEmpty()) {
+    //         Node cur = q.poll();
+    //         for (Node nei : cur.neighbors) {
+    //             if (!map.containsKey(nei)) {
+    //                 Node copied = new Node(nei.val);
+    //                 map.put(nei, copied);
+    //                 q.offer(nei);
+    //             }
+    //             map.get(cur).neighbors.add(map.get(nei));
+    //         }
+    //     }
+    //     return newNode;
+    // }
 }
