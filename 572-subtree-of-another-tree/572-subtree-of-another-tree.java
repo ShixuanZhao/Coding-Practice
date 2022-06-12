eae144714d54a8b640031db3391f5480413dd85c
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-06-12 23:10:37 +0800

    Time: 33 ms (5.05%), Space: 66.3 MB (5.82%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.java
@@ -15,26 +15,54 @@
  */
 class Solution {
     //check each root of subtree, whether it is the same as subRoot tree
+    //O(n)
+//     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+//         if (root == null) {
+//             return false;
+//         }
+//         if (isSame(root, subRoot)) {
+//             return true;
+//         }
+//         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+//     }
+    
+//     private boolean isSame(TreeNode root, TreeNode subRoot) {
+//         if (root == null && subRoot == null) {
+//             return true;
+//         }
+//         if (root == null || subRoot == null) {
+//             return false;
+//         }
+//         if (root.val != subRoot.val) {
+//             return false;
+//         }
+//         return isSame(root.left, subRoot.left) && isSame(root.right, subRoot.right);
+//     }
+    
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if (root == null) {
-            return false;
-        }
-        if (isSame(root, subRoot)) {
-            return true;
-        }
-        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        String s1 = getString(root);
+        String s2 = getString(subRoot);
+        return s1.indexOf(s2) == -1 ? false : true;
     }
     
-    private boolean isSame(TreeNode root, TreeNode subRoot) {
-        if (root == null && subRoot == null) {
-            return true;
+    //corner case: 12 2 return false
+    private String getString(TreeNode root) {
+        if (root == null) {
+            return "#";
         }
-        if (root == null || subRoot == null) {
-            return false;
-        }
-        if (root.val != subRoot.val) {
-            return false;
-        }
-        return isSame(root.left, subRoot.left) && isSame(root.right, subRoot.right);
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append("(");
+        sb.append(root.val);
+        sb.append(")");
+        
+        sb.append("(");
+        sb.append(getString(root.left));
+        sb.append(")");
+        
+        sb.append("(");
+        sb.append(getString(root.right));
+        sb.append(")");
+        return sb.toString();
     }
 }
