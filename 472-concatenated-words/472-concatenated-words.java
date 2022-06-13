f3639bb9922b21dd1f00b71b927aacf97cc15378
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-06-14 00:25:50 +0800

    Time: 118 ms (35.21%), Space: 73.6 MB (37.79%) - LeetHub

---
+++ 472-concatenated-words/472-concatenated-words.java
@@ -0,0 +1,46 @@
+class Solution {
+    /*
+    Do you still remember how did you solve this problem? https://leetcode.com/problems/word-break/
+If you do know one optimized solution for above question is using DP, 
+this problem is just one more step further. We iterate through each word and see if it can be formed by using other words.
+Of course it is also obvious that a word can only be formed by words shorter than it. 
+So we can first sort the input by length of each word, and only try to form one word by using words in front of it.
+    */
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        List<String> res = new ArrayList<>();
+        if (words == null || words.length <= 1) {
+            return res;
+        }
+        Set<String> prevSet = new HashSet<>();
+        Arrays.sort(words, (a, b) -> (a.length() - b.length()));
+        for (String s : words) {
+            //set里面都睡比他短的word
+            if (canBreak(s, prevSet)) {
+                res.add(s);
+            }
+            //再放进去
+            prevSet.add(s);
+        }
+        return res;
+    }
+    
+    private boolean canBreak(String s, Set<String> dict) {
+        //this case can not be ignored
+        if (dict.size() == 0) {
+            return false;
+        }
+        int n = s.length();
+        boolean[] M = new boolean[n + 1];
+        M[0] = true;
+        for (int i = 1; i <= n; i++) {
+            //j is the size of left big part
+            for (int j = 0; j < i; j++) {
+                if (M[j] && dict.contains(s.substring(j, i))) {
+                    M[i] = true;
+                    break;
+                }
+            }
+        }
+        return M[n];
+    }
+}
