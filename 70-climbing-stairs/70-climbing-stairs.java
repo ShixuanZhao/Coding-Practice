2cadc7416b5a7f75e85074dcab05cfedc84f4e0f
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-06-13 23:14:24 +0800

    Time: 0 ms (100.00%), Space: 40.4 MB (65.76%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,17 +1,35 @@
 class Solution {
     //left big part is the subproblem, right small part we can choose 1 step ot 2 step
+    // public int climbStairs(int n) {
+    //     //M[i] i number of stair
+    //     int[] M = new int[n + 1];
+    //     M[0] = 1;
+    //     M[1] = 1;
+    //     for (int i = 2; i <= n; i++) {
+    //         if (i - 1 >= 0) {
+    //             M[i] = M[i - 1];
+    //         }
+    //         if (i - 2 >= 0) {
+    //             M[i] += M[i - 2];
+    //         }
+    //     }
+    //     return M[n];
+    // }
+    
     public int climbStairs(int n) {
-        int[] M = new int[n + 1];
-        M[0] = 1;
-        M[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            if (i - 1 >= 0) {
-                M[i] = M[i - 1];
-            }
-            if (i - 2 >= 0) {
-                M[i] += M[i - 2];
-            }
+        if (n <= 0) {
+            return 1;
         }
-        return M[n];
+        //just need to keep two variable
+        //liear scan previous one step and previous two steps
+        int one = 1;
+        int two = 1;
+        int res = 1;
+        for (int i = 2; i <= n; i++) {
+            res = one + two;
+            one = two;
+            two = res;
+        }
+        return res;
     }
 }
