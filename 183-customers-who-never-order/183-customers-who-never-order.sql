aa367d6fa9af63ffa803f270302c23d5b4be62c1
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-06-18 22:30:54 +0800

    Time: 467 ms (69.88%), Space: 0B (100.00%) - LeetHub

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 
-#M1
+#M1 fastest
 #SELECT C.name AS "Customers" FROM Customers C LEFT JOIN Orders O ON C.id = O.customerId WHERE O.customerId is NULL 
 
 # M2:
