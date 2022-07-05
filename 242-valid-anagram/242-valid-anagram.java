c240ba125dc52f86badc845c85111dd677c9a88f
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-07-05 22:53:55 +0800

    Time: 3 ms (95.03%), Space: 43.6 MB (61.87%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,20 +1,48 @@
 class Solution {
+    /*
+    M1: sort and compare O(nlogn) O(n)
+    M2;one hashmap store ele and freq in s
+    boss sosb
+    b:1
+    s:1
+    
+    check whether the size == 0
+    clarification: whether there is only letter in the string
+    if there are " ", we cannot use int[]
+    */
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) {
             return false;
         }
-        Map<Character, Integer> map = new HashMap<>();
-        for (char c : s.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
+        //traverse s
+        int[] freq = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
         }
-        for (char c : t.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) - 1);
-            if (map.get(c) < 0) {
+        //traverse t
+        for (int i = 0; i < t.length(); i++) {
+            int cnt = --freq[t.charAt(i) - 'a'];
+            if (cnt < 0) {
                 return false;
-            } else if (map.get(c) == 0) {
-                map.remove(c);
             }
         }
-        return map.size() == 0;
+        return true;
     }
+    // public boolean isAnagram(String s, String t) {
+    //     Map<Character, Integer> map = new HashMap<>();
+    //     for (char c : s.toCharArray()) {
+    //         map.put(c, map.getOrDefault(c, 0) + 1);
+    //     }
+    //     for (char c : t.toCharArray()) {
+    //         if (!map.containsKey(c)) {
+    //             return false;
+    //         } else {
+    //             map.put(c, map.get(c) - 1);
+    //             if (map.get(c) == 0) {
+    //                 map.remove(c);
+    //             }
+    //         }
+    //     }
+    //     return map.size() == 0;
+    // }
 }
