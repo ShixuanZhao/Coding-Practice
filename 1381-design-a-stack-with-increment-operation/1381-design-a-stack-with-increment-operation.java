e2891e1f0ab86e1a1661ff92b717a323740d0fdd
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-07-10 20:46:19 +0800

    Time: 4 ms (100.00%), Space: 42.8 MB (93.98%) - LeetHub

--- 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.java
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.java
@@ -1,116 +1,39 @@
-// class CustomStack {
-//     int size;
-//     Deque<Integer> stack;
-//     public CustomStack(int maxSize) {
-//         size = maxSize;
-//         stack = new ArrayDeque<>();
-//     }
-    
-//     public void push(int x) {
-//         if (stack.size() < size) {
-//             stack.offerFirst(x);
-//         }
-//     }
-    
-//     public int pop() {
-//         if (stack.isEmpty()) {
-//             return -1;
-//         }
-//         return stack.pollFirst();
-//     }
-    
-//     public void increment(int k, int val) {
-//         Deque<Integer> temp = new ArrayDeque<>();
-//         while (!stack.isEmpty()) {
-//             temp.offerFirst(stack.pollFirst());
-//         }
-//         while (k-- > 0) {
-//             if (!temp.isEmpty()) {
-//                 stack.offerFirst(temp.pollFirst() + val);
-//             } else {
-//                 return;
-//             }
-//         }
-//         while (!temp.isEmpty()) {
-//             stack.offerFirst(temp.pollFirst());
-//         }
-//     }    
-// }
-
-// class CustomStack {
-//     List<Integer> list;
-//     int n;
-//     public CustomStack(int maxSize) {
-//         n = maxSize;
-//         list = new ArrayList<>();
-//     }
-    
-//     public void push(int x) {
-//         if (list.size() < n) {
-//             list.add(x);
-//         }
-//     }
-    
-//     public int pop() {
-//         if (list.size() <= 0) {
-//             return -1;
-//         }
-//         int res = list.get(list.size() - 1);
-//         list.remove(list.size() - 1);
-//         return res;
-//     }
-    
-//     public void increment(int k, int val) {
-//         for (int i = 0; i < Math.min(k, list.size()); i++) {
-//             list.set(i, list.get(i) + val);
-//         }
-//     }
-// }
-
-//Lazy increment
-/*
-Use an additional array to record the increment value.
-inc[i] means for all elements stack[0] ~ stack[i],
-we should plus inc[i] when popped from the stack.
-Then inc[i-1]+=inc[i],
-so that we can accumulate the increment inc[i]
-for the bottom elements and the following pops.
-*/
-
 class CustomStack {
-    int n;
-    //when the ele is poped,we do increment
-    //inc[i]; index i ele should be added inc[i] when it is poped
+    Deque<Integer> stack;
     int[] inc;
-    Stack<Integer> stack;
+    int maxSize;
     public CustomStack(int maxSize) {
-        n = maxSize;
-        inc = new int[n];
-        stack = new Stack<>();
+        stack = new ArrayDeque<>();
+        inc = new int[maxSize];
+        this.maxSize = maxSize;
     }
-
+    
     public void push(int x) {
-        if (stack.size() < n)
-            stack.push(x);
+        if (stack.size() < maxSize) {
+            stack.offerFirst(x);
+        }
     }
-
+    
     public int pop() {
-        int i = stack.size() - 1;
-        if (i < 0)
+        if (stack.isEmpty()) {
             return -1;
-        if (i > 0)
-            //update i - 1 position 
+        }
+        int i = stack.size() - 1;
+        if (i > 0) {
             inc[i - 1] += inc[i];
-        int res = stack.pop() + inc[i];
+        }
+        int res = stack.pollFirst() + inc[i];
         inc[i] = 0;
         return res;
     }
-
+    
     public void increment(int k, int val) {
-        int i = Math.min(k, stack.size()) - 1;
-        if (i >= 0)
-            //just mark in inc 
-            inc[i] += val;
+        int index = Math.min(k, stack.size()) - 1;
+        if (index < 0) {
+            return;
+        } else {
+            inc[index] += val;
+        }
     }
 }
 
