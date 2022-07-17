941d3067c33329d65a91b1e20baf153b8b43dd05
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-07-17 16:32:15 +0800

    Time: 14 ms (49.15%), Space: 43 MB (72.88%) - LeetHub

--- 439-ternary-expression-parser/439-ternary-expression-parser.java
+++ 439-ternary-expression-parser/439-ternary-expression-parser.java
@@ -1,30 +1,51 @@
 class Solution {
-    public String parseTernary(String expression) {
-        if (expression == null || expression.length() == 0) {
-            return "";
-        }
-        char[] arr = expression.toCharArray();
-        return dfs(arr, 0, expression.length() - 1) + "";
-    }
+//     public String parseTernary(String expression) {
+//         if (expression == null || expression.length() == 0) {
+//             return "";
+//         }
+//         char[] arr = expression.toCharArray();
+//         return dfs(arr, 0, expression.length() - 1) + "";
+//     }
     
-    private char dfs(char[] arr, int left, int right) {
-        if (left == right) {
-            return arr[left];
-        }
-        //find the most outer layer ? :
-        //the position of matched :
-        int i = left;
-        int cnt = 0;
-        for (;i < right; i++) {
-            if (arr[i] == '?') {
-                cnt++;
-            } else if (arr[i] == ':') {
-                cnt--;
-                if (cnt == 0) {
-                    break;
+//     private char dfs(char[] arr, int left, int right) {
+//         if (left == right) {
+//             return arr[left];
+//         }
+//         //find the most outer layer ? :
+//         //the position of matched :
+//         int i = left;
+//         int cnt = 0;
+//         for (;i < right; i++) {
+//             if (arr[i] == '?') {
+//                 cnt++;
+//             } else if (arr[i] == ':') {
+//                 cnt--;
+//                 if (cnt == 0) {
+//                     break;
+//                 }
+//             }
+//         }
+//         return arr[left] == 'T' ? dfs(arr, left + 2, i - 1) : dfs(arr, i + 1, right);
+//     }
+    
+    public String parseTernary(String expression) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = expression.length() - 1; i >= 0; i--) {
+            char c = expression.charAt(i);
+            if (!stack.isEmpty() && stack.peekFirst() == '?') {
+                stack.pollFirst();
+                char a = stack.pollFirst();
+                stack.pollFirst();
+                char b = stack.pollFirst();
+                if (c == 'T') {
+                    stack.offerFirst(a);
+                } else {
+                    stack.offerFirst(b);
                 }
+            } else {
+                stack.offerFirst(c);
             }
         }
-        return arr[left] == 'T' ? dfs(arr, left + 2, i - 1) : dfs(arr, i + 1, right);
+        return stack.peekFirst().toString();
     }
 }
