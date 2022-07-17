f57c402ff3998b646f3e04a15abe439b0480fb64
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-07-17 11:58:01 +0800

    Time: 2 ms (81.36%), Space: 43.1 MB (72.88%) - LeetHub

--- 439-ternary-expression-parser/439-ternary-expression-parser.java
+++ 439-ternary-expression-parser/439-ternary-expression-parser.java
@@ -1,56 +1,30 @@
-class Solution {   
-// Iterate the expression from tail, whenever encounter a character before '?', calculate the right value and push back to stack.
-// P.S. this code is guaranteed only if "the given expression is valid" base on the requirement.(number is one-digit)
-    // public String parseTernary(String expression) {
-    //     Deque<Character> stack = new ArrayDeque<>();
-    //     for (int i = expression.length() - 1; i >= 0; i--) {
-    //         char c = expression.charAt(i);
-    //         if (!stack.isEmpty() && stack.peekFirst() == '?') {
-    //             //pop "?"
-    //             stack.pollFirst();
-    //             char a = stack.pollFirst();
-    //             //pop ":"
-    //             stack.pollFirst();
-    //             char b = stack.pollFirst();
-    //             if (c == 'T') {
-    //                 stack.offerFirst(a);
-    //             } else {
-    //                 stack.offerFirst(b);
-    //             }
-    //         } else {
-    //             stack.offerFirst(c);
-    //         }
-    //     }
-    //     //convert char to string
-    //     return String.valueOf(stack.peekFirst());
-    // }
-    
+class Solution {
     public String parseTernary(String expression) {
-        if(expression == null || expression.length() == 0){
-            return expression;
+        if (expression == null || expression.length() == 0) {
+            return "";
         }
-        char[] exp = expression.toCharArray();
-        
-        return DFS(exp, 0, exp.length - 1) + "";
-        
+        char[] arr = expression.toCharArray();
+        return dfs(arr, 0, expression.length() - 1) + "";
     }
     
-    private char DFS(char[] exp, int left, int right) {
+    private char dfs(char[] arr, int left, int right) {
         if (left == right) {
-            return exp[left];
+            return arr[left];
         }
+        //find the most outer layer ? :
+        //the position of matched :
         int i = left;
         int cnt = 0;
-        for (; i <= right; i++) {
-            if (exp[i] == '?') {
+        for (;i < right; i++) {
+            if (arr[i] == '?') {
                 cnt++;
-            } else if (exp[i] == ':') {
+            } else if (arr[i] == ':') {
                 cnt--;
                 if (cnt == 0) {
                     break;
                 }
             }
         }
-        return exp[left] == 'T' ? DFS(exp, left + 2, i - 1) : DFS(exp, i + 1, right);
+        return arr[left] == 'T' ? dfs(arr, left + 2, i - 1) : dfs(arr, i + 1, right);
     }
 }
