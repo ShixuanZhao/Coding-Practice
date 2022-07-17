06ff68c98f0bf22c3f00101436c2067d9bf8e78c
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-07-17 21:19:48 +0800

    Time: 217 ms (34.10%), Space: 68.3 MB (60.21%) - LeetHub

--- 204-count-primes/204-count-primes.java
+++ 204-count-primes/204-count-primes.java
@@ -5,21 +5,25 @@ class Solution {
       T T F T F T F F 
     */
     public int countPrimes(int n) {
+        //         首先从 2 开始，我们知道 2 是一个素数，那么 2 × 2 = 4, 3 × 2 = 6, 4 × 2 = 8… 都不可能是素数了。
+
+// 然后我们发现 3 也是素数，那么 3 × 2 = 6, 3 × 3 = 9, 3 × 4 = 12… 也都不可能是素数了。
+        //排除法
         boolean[] isPrime = new boolean[n];
         Arrays.fill(isPrime, true);
-        for (int i = 2; i <= (int)Math.sqrt(n); i++) {
+        for (int i = 2; i <= (int) Math.sqrt(n); i++) {
             if (isPrime[i]) {
                 for (int j = 2 * i; j < n; j += i) {
                     isPrime[j] = false;
                 }
             }
         }
-        int res = 0;
+        int cnt = 0;
         for (int i = 2; i < n; i++) {
             if (isPrime[i]) {
-                res++;
+                cnt++;
             }
         }
-        return res;
+        return cnt;
     }
 }
