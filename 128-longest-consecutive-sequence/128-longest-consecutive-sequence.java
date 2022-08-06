ab65544a25a3ec9db660e0530a5d7ce049ac481f
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-08-06 20:16:14 +0800

    Time: 413 ms (31.17%), Space: 59.3 MB (93.05%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,4 +1,13 @@
 class Solution {
+     /*
+    [100,4,200,1,3,2]
+               i
+    check for each possible start, use a set to record all the ele
+    keep checking for cur + 1 in the set 
+    case1:cur - 1 is in the set || cur + 1 is not in the set, it means cur is  not the start
+        ignore and check next ele
+    case2:repeat and update the maxLen
+    */
     public int longestConsecutive(int[] nums) {
         if (nums.length == 0) {
             return 0;
