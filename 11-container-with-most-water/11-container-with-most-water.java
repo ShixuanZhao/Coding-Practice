4e8d293f83e31163227581e46813aaed0010e839
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-08-06 18:54:09 +0800

    Time: 4 ms (84.27%), Space: 52.6 MB (93.20%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -1,9 +1,17 @@
 class Solution {
+    /*
+    if A[i] < A[j] i++
+    else j--
+    [1,8,6,2,5,4,8,3,7]
+     i
+                     j
+    
+    */
     public int maxArea(int[] height) {
         int res = 0;
         int left = 0;
         int right = height.length - 1;
-        while (left <= right) {
+        while (left < right) {
             res = Math.max(res, (right - left) * Math.min(height[left], height[right]));
             //we move the lower bound, because we maybe can get higher height, which could get greater res
             if (height[left] < height[right]) {
