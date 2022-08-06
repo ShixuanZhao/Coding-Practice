43e6134fa307ed6c7b9dd0171ec20e8fb4af5019
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-08-06 19:26:13 +0800

    Time: 60 ms (6.07%), Space: 43.1 MB (74.75%) - LeetHub

--- 133-clone-graph/133-clone-graph.java
+++ 133-clone-graph/133-clone-graph.java
@@ -19,7 +19,6 @@ class Node {
 */
 
 class Solution {
-    //dfs
     public Node cloneGraph(Node node) {
         Map<Node, Node> map = new HashMap<>();
         return dfs(node, map);
@@ -27,7 +26,7 @@ class Solution {
     
     private Node dfs(Node node, Map<Node, Node> map) {
         if (node == null) {
-            return node;
+            return null;
         }
         if (map.containsKey(node)) {
             return map.get(node);
@@ -39,28 +38,4 @@ class Solution {
         }
         return newNode;
     }
-    
-    //bfs
-    // public Node cloneGraph(Node node) {
-    //     if (node == null) {
-    //         return node;
-    //     }
-    //     Map<Node, Node> map = new HashMap<>();
-    //     Queue<Node> q = new LinkedList<>();
-    //     q.offer(node);
-    //     Node newNode = new Node(node.val);
-    //     map.put(node, newNode);
-    //     while (!q.isEmpty()) {
-    //         Node cur = q.poll();
-    //         for (Node nei : cur.neighbors) {
-    //             if (!map.containsKey(nei)) {
-    //                 Node copied = new Node(nei.val);
-    //                 map.put(nei, copied);
-    //                 q.offer(nei);
-    //             }
-    //             map.get(cur).neighbors.add(map.get(nei));
-    //         }
-    //     }
-    //     return newNode;
-    // }
 }
