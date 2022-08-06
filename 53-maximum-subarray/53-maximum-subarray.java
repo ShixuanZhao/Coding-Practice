de61f30d553462a8e9408f57671a816ed910faf0
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-08-06 12:22:25 +0800

    Time: 4 ms (10.07%), Space: 83.8 MB (5.34%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,26 +1,26 @@
 class Solution {
     /*
-    M[i]:the max sum ending at index i
-    M[i] = M[i - 1] + nums[i] M[i - 1] > 0
-            nums[i]           else
-    keep a globalMax, update for each M[i]
-    [-2,1,-3,4,-1,2,1,-5,4]
+    max[i]: the max sun from index 0 to index i
+    if max[i - 1] < 0 max[i] = arr[i]
+    else max[i] = max[i - 1] + arr[i]
+    return globalMax
     */
     public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
         int n = nums.length;
-        if (n == 1) {
-            return nums[0];
-        }
-        int pre = nums[0];
-        int res = nums[0];
+        int[] M = new int[n];
+        M[0] = nums[0];
+        int max = nums[0];
         for (int i = 1; i < n; i++) {
-            if (pre > 0) {
-                pre = pre + nums[i];
+            if (M[i - 1] < 0) {
+                M[i] = nums[i];
             } else {
-                pre = nums[i];
+                M[i] = M[i - 1] + nums[i];
             }
-            res = Math.max(res, pre);
+            max = Math.max(max, M[i]);
         }
-        return res;
+        return max;
     }
 }
