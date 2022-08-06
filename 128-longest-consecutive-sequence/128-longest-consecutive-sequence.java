94c3f349206a66cf784c998e9b6611faeb4c04c3
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-08-06 20:15:21 +0800

    Time: 798 ms (8.60%), Space: 157.9 MB (13.44%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,33 +1,25 @@
 class Solution {
-    /*
-    [100,4,200,1,3,2]
-               i
-    check for each possible start, use a set to record all the ele
-    keep checking for cur + 1 in the set 
-    case1:cur - 1 is in the set || cur + 1 is not in the set, it means cur is  not the start
-        ignore and check next ele
-    case2:repeat and update the maxLen
-    */
     public int longestConsecutive(int[] nums) {
-        if (nums == null || nums.length == 0) {
+        if (nums.length == 0) {
             return 0;
         }
-        int res = 1;
+        int n = nums.length;
         Set<Integer> set = new HashSet<>();
         for (int num : nums) {
             set.add(num);
         }
-        for (int i = 0; i < nums.length; i++) {
-            int cur = nums[i];
-            if (set.contains(cur - 1) || !set.contains(cur + 1)) {
+        int res = 1;
+        for (int num : nums) {
+            if (set.contains(num - 1)) {
                 continue;
             }
-            int curLen = 1;
-            while (i < nums.length && set.contains(cur + 1)) {
+            int cur = num;
+            int cnt = 0;
+            while (set.contains(cur)) {
+                cnt++;
                 cur = cur + 1;
-                curLen++;
             }
-            res = Math.max(res, curLen);
+            res = Math.max(res, cnt);
         }
         return res;
     }
