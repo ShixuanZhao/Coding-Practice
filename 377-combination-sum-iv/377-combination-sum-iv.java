5def2325186d3d3567c845d0d505f18d36c0ad2f
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-08-07 01:58:05 +0800

    Time: 2 ms (65.18%), Space: 40.7 MB (83.97%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -1,14 +1,13 @@
 class Solution {
+    ////无限背包，每个元素可以重复用
+    //coin change,每个元素只能用一次是01背包
     public int combinationSum4(int[] nums, int target) {
+        int n = nums.length;
         int[] M = new int[target + 1];
         M[0] = 1;
-        /*
-        T = 3 (1, 2)  (2, 1)
-        */
-        for (int i = 1; i < M.length; i++) {
-            //j 是右小段，右小段是一个数字，这样算的话肯定会有重读其实是permutation问题
-            for (int j = 0; j < nums.length; j++) {
-                if (i - nums[j] >= 0) {
+        for (int i = 1; i <= target; i++) {
+            for (int j = 0; j < n; j++) {
+                if (nums[j] <= i) {
                     M[i] += M[i - nums[j]];
                 }
             }
