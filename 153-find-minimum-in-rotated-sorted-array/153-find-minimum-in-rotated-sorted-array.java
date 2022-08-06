630623d00df11ac359623f63c86fe14b360e42b1
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-08-06 18:12:33 +0800

    Time: 0 ms (100.00%), Space: 41.8 MB (95.17%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
@@ -1,49 +1,31 @@
 class Solution {
     /*
-      max
-    
-  1   / \
-    /    \
-          \ 
-           \  /  2
-            \/
-            min
-            begin > end
-    from max 直接到 Min 中间是没有点的，在1或者2两段
+    corner case: ascending arr return nums[0]
+    compare the nums[mid] with nums[left]
+    nums[mid] > nums[left] left = mid
+    else right = mid
+    0 1 2 3 4
+    3 4 5 1 2
+        l 
+          
+          r
+    return the smaller one
     */
     public int findMin(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return Integer.MAX_VALUE;
+        int n = nums.length;
+        if (n == 0 || nums[0] < nums[n - 1]) {
+            return nums[0];
         }
         int left = 0;
-        int right = nums.length - 1;
+        int right = n - 1;
         while (left < right - 1) {
             int mid = left + (right - left) / 2;
-            //must compare with last ele, because when the arr is ascending, this case can pass
-            if (nums[mid] < nums[right]) {
-                right = mid;
-            } else {
+            if (nums[mid] > nums[left]) {
                 left = mid;
+            } else {
+                right = mid;
             }
         }
-        return Math.min(nums[left], nums[right]);
+        return nums[left] < nums[right] ? nums[left] : nums[right];
     }
-    
-    //wrong when the array is rotated n times, this case the arr would be keep in ascending order
-    // public int findMin(int[] nums) {
-    //     if (nums == null || nums.length == 0) {
-    //         return Integer.MAX_VALUE;
-    //     }
-    //     int left = 0;
-    //     int right = nums.length - 1;
-    //     while (left < right - 1) {
-    //         int mid = left + (right - left) / 2;
-    //         if (nums[mid] > nums[0]) {
-    //             left = mid;
-    //         } else {
-    //             right = mid;
-    //         }
-    //     }
-    //     return Math.min(nums[left], nums[right]);
-    // }
 }
