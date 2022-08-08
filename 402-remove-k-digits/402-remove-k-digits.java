0b4b4bf6a2cde7071c6c4f8e1eea7f2574606cd8
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-08-08 22:28:15 +0800

    Time: 26 ms (77.29%), Space: 52.3 MB (24.19%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.java
+++ 402-remove-k-digits/402-remove-k-digits.java
@@ -1,39 +1,42 @@
 class Solution {
     /*
-        num = "1432219", k = 3
-        stack相当于是一个缓存我们暂时还不确定在缓存里的元素要不要
-        stack:1219
-        k:3 2 1 0
+    the length between num and k
+    0000  00034 k = 1 return 0 return 3
+    Use a stack, keep a increasing stack 
+    "1432219", k = 3
+          i
+    stack:1219
+    k:0
+    if, k > 0, after go through the string
+    remove ele from backward
+    remove the leading zero
+    corner case: after remove the leading zero, stack is empty, return "0"
     */
     public String removeKdigits(String num, int k) {
-        //生成最小递增序列，每次出现更小的时候我们就把前面更大的替换，可替换次数不超过k
-        //ascending stack
+        //suppose k <= num.length()
+        if (num == null || num.length() == 0) {
+            return "";
+        }
+        int n = num.length();
         Deque<Character> stack = new ArrayDeque<>();
-        for (char c : num.toCharArray()) {
-            while (!stack.isEmpty() && k > 0 && c < stack.peekFirst()) {
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && k > 0 && num.charAt(i) < stack.peekFirst()) {
                 stack.pollFirst();
                 k--;
             }
-            stack.offerFirst(c);
+            stack.offerFirst(num.charAt(i));
         }
-        //如果k还有剩余，stack从后往前remove
-        while (k > 0) {
-            stack.pollFirst();
+        while (k > 0 && !stack.isEmpty()) {
             k--;
+            stack.pollFirst();
         }
-        //corner case 0200 remove the leading zero
+        //remove the leading zero, so remove from Last side of stack
+        while (!stack.isEmpty() && stack.peekLast() == '0') {
+            stack.pollLast();
+        }
         StringBuilder sb = new StringBuilder();
-        //use a flag to represent the leading zero
-        boolean flag = true;
-        //this is a deque, this time we operate the other end of deque
         while (!stack.isEmpty()) {
-            while (!stack.isEmpty() && stack.peekLast() == '0' && flag) {
-                stack.pollLast();
-            } 
-            flag = false;
-            if (!stack.isEmpty()) {
-                sb.append(stack.pollLast());
-            }
+            sb.append(stack.pollLast());
         }
         return sb.length() == 0 ? "0" : sb.toString();
     }
