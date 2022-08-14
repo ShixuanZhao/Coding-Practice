3de74d60aed62e4a87d776dec0fa517143d2a8d7
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-08-14 11:32:22 +0800

    Time: 5 ms (62.92%), Space: 42.9 MB (13.17%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.java
+++ 767-reorganize-string/767-reorganize-string.java
@@ -15,44 +15,81 @@ a b a _ a _ b _ b // fill in "b" at position 6, 8, 1
 a b a c a _ b _ b // fill in "c" at position 3
 a b a c a d b d b // fill in "d" at position 5, 7
     */
+    // public String reorganizeString(String s) {
+    //     int[] hash = new int[26];
+    //     //construct hash array
+    //     for (char c : s.toCharArray()) {
+    //         hash[c - 'a']++;
+    //     }
+    //     //find the most letter and its freq
+    //     char letter = 'a';
+    //     int cnt = 0;
+    //     for (int i = 0; i < 26; i++) {
+    //         if (hash[i] > cnt) {
+    //             letter = (char)(i + 'a');
+    //             cnt = hash[i];
+    //         }
+    //     }
+    //     int n = s.length();
+    //     if (cnt > (n + 1) / 2) {
+    //         return "";
+    //     }
+    //     char[] arr = new char[n];
+    //     //put the letter into even index numbe (0, 2, 4 ...)
+    //     int index = 0;
+    //     while (hash[letter - 'a'] > 0) {
+    //         arr[index] = letter;
+    //         index += 2;
+    //         hash[letter - 'a']--;
+    //     }
+    //     //put the rest into the array
+    //     for (int i = 0; i < 26; i++) {
+    //         while (hash[i] > 0) {
+    //             if (index >= n) {
+    //                 index = 1;
+    //             }
+    //             arr[index] = (char)(i + 'a');
+    //             index += 2;
+    //             hash[i]--;
+    //         }
+    //     }
+    //     return new String(arr);
+    // }
+    
     public String reorganizeString(String s) {
-        int[] hash = new int[26];
-        //construct hash array
+        Map<Character, Integer> map = new HashMap<>();
         for (char c : s.toCharArray()) {
-            hash[c - 'a']++;
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
-        //find the most letter and its freq
-        char letter = 'a';
-        int cnt = 0;
-        for (int i = 0; i < 26; i++) {
-            if (hash[i] > cnt) {
-                letter = (char)(i + 'a');
-                cnt = hash[i];
+        //freq from large to small
+        PriorityQueue<Point> pq = new PriorityQueue<>((a, b) -> (b.freq - a.freq));
+        for (char c : map.keySet()) {
+            pq.add(new Point(c, map.get(c)));
+        }
+        StringBuilder sb = new StringBuilder();
+        while (pq.size() >= 2) {
+            Point p1 = pq.poll();
+            Point p2 = pq.poll();
+            sb.append(p1.c).append(p2.c);
+            if (--p1.freq > 0) {
+                pq.offer(p1);
+            }
+            if (--p2.freq > 0) {
+                pq.offer(p2);
             }
         }
-        int n = s.length();
-        if (cnt > (n + 1) / 2) {
-            return "";
+        if (!pq.isEmpty()) {
+            sb.append(pq.poll().c);
         }
-        char[] arr = new char[n];
-        //put the letter into even index numbe (0, 2, 4 ...)
-        int index = 0;
-        while (hash[letter - 'a'] > 0) {
-            arr[index] = letter;
-            index += 2;
-            hash[letter - 'a']--;
+        return sb.length() < s.length() ? "" : sb.toString();
+    }
+    
+    class Point {
+        char c;
+        int freq;
+        public Point(char c, int freq) {
+            this.c = c;
+            this.freq = freq;
         }
-        //put the rest into the array
-        for (int i = 0; i < 26; i++) {
-            while (hash[i] > 0) {
-                if (index >= n) {
-                    index = 1;
-                }
-                arr[index] = (char)(i + 'a');
-                index += 2;
-                hash[i]--;
-            }
-        }
-        return new String(arr);
     }
 }
