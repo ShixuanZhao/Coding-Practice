d7f12c5553b41142b2bbfb28587c00a16a20ef44
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-08-14 15:30:13 +0800

    Time: 14 ms (13.29%), Space: 57.5 MB (84.20%) - LeetHub

--- 690-employee-importance/690-employee-importance.java
+++ 690-employee-importance/690-employee-importance.java
@@ -26,4 +26,25 @@ class Solution {
         }
         return res;
     }
+    
+//     public int getImportance(List<Employee> employees, int id) {
+//         //注意类比到一个tree，就是求以root为根节点的树所有节点的和
+//         //dfs
+//         //construct a Hashmap to use id as index
+//         Map<Integer, Employee> map = new HashMap<>();
+//         for (Employee e : employees) {
+//             map.put(e.id, e);
+//         }
+//         return getSum(map, id);
+//     }
+    
+//     private int getSum(Map<Integer, Employee> map, int id) {
+//         Employee master = map.get(id);
+//         int sum = master.importance;
+//         //n-nary tree
+//         for (Integer sub : master.subordinates) {
+//             sum += getSum(map, sub);
+//         }
+//         return sum;
+//     }
 }
