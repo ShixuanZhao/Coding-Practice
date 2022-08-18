e30297e06df80b816b828b67290e1d1a1e6d3525
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-08-18 17:36:00 +0800

    Time: 227 ms (43.44%), Space: 46.7 MB (95.78%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.java
+++ 2013-detect-squares/2013-detect-squares.java
@@ -1,3 +1,8 @@
+// To compute count(p1):
+// We try all points p3 which together with p1 form the diagonal of non-empty square, it means abs(p1.x-p3.x) == abs(p1.y-p3.y) && abs(p1.x-p3.x) > 0
+// Since we have 2 points p1 and p3, we can form a square by computing the positions of 2 remain points p2, p4.
+// p2 = (p1.x, p3.y)
+// p4 = (p3.x, p1.y)
 class DetectSquares {
     List<int[]> points;
     Map<String, Integer> countMap;
@@ -20,6 +25,12 @@ class DetectSquares {
             //find the valid p3
             if (Math.abs(p[0] - x1) == Math.abs(p[1] - y1) && p[0] != x1) {
                 //find the valid p2 and p4
+                //x2 and x4
+            /*
+            x1   x2
+            
+            x4   x3
+            */
                 //p2:(p[0],y1) p4:(x1, p[1])
                 res += countMap.getOrDefault(p[0] + "," + y1, 0) * countMap.getOrDefault(x1 + "," + p[1], 0); 
             }
