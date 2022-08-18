e5f41388e8e902da0e493f3604209cb62fe33ff0
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-08-18 17:35:07 +0800

    Time: 216 ms (47.31%), Space: 47.1 MB (92.87%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.java
+++ 2013-detect-squares/2013-detect-squares.java
@@ -1,41 +1,33 @@
-// To compute count(p1):
-// We try all points p3 which together with p1 form the diagonal of non-empty square, it means abs(p1.x-p3.x) == abs(p1.y-p3.y) && abs(p1.x-p3.x) > 0
-// Since we have 2 points p1 and p3, we can form a square by computing the positions of 2 remain points p2, p4.
-// p2 = (p1.x, p3.y)
-// p4 = (p3.x, p1.y)
 class DetectSquares {
-    List<int[]> coordinates;
-    Map<String, Integer> counts;
-    
+    List<int[]> points;
+    Map<String, Integer> countMap;
     public DetectSquares() {
-        coordinates = new ArrayList<>();
-        counts = new HashMap<>();
+        points = new ArrayList<>();
+        countMap = new HashMap<>();
     }
     
     public void add(int[] point) {
-        coordinates.add(point);
-        String key = point[0] + "@" + point[1];
-        counts.put(key, counts.getOrDefault(key, 0) + 1);
+        points.add(point);
+        String key = point[0] + "," + point[1];
+        countMap.put(key, countMap.getOrDefault(key, 0) + 1);
     }
     
     public int count(int[] point) {
-        int sum = 0, px = point[0], py = point[1];
-        for (int[] coordinate : coordinates) {
-            int x = coordinate[0], y = coordinate[1];
-            if (px == x || py == y || (Math.abs(px - x) != Math.abs(py - y)))
-                continue;
-            //x2 and x4
-            /*
-            x1   x2
-            
-            x4   x3
-            */
-            sum += counts.getOrDefault(x + "@" + py, 0) * counts.getOrDefault(px + "@" + y, 0);
+        int x1 = point[0];
+        int y1 = point[1];
+        int res = 0;
+        for (int[] p : points) {
+            //find the valid p3
+            if (Math.abs(p[0] - x1) == Math.abs(p[1] - y1) && p[0] != x1) {
+                //find the valid p2 and p4
+                //p2:(p[0],y1) p4:(x1, p[1])
+                res += countMap.getOrDefault(p[0] + "," + y1, 0) * countMap.getOrDefault(x1 + "," + p[1], 0); 
+            }
         }
-        
-        return sum;
+        return res;
     }
 }
+
 /**
  * Your DetectSquares object will be instantiated and called as such:
  * DetectSquares obj = new DetectSquares();
