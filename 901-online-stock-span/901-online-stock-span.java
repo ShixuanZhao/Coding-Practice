242ce4c08143ee13df59a36a5e3fd8e90c3e7c9a
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-08-18 22:43:07 +0800

    Time: 24 ms (99.67%), Space: 49.9 MB (99.78%) - LeetHub

--- 901-online-stock-span/901-online-stock-span.java
+++ 901-online-stock-span/901-online-stock-span.java
@@ -3,7 +3,9 @@ class StockSpanner {
     /*
     initial res to 1
     [100,80,60,70,60,75,85]
-                      i
+                  i
+      stack:100 80 60
+      res:1 1 1 2 2
      stack:100 85 
      1 1 1 2 1 4(1 + 1 + 2) 1 + 4 + 1 = 6
                      60  70
@@ -18,9 +20,9 @@ class StockSpanner {
     public int next(int price) {
         //pair <price, res>
         int res = 1;
-        while (!stack.isEmpty() && stack.peekFirst()[0] <= price) {
-            res += stack.peekFirst()[1];
-            stack.pollFirst();
+        //less than or equal to today's price.
+        while (!stack.isEmpty() && price >= stack.peekFirst()[0]) {
+            res += stack.pollFirst()[1];
         }
         stack.offerFirst(new int[] {price, res});
         return res;
