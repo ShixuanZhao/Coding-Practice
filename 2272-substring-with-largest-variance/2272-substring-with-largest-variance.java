42725683ee99ce3629d12a0ba2eb75f3bc9cdac3
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-08-21 00:32:43 +0800

    Time: 364 ms (43.54%), Space: 43.7 MB (36.86%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.java
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.java
@@ -1,52 +1,38 @@
-/**
-For every possible character pair a(min freq) & b(max freq),
-find the substring with the max differenct between the freq of a & b which can be done using kadanes algorithm.
-
-We can think about it as finding the maximum subarray with a = -1 and b = 1 and other characters = 0. But we should have at least one occurrence of a.
-53. Maximum Subarray
-**/
 class Solution {
-    
     public int largestVariance(String s) {
-        HashMap<Character, Integer> charCount = new HashMap<Character, Integer>();
-        for (int i = 0; i < s.length(); i++) {
-            charCount.put(s.charAt(i), charCount.getOrDefault(s.charAt(i), 0)+1);
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
-        int variance = 0;
-        //Iterate through unique characters
-        for (Character primaryChar: charCount.keySet()) {
-            for  (Character secondaryChar: charCount.keySet()) {
-                // We need a unique character  pair to compute variance
-                if (primaryChar == secondaryChar) { continue; }
-                //we have to make sure for every candidate substring, there must be at least one a. A substring with all b is not a valid one 确保至少有一个secondaryChar
-                int secondaryCharCount = charCount.get(secondaryChar);
-                int primaryFreq = 0;
-                int secondaryFreq = 0;              
-                
+        int res = 0;
+        //suppose freq: c1 > c2 
+        for (char c1 : map.keySet()) {
+            for (char c2 : map.keySet()) {
+                if (c1 == c2) {
+                    continue;
+                }
+                int f1 = 0;
+                int f2 = 0;
+                int f2Cnt = map.get(c2);
                 for (int i = 0; i < s.length(); i++) {
-                    Character currChar = s.charAt(i);
-                    if (currChar == primaryChar) {
-                        primaryFreq++;  
+                    char cur = s.charAt(i);
+                    if (cur == c1) {
+                        f1++;
                     }
-                    if (currChar == secondaryChar) {
-                        secondaryFreq++;
-                        secondaryCharCount--;
+                    if (cur == c2) {
+                        f2++;
+                        f2Cnt--;
                     }
-                    // Variance only computed if we have already seen both a primary
-                    // and a secondary character
-                    if (primaryFreq > 0 && secondaryFreq > 0 && primaryFreq > secondaryFreq) {                    
-                        variance  = Math.max(variance, (primaryFreq - secondaryFreq));
+                    if (f1 > 0 && f2 > 0 && f1 > f2) {
+                        res = Math.max(res, f1 - f2);
                     }
-                    // We reset our index so we are ignoring the substring which will
-                    // impact our variance negatively
-                    if (primaryFreq < secondaryFreq && secondaryCharCount > 0) {                      
-                        primaryFreq = 0;
-                        secondaryFreq = 0;                       
+                    if (f1 < f2 && f2Cnt > 0) {
+                        f1 = 0;
+                        f2 = 0;
                     }
                 }
-                
             }
         }
-        return variance;
-    }   
+        return res;
+    }
 }
