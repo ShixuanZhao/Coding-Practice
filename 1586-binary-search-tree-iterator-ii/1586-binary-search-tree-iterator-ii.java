9dab62ad44a40cca2b58ddbcb93b686a9f483e6b
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-08-20 16:38:47 +0800

    Time: 137 ms (39.52%), Space: 112.4 MB (22.75%) - LeetHub

---
+++ 1586-binary-search-tree-iterator-ii/1586-binary-search-tree-iterator-ii.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+        Stack<TreeNode> nextStack = new Stack<>();
+        Stack<TreeNode> prevStack = new Stack<>();
+    //why need visited
+    //That is because if a node is moved from the prev stack back to next by calling prev() method we shouldn't be looking at it's children again to add to nextStack.
+        Set<TreeNode> visited = new HashSet<>();
+
+        public BSTIterator(TreeNode root) {
+            fillInNextStack(root);
+        }
+
+        public boolean hasNext() {
+            return !nextStack.isEmpty();
+        }
+
+        public int next() {
+            TreeNode node = nextStack.pop();
+            if(!visited.contains(node) && node.right != null) {
+                fillInNextStack(node.right);
+            }
+            visited.add(node);
+            prevStack.push(node);
+            return node.val;
+        }
+
+        public boolean hasPrev() {
+            return !prevStack.isEmpty() && prevStack.size() > 1;
+        }
+
+        public int prev() {
+            nextStack.push(prevStack.pop());
+            return prevStack.peek().val;
+        }
+
+        private void fillInNextStack(TreeNode node) {
+            while(node != null) {
+                nextStack.push(node);
+                node = node.left;
+            }
+        }
+    }
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * boolean param_1 = obj.hasNext();
+ * int param_2 = obj.next();
+ * boolean param_3 = obj.hasPrev();
+ * int param_4 = obj.prev();
+ */
