e753860a24f16c0a0c13aa84d76c1f09d65035ea
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-08-31 00:32:55 +0800

    Time: 31 ms (12.56%), Space: 45.2 MB (48.02%) - LeetHub

--- 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.java
+++ 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.java
@@ -1,25 +1,38 @@
 class Solution {
+    //must use map instead of map because set will remove all letter in the window
+    // public int lengthOfLongestSubstringKDistinct(String s, int k) {
+    //     int res = 0;
+    //     Set<Character> set = new HashSet<>();
+    //     int left = 0;
+    //     for (int i = 0; i < s.length(); i++) {
+    //         char c = s.charAt(i);
+    //         set.add(c);
+    //         while (set.size() > k) {
+    //             set.remove(s.charAt(i));
+    //             i++;
+    //         }
+    //         res = Math.max(res, i - left + 1);
+    //     }
+    //     return res;
+    // }
+    
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
-        //distinct的理解：aba distinct number是2,2个a只能算一个就是问有几个不一样的letter
-        //map: key is distinct character, value 是出现的次数，所以key的size直接和k比较久可以
+        int res = 0;
         Map<Character, Integer> map = new HashMap<>();
         int left = 0;
-        int res = 0;
         for (int i = 0; i < s.length(); i++) {
-            char cur = s.charAt(i);
-            map.put(cur, map.getOrDefault(cur, 0) + 1);
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
             while (map.size() > k) {
-                char c = s.charAt(left);
-                map.put(c, map.get(c) - 1);
-                if (map.get(c) == 0) {
-                    map.remove(c);
+                char c1 = s.charAt(left);
+                map.put(c1, map.get(c1) - 1);
+                if (map.get(c1) == 0) {
+                    map.remove(c1);
                 }
                 left++;
             }
             res = Math.max(res, i - left + 1);
         }
         return res;
-        
-        
     }
 }
