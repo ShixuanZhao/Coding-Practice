4ece6dd60cc19885b2ea84cc7ba10ed913b06b62
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-09-03 19:29:50 +0800

    Time: 19 ms (62.43%), Space: 54.4 MB (62.15%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,4 +1,15 @@
 class Solution {
+     /*
+    a b c
+    3 2 1
+    used:3 2
+    res:2
+    use a hashset to record the used freq
+    Greedy works since we can only delete characters (we cannot add or replace characters).
+
+So, count each character first. For each 26 characters, check if it's count is already used. 
+If so, delete characters until you find unused count, or reach zero.
+    */
     public int minDeletions(String s) {
         int[] freq = new int[26];
         for (int i = 0 ; i < s.length(); i++) {
