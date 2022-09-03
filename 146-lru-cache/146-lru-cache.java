4ebef14eab68f64b48cf9e417d4415efdc1e4128
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-09-03 12:49:48 +0800

    Time: 79 ms (69.92%), Space: 117.9 MB (88.03%) - LeetHub

--- 146-lru-cache/146-lru-cache.java
+++ 146-lru-cache/146-lru-cache.java
@@ -1,21 +1,4 @@
-/*
-    capacity:2
-map    3     1
-       |     | 
-     (3,3) (1,1)
-     
-    ds: double LinkedList HashMap<Integer, Node<Integer, Integer>>
-    get:case1:if 4 is not in the list, return -1
-        case2:   in the list   return value remove from original position and moveToHead
-               
-    put:
-    case1:put(1,5), put the exsiting node update the value and move to head
-    case2:put(3,3) not exceed capacity, move to head
-    case3:put(4,3) the capacity is not enough,remove the last ele, move the new one to head
-    update the HashMap
-*/
 class LRUCache {
-   //define double LinkedList
     class Node {
         int key;
         int val;
@@ -27,71 +10,67 @@ class LRUCache {
         }
     }
     
+    Map<Integer, Node> map;
     Node head;
     Node tail;
-    Map<Integer, Node> map;
     int capacity;
     
     public LRUCache(int capacity) {
+        map = new HashMap<>();
+        this.capacity = capacity;
         head = new Node(0, 0);
         tail = head;
         head.next = tail;
         tail.prev = head;
-        map = new HashMap<>();
-        this.capacity = capacity;
     }
     
     public int get(int key) {
-       //case1
         if (!map.containsKey(key)) {
             return -1;
-        } else {
-            //case2
-            Node cur = map.get(key);
-            int res = cur.val;
-            remove(cur);
-            moveToHead(cur);
-            return res;
         }
+        Node res = map.get(key);
+        remove(res);
+        moveToHead(res);
+        return res.val;
     }
     
     public void put(int key, int value) {
-        //case1
         if (map.containsKey(key)) {
-            Node cur = map.get(key);
-            cur.val = value;
-            remove(cur);
-            moveToHead(cur);
+            Node node = map.get(key);
+            node.val = value;
+            remove(node);
+            moveToHead(node);
         } else {
-            //case2:not exceed capacity
-            Node cur = new Node(key, value);
+            Node newNode = new Node(key, value);
             if (map.size() < capacity) {
-                moveToHead(cur);
+                moveToHead(newNode);
             } else {
-                //case3
-                //remove the last element
+                moveToHead(newNode);
                 remove(tail.prev);
-                moveToHead(cur);
             }
         }
+        
     }
     
-    //add the ele to the head
-    private void moveToHead(Node cur) {
-        map.put(cur.key, cur);
-        cur.next = head.next;
-        head.next.prev = cur;
-        head.next = cur;
-        cur.prev = head;
+    /*
+    node1 node node2
+    */
+    private void remove(Node node) {
+        map.remove(node.key);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        node.next = null;
+        node.prev = null;
     }
     
-    //delete the certain ele
-    private void remove(Node cur) {
-        map.remove(cur.key);
-        cur.prev.next = cur.next;
-        cur.next.prev = cur.prev;
+    //head node node1
+    private void moveToHead(Node node) {
+        map.put(node.key, node);
+        node.next = head.next;
+        node.next.prev = node;
+        head.next = node;
+        node.prev = head;
     }
-    
 }
 
 /**
