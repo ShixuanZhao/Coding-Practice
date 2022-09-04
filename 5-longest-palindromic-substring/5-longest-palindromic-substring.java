ff2c6e0e206efc57fad2f86db05f7d202cff432c
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-09-05 04:43:47 +0800

    Time: 40 ms (64.60%), Space: 46.3 MB (47.49%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,122 +1,28 @@
 class Solution {
     /*
-        input: string output:string
-        clarify: s == null s.length() == 0
-            return ""
-        enumrate all the substring,check if the it is palin
-        if true, update the maxLen, record the begin letter
-        substring(left, right)
-        for left in s
-            for right in s
-                check palir 
-        O(n^3)
-        
-        a(bb)a  is, update the maxLen
-        i    j
-        a(bb)c  is not palin
-        high level:dp
-        M[i][j] i < j whether [i, j] is palin
-        induction rule: 
-        M[i][j] = M[i + 1][j - 1] if char i = char j
-                  j - i < 3 aba  aa   true
-                            i j  i j
-                char i != char j false
-        if M[i][j] is true
-        update the maxLen record the left ele
-        expand from the center and check the edge
-        T = O(n^2) S = O(n^2)
+    expand from center for each letter,and there are only 2n - 12n−1 such centers.
     */
-    // public String longestPalindrome(String s) {
-        // if (s == null || s.length() == 0) {
-        //     return "";
-        // }
-        // int n = s.length();
-        // boolean[][] M = new boolean[n][n];
-        // for (int i = 0; i < n; i++) {
-        //     M[i][i] = true;
-        // }
-        // int begin = 0;
-        // //the minimum len is 1
-        // int maxLen = 1;
-        // for (int j = 1; j < n; j++) {
-        //     for (int i = j - 1; i >= 0; i--) {
-        //         if (s.charAt(i) == s.charAt(j)) {
-        //             //guarantee at least 3 ele
-        //             if (j - i < 3) {
-        //                 M[i][j] = true;
-        //             } else {
-        //                 M[i][j] = M[i + 1][j - 1];
-        //             }
-        //         } else {
-        //             M[i][j] = false;
-        //         }
-        //         if (M[i][j] && j - i + 1 > maxLen) {
-        //             begin = i;
-        //             maxLen = j - i + 1;
-        //         }
-        //     }
-        // }
-        // return s.substring(begin, begin + maxLen);
-        
-//         int n = s.length();
-//         if (n == 1) {
-//             return s;
-//         }
-//         StringBuilder sb = new StringBuilder();
-//         int[] maxLen = new int[1];
-//         for (int i = 0; i < n; i++) {
-//             checkP(s, i, i, sb, maxLen);
-//             checkP(s, i, i + 1, sb, maxLen);
-//         }
-//         return sb.toString();
-//     }
-    
-//     private void checkP(String s, int left, int right, StringBuilder sb, int[] maxLen) {
-//         while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-//             left--;
-//             right++;
-//         }
-//         //(left, right) not include boundary
-//         if (maxLen[0] < right - left - 1) {
-//             maxLen[0] = right - left - 1;
-//             sb.setLength(0);
-//             sb.append(s.substring(left + 1, right));
-//         }
-    
-//     }
-    
+    int maxLen = 0;
     public String longestPalindrome(String s) {
-        if (s == null || s.length() == 0) {
-            return "";
-        }
+        if (s == null || s.length() < 1) return "";
         int n = s.length();
-        boolean[][] M = new boolean[n][n];
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            M[i][i] = true;
+            expand(s, i, i, sb);
+            expand(s, i, i + 1, sb);
         }
-        int begin = 0;
-        //the minimum len is 1
-        int maxLen = 1;
-        for (int len = 2; len <= n; len++) {
-            for (int i = 0; i + len - 1 < n; i++) {
-                int j = i + len - 1;
-                if (s.charAt(i) == s.charAt(j)) {
-                    //guarantee at least 3 ele
-                    //only two ele
-                    if (j - i < 3) {
-                        M[i][j] = true;
-                    } else {
-                        M[i][j] = M[i + 1][j - 1];
-                    }
-                } else {
-                    M[i][j] = false;
-                }
-                if (M[i][j] && j - i + 1 > maxLen) {
-                    begin = i;
-                    maxLen = j - i + 1;
-                }
-            }
+        return sb.toString();
+    }
+    
+    private void expand(String s, int i, int j, StringBuilder sb) {
+        while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)) {
+            i--;
+            j++;
         }
-        return s.substring(begin, begin + maxLen);
+        if (j - i - 1 > maxLen) {
+            maxLen = j - i - 1;
+            sb.setLength(0);
+            sb.append(s.substring(i + 1, j));
+        }
     }
 }
