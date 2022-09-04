f2dc143f865c90e926d2c570d44ec0f6ac7f7b06
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-09-05 07:11:11 +0800

    Time: 38 ms (37.07%), Space: 45.2 MB (57.32%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,15 +1,23 @@
 class Solution {
+    //M[i]: the min coin to make up amount i
+    //M[i] = Min(M[j]) + 1 j = i - all the possible coins value, and we can reach j
     public int coinChange(int[] coins, int amount) {
+        //jump game similiar
+        //M[i]凑i的钱数最少需要的coin数量
         int[] M = new int[amount + 1];
         Arrays.fill(M, Integer.MAX_VALUE);
         M[0] = 0;
         for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < coins.length; j++) {
-                if (i >= coins[j] && M[i - coins[j]] != Integer.MAX_VALUE) {
-                    M[i] = Math.min(M[i], M[i - coins[j]] + 1);
+            //右小段是一枚硬币
+            for (int coin : coins) {
+                //coindition!
+                if (i >= coin && M[i - coin] != Integer.MAX_VALUE) {
+                    M[i] = Math.min(M[i], M[i - coin] + 1);
                 }
             }
         }
         return M[amount] == Integer.MAX_VALUE ? -1 : M[amount];
     }
+    //M2:无限背包，int[coins.length + 1][amount + 1]
+    //j是空间容量,if j is valid M[i][j] = Math.min(M[i - 1][j], M[i][j - coins[i - 1]] + 1)
 }
