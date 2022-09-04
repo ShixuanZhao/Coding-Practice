7e655361d63bc5074bd300d01e35d280bfbbe8b7
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-09-05 04:57:37 +0800

    Time: 1 ms (53.08%), Space: 41.4 MB (22.67%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,22 +1,19 @@
 class Solution {
     /*
-        //dp
-        M[i][j]:the number of paths to get the position (i, j) from start position
-        M[i][j] = M[i][j - 1] + M[i - 1][j]
-        1 1
-        1 2
+    M[i][j]: ways from (0, 0) to (i, j)
+    M[i][j] = M[i - 1][j] + M[i][j - 1]
     */
     public int uniquePaths(int m, int n) {
         int[][] M = new int[m][n];
-        M[0][0] = 1;
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (j - 1 >= 0) {
-                    M[i][j] = M[i][j - 1];
-                }
-                if (i - 1 >= 0) {
-                    M[i][j] += M[i - 1][j];
-                }
+            M[i][0] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            M[0][j] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                M[i][j] = M[i - 1][j] + M[i][j - 1];
             }
         }
         return M[m - 1][n - 1];
