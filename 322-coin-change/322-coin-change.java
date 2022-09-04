81b378f1deaeb956b19cbef0cef932b4bfd776a7
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-09-05 07:10:39 +0800

    Time: 43 ms (32.54%), Space: 45.7 MB (35.50%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,16 +1,12 @@
 class Solution {
-    /*
-    coins = [1,2,5], amount = 11
-    M:0 1
-    */
     public int coinChange(int[] coins, int amount) {
         int[] M = new int[amount + 1];
         Arrays.fill(M, Integer.MAX_VALUE);
         M[0] = 0;
         for (int i = 1; i <= amount; i++) {
-            for (int coin : coins) {
-                if (i >= coin && M[i - coin] != Integer.MAX_VALUE) {
-                    M[i] = Math.min(M[i], M[i - coin] + 1);
+            for (int j = 0; j < coins.length; j++) {
+                if (i >= coins[j] && M[i - coins[j]] != Integer.MAX_VALUE) {
+                    M[i] = Math.min(M[i], M[i - coins[j]] + 1);
                 }
             }
         }
