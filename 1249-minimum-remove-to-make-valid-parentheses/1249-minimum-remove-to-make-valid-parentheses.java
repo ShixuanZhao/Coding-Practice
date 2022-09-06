c332a97898f95bbfcc46f9dd17d1162bd65e83a3
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-09-06 14:03:29 +0800

    Time: 23 ms (82.77%), Space: 51.9 MB (58.49%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -8,6 +8,7 @@ class Solution {
     make diff with 921. Minimum Add to Make Parentheses Valid
     */
     public String minRemoveToMakeValid(String s) {
+        //open left (
         int open = 0;
         StringBuilder sb = new StringBuilder();
         for (char c : s.toCharArray()) {
@@ -32,4 +33,30 @@ class Solution {
         }
         return res.reverse().toString();
     }
+    
+    //intuition
+    // public String minRemoveToMakeValid(String s) {
+    //     Set<Integer> indexesToRemove = new HashSet<>();
+    //     Deque<Integer> stack = new ArrayDeque<>();
+    //     for (int i = 0; i < s.length(); i++) {
+    //         if (s.charAt(i) == '(') {
+    //             stack.push(i);
+    //         } if (s.charAt(i) == ')') {
+    //             if (stack.isEmpty()) {
+    //                 indexesToRemove.add(i);
+    //             } else {
+    //                 stack.pop();
+    //             }
+    //         }
+    //     }
+    //     // Put any indexes remaining on stack into the set.
+    //     while (!stack.isEmpty()) indexesToRemove.add(stack.pop());
+    //     StringBuilder sb = new StringBuilder();
+    //     for (int i = 0; i < s.length(); i++) {
+    //         if (!indexesToRemove.contains(i)) {
+    //             sb.append(s.charAt(i));
+    //         }
+    //     }
+    //     return sb.toString();
+    // }
 }
