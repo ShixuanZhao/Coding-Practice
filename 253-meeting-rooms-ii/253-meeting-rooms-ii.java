7023d0c727827e0b3b45503e0457093dec37da47
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-09-10 04:35:20 +0800

    Time: 14 ms (31.10%), Space: 43.5 MB (85.49%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.java
@@ -1,42 +1,23 @@
 class Solution {
-    // public int minMeetingRooms(int[][] intervals) {
-    //     //起点+1，终点-1放进TreeMap
-    //     //TreeMap根据key的大小从小到大排序,即时间的顺序
-    //     Map<Integer, Integer> map = new TreeMap<>();
-    //     for (int[] interval : intervals) {
-    //         map.put(interval[0], map.getOrDefault(interval[0], 0) + 1);
-    //         map.put(interval[1], map.getOrDefault(interval[1], 0) - 1);
-    //     }
-    //     int room = 0;
-    //     int maxRoom = 0;
-    //     //按照时间顺序，过一遍value
-    //     for (int val : map.values()) {
-    //         room += val;
-    //         maxRoom = Math.max(maxRoom, room);
-    //     }
-    //     return maxRoom;
-    // }
-    
-    //pq T = O(nlogn)
     /*
-    pq store the meeting that have not end
-    1.sort the interval by starting time
-    2.iterate the arrat
-    [[0,30],[5,10],[15,20]]
-                     i
-      pq:[0,30], [15,20]
+    use a treemap, key is the time point, val is the change
+    0  5 10   15 20  30
+    1  1  -1  1  -1   -1
+cnt:                  0
+res:2
     */
-     public int minMeetingRooms(int[][] intervals) {
-         Arrays.sort(intervals, (a, b) -> (a[0] - b[0]));
-         //pq always pop the earliest ending time
-         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[1] - b[1]));
-         pq.offer(intervals[0]);
-         for (int i = 1; i < intervals.length; i++) {
-             if (intervals[i][0] >= pq.peek()[1]) {
-                 pq.poll();
-             }
-             pq.offer(intervals[i]);
-         }
-         return pq.size();
-     }
+    public int minMeetingRooms(int[][] intervals) {
+        Map<Integer, Integer> map = new TreeMap<>();
+        for (int[] cur : intervals) {
+            map.put(cur[0], map.getOrDefault(cur[0], 0) + 1);
+            map.put(cur[1], map.getOrDefault(cur[1], 0) - 1);
+        }
+        int res = 0;
+        int cnt = 0;
+        for (int cur : map.values()) {
+            cnt += cur;
+            res = Math.max(res, cnt);
+        }
+        return res;
+    }
 }
