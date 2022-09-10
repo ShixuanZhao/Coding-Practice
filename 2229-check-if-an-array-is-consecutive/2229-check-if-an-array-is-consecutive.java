625083a908fdfea7fd7603ded64b3ebc1b1c243d
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-09-11 06:53:25 +0800

    Time: 25 ms (84.42%), Space: 58.6 MB (98.70%) - LeetHub

--- 2229-check-if-an-array-is-consecutive/2229-check-if-an-array-is-consecutive.java
+++ 2229-check-if-an-array-is-consecutive/2229-check-if-an-array-is-consecutive.java
@@ -1,11 +1,28 @@
 class Solution {
+    // public boolean isConsecutive(int[] nums) {
+    //     Arrays.sort(nums);
+    //     for (int i = 0; i < nums.length - 1; i++) {
+    //         if (nums[i] + 1 != nums[i + 1]) {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+    
     public boolean isConsecutive(int[] nums) {
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] + 1 != nums[i + 1]) {
-                return false;
-            }
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
         }
-        return true;
+        if (set.size() != nums.length) {
+            return false;
+        }
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+        return max - min + 1 == set.size();
     }
 }
