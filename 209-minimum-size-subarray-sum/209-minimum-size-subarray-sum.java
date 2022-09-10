30514d8768d8309d8420ef991c9ddeecb1cf604b
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-09-11 03:43:14 +0800

    Time: 2 ms (80.33%), Space: 57.9 MB (61.16%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
@@ -1,22 +1,23 @@
 class Solution {
     /*
-    minLen:3
-    [2,3,1,2,4,3]
-               f
-           s
-    curSum:6
-    when to move slow:curSum > target
+    nums = [2,3,1,2,4,3] T = 7
+                    i
+                      j
+    [i,j]
+    sum:7
+    minLen: 2
     */
     public int minSubArrayLen(int target, int[] nums) {
+        int n = nums.length;
+        int i = 0;
+        int sum = 0;
         int minLen = Integer.MAX_VALUE;
-        int slow = 0;
-        int curSum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            curSum += nums[i];
-            while (curSum >= target) {
-                minLen = Math.min(minLen, i - slow + 1);
-                curSum -= nums[slow];
-                slow++;
+        for (int j = 0; j < n; j++) {
+            sum += nums[j];
+            while (sum >= target) {
+                minLen = Math.min(minLen, j - i + 1);
+                sum -= nums[i];
+                i++;
             }
         }
         return minLen == Integer.MAX_VALUE ? 0 : minLen;
