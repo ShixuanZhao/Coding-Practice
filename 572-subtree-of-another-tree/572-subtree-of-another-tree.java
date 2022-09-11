a2cb030543de6bbae359bff9d3df10c8769f3f08
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-09-11 09:00:33 +0800

    Time: 5 ms (38.59%), Space: 46.8 MB (26.72%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.java
@@ -39,6 +39,38 @@ class Solution {
         return isSame(root1.left, root2.left) && isSame(root1.right, root2.right);
     }
     
+// public boolean isSubtree(TreeNode s, TreeNode t) {
+//     Queue<TreeNode> nodes = new ArrayDeque<>();
+//     nodes.offer(s);
+//     while (!nodes.isEmpty()) {
+//         TreeNode node = nodes.poll();
+//         if (isSameTree(node, t)) {
+//             return true;
+//         }
+//         if (node.left != null) {
+//             nodes.offer(node.left);
+//         }
+//         if (node.right != null) {
+//             nodes.offer(node.right);
+//         }
+//     }
+//     return false;
+// }
+
+// public boolean isSameTree(TreeNode s, TreeNode t) {
+//     if (s == null && t == null) {
+//         return true;
+//     }
+//     if (s == null || t == null) {
+//         return false;
+//     }
+//     if (s.val != t.val) {
+//         return false;
+//     } else {
+//         return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
+//     }
+// }
+    
 //     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
 //         String s1 = getString(root);
 //         String s2 = getString(subRoot);
