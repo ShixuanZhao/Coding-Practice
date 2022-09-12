6462c4806617e7a82821a201ea57fa7575143c38
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-09-12 11:58:39 +0800

    Time: 750 ms (5.01%), Space: 117.8 MB (11.37%) - LeetHub

--- 1102-path-with-maximum-minimum-value/1102-path-with-maximum-minimum-value.java
+++ 1102-path-with-maximum-minimum-value/1102-path-with-maximum-minimum-value.java
@@ -1,6 +1,9 @@
 class Solution {
     int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-  
+//   Let me try to explain the approach.
+// This is just plain BFS but instead of using standard FIFO queue , we are using Max heap/ priority queue. This will ensure that we always pick up next node in queue with maximum smallest value. So queue will still have all possible values from a cell, but we only pick node with maximum value amongst them.
+// Example Suppose from (0,0) position we have 2 options to go (3,4}. We will add both values in queue but 4 will be at top. Next we process 4 and suppose options from 4 are {1,5}
+// We will add both the options, but cell with value 5 will be processed next( and not 3 as in normal FIFO queue). Going this way once you reach target, you are done as BFS approach finds best solution first.
   public int maximumMinimumPath(int[][] A) {
     int n = A.length;
     int m = A[0].length;
@@ -18,6 +21,7 @@ class Solution {
       int col = cell[1];
       
       if (row == n - 1 && col == m - 1) {
+          //cell[2] is the min val in the path
         return cell[2];
       }
       
