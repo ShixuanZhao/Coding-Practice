4936e2544fbf337c1e56871f922168d99fbfcdb3
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-09-15 16:05:09 -0700

    Time: 12 ms (72.26%), Space: 53.7 MB (76.72%) - LeetHub

--- 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.java
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.java
@@ -18,64 +18,64 @@ class Solution {
 // two or more negative - either product of first two * last(k - 2) or product of last k
     
     //M2:find out the three largest numbers and the two smallest numbers using one pass.
-//     public int maximumProduct(int[] nums) {
-//         int max1 = Integer.MIN_VALUE;
-//         int max2 = Integer.MIN_VALUE;
-//         int max3 = Integer.MIN_VALUE;
-//         int min1 = Integer.MAX_VALUE;
-//         int min2 = Integer.MAX_VALUE;
-//         for (int n : nums) {
-//             if (n > max1) {
-//                 max3 = max2;
-//                 max2 = max1;
-//                 max1 = n;
-//             } else if (n > max2) {
-//                 max3 = max2;
-//                 max2 = n;
-//             } else if (n > max3){
-//                 max3 = n;
-//             }
+    public int maximumProduct(int[] nums) {
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        for (int n : nums) {
+            if (n >= max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if (n >= max2) {
+                max3 = max2;
+                max2 = n;
+            } else if (n > max3){
+                max3 = n;
+            }
             
-//             if (n < min1) {
-//                 min2 = min1;
-//                 min1 = n;
-//             } else if (n < min2) {
-//                 min2 = n;
-//             }
-//         }
-//         return Math.max(max1 * max2 * max3, max1 * min1 * min2);
-//     }
+            if (n <= min1) {
+                min2 = min1;
+                min1 = n;
+            } else if (n <= min2) {
+                min2 = n;
+            }
+        }
+        return Math.max(max1 * max2 * max3, max1 * min1 * min2);
+    }
     
     //a solution that can extend to k numbers
-    public int maximumProduct(int[] nums) {
-        //minHeap
-        PriorityQueue<Integer> posHeap = new PriorityQueue<>(3);
-        //maxHeap
-        PriorityQueue<Integer> negHeap = new PriorityQueue<>(2, Collections.reverseOrder());
-        for (int x: nums) {
-            if (posHeap.size() < 3) {
-                posHeap.offer(x);
-            } else if (posHeap.peek() < x) {
-                posHeap.poll();
-                posHeap.offer(x);
-            }
+//     public int maximumProduct(int[] nums) {
+//         //minHeap
+//         PriorityQueue<Integer> posHeap = new PriorityQueue<>(3);
+//         //maxHeap
+//         PriorityQueue<Integer> negHeap = new PriorityQueue<>(2, Collections.reverseOrder());
+//         for (int x: nums) {
+//             if (posHeap.size() < 3) {
+//                 posHeap.offer(x);
+//             } else if (posHeap.peek() < x) {
+//                 posHeap.poll();
+//                 posHeap.offer(x);
+//             }
             
-            if (negHeap.size() < 2) {
-                negHeap.offer(x);
-            } else if (negHeap.peek() > x) {
-                negHeap.poll();
-                negHeap.offer(x);
-            }
-        }
-        int c1 = 1;
-        int max = 0;
-        while (!posHeap.isEmpty()) {
-            max = posHeap.poll();
-            c1 *= max;
-        }
-        while (!negHeap.isEmpty()) {
-            max *= negHeap.poll();
-        }
-        return Math.max(c1, max);
-    }
+//             if (negHeap.size() < 2) {
+//                 negHeap.offer(x);
+//             } else if (negHeap.peek() > x) {
+//                 negHeap.poll();
+//                 negHeap.offer(x);
+//             }
+//         }
+//         int c1 = 1;
+//         int max = 0;
+//         while (!posHeap.isEmpty()) {
+//             max = posHeap.poll();
+//             c1 *= max;
+//         }
+//         while (!negHeap.isEmpty()) {
+//             max *= negHeap.poll();
+//         }
+//         return Math.max(c1, max);
+//     }
 }
