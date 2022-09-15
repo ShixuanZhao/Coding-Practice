8bdad7249407c163376d2e9afd9cca1c0203aca6
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-09-15 01:04:51 -0700

    Time: 1 ms (97.81%), Space: 44 MB (38.93%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,24 +1,25 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
+        //recursion tree:2n level, choose '(' : left:左括号的数量<n
+        //can add ')':left > right
         StringBuilder sb = new StringBuilder();
         List<String> res = new LinkedList<>();
         dfs(n, sb, 0, 0, 0, res);
         return res;
     }
-    
+
     private void dfs(int n, StringBuilder sb, int index, int left, 
-        int right, List<String> res) {
+    int right, List<String> res) {
         if (index == 2 * n) {
             res.add(sb.toString());
-            return;
         }
         if (left < n) {
-            sb.append("(");
+            sb.append('(');
             dfs(n, sb, index + 1, left + 1, right, res);
             sb.deleteCharAt(sb.length() - 1);
         }
-        if (left > right) {
-            sb.append(")");
+        if (right < left) {
+            sb.append(')');
             dfs(n, sb, index + 1, left, right + 1, res);
             sb.deleteCharAt(sb.length() - 1);
         }
