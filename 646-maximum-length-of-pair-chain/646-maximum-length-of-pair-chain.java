11c34384aa470e4450c3567e1b7f29ac5c625c20
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-09-29 15:44:46 +0800

    Time: 56 ms (25.14%), Space: 54.8 MB (9.93%) - LeetHub

---
+++ 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.java
@@ -0,0 +1,26 @@
+class Solution {
+    //longest increasing subsequence
+    public int findLongestChain(int[][] pairs) {
+      int n  = pairs.length;
+      Arrays.sort(pairs, (a , b)->{
+        if(a[0] == b[0]){
+          return a[1] - b[1];
+        } else {
+          return a[0] - b[0]; 
+        }
+      });  
+      
+      int[] dp = new int[n];
+      Arrays.fill(dp, 1);
+      
+      for(int i = 1; i < n; i++){
+        for(int j = 0; j < i; j++){
+          if(pairs[j][1] < pairs[i][0]){
+            dp[i] = Math.max(dp[i], dp[j] + 1); 
+          }
+        }
+      }
+      
+      return dp[n - 1];
+    }
+}
