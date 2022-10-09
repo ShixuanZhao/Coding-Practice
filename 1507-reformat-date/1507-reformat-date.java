b6c7f805c66991aa9967404656b8d7078579e2af
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-10-10 06:54:54 +0800

    Time: 20 ms (7.03%), Space: 43.4 MB (6.31%) - LeetHub

---
+++ 1507-reformat-date/1507-reformat-date.java
@@ -0,0 +1,14 @@
+class Solution {
+    //保证日期和月份都是2位数
+    public String reformatDate(String date) {
+        String[] months = new String[]{"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        Map<String, String> map = new HashMap<>();
+        for (int i = 0; i < months.length; ++i) {
+            map.put(months[i], (i + 1 < 10 ? "0" : "") + (i+1));    
+        }
+        String[] parts = date.split(" "); 
+        String day = (parts[0].length() == 3 ? "0" : "") + parts[0].substring(0, parts[0].length() - 2);
+        return parts[2] + "-" + map.get(parts[1]) + "-" + day;
+
+    }
+}
