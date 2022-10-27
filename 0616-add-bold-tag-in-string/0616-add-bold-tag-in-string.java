e29287e05e3eb454829f9fce4052d4125d3efc7a
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-10-28 03:08:10 +0800

    Time: 9 ms (94.19%), Space: 44 MB (81.60%) - LeetHub

--- 0616-add-bold-tag-in-string/0616-add-bold-tag-in-string.java
+++ 0616-add-bold-tag-in-string/0616-add-bold-tag-in-string.java
@@ -1,25 +1,38 @@
 class Solution {
+    /*
+    "abcxyz123abc", words = ["abc","123"]
+    */
     public String addBoldTag(String s, String[] words) {
+        //use a boolean[] to mark the char that need to be bold
         int n = s.length();
-        boolean[] mark = new boolean[n];
+        boolean[] bold = new boolean[n];
+        //the start index of word in s
         for (String word : words) {
             int start = 0;
             while (s.indexOf(word, start) != -1) {
                 int index = s.indexOf(word, start);
                 int end = index + word.length();
                 for (int i = index; i < end; i++) {
-                    mark[i] = true;
+                    bold[i] = true;
                 }
-                start = start + 1;
+                //aaaa find aaa, the letter can be reused!
+                //aaa
+                // aaa
+                //not index + word.legnth()
+                start = index + 1;
             }
         }
+        //bold like this[111000111]
+        //add <b> and </b>
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            if (mark[i] && (i == 0 || !mark[i - 1])) {
+            //add open tag before the char in s, [00<b>1...]
+            if (bold[i] && (i == 0 || !bold[i - 1])) {
                 sb.append("<b>");
             }
             sb.append(s.charAt(i));
-            if (mark[i] && (i == n - 1 || !mark[i + 1])) {
+            //add close tag after the cur char in s [11</b>0...]
+            if (bold[i] && (i == n - 1 || !bold[i + 1])) {
                 sb.append("</b>");
             }
         }
