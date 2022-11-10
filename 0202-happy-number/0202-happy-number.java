460de580dbd80b578f364b5c71aaa10c39b14754
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-11-10 14:03:12 +0800

    Time: 3 ms (55.19%), Space: 41.1 MB (62.37%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        set.add(n);
+        while (true) {
+            int cur = 0;
+            for (int i = n; i > 0; i /= 10) {
+                cur += (i % 10) * (i % 10);
+            }
+            if (cur == 1) {
+                return true;
+            }
+            if (!set.add(cur)) {
+                return false;
+            }
+            n = cur;
+        }
+    }
+}
