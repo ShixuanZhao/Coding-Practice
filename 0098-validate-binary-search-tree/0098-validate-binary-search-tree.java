9a8bd7c03639ecb22efd1812ed5f916d50527a2d
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-11-15 15:21:42 +0800

    Time: 3 ms (18.73%), Space: 44.1 MB (29.19%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,20 +14,44 @@
  * }
  */
 class Solution {
+    /*
+         5
+(min,5)1   4(5, max)    every node (min, max)
+         3  6            if the whole tree is a bst, for every node, min < cur.val < max
+    */
+//     public boolean isValidBST(TreeNode root) {
+//        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+//     }
+    
+//     private boolean helper(TreeNode root, long min, long max) {
+//         if (root == null) {
+//             return true;
+//         }
+//         if (root.val <= min || root.val >= max) {
+//             return false;
+//         }
+//         return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+//     }
+    //the above solution is recursion, it can casuse stackoverflow, we can iterative way to sovle it by inorder traversal
     public boolean isValidBST(TreeNode root) {
         if (root == null) {
             return false;
         }
-        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
-    }
-    
-    private boolean isValidBST(TreeNode root, long min, long max) {
-        if (root == null) {
-            return true;
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode prev = null;
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.offerFirst(root);
+                root = root.left;
+            }
+            root = stack.pollFirst();
+            if (prev != null && root.val <= prev.val) {
+                return false;
+            }
+            //!!!
+            prev = root;
+            root = root.right;
         }
-        if (root.val <= min || root.val >= max) {
-            return false;
-        }
-        return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+        return true;
     }
 }
