996421edd5c95c5015eeff0653df00193ecfb644
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-11-15 16:06:18 +0800

    Time: 14 ms (77.64%), Space: 55.1 MB (64.08%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,153 @@
+class Solution {
+    /*
+        M1:step1:sort by start
+        -------  prev
+             -----  cur
+        ---------- merge
+                      -----
+ step2   for each interval
+            if  prev.end >= cur.start merge
+            else cannot merge add prev to res
+            T = O(nlogn + n) n is the length
+            
+            [[1,6],[2,6],[8,10],[15,18]]
+                                  cur   
+                                        next
+            res:[1, 6] [8, 10] [15, 18]
+              
+    */
+    // public int[][] merge(int[][] intervals) {
+    //    int n = intervals.length;
+    //     if (n == 1) {
+    //         return intervals;
+    //     }
+    //     Arrays.sort(intervals, (a, b) -> (a[0] - b[0]));
+    //     List<int[]> res = new LinkedList<>();
+    //     int[] prev = intervals[0];
+    //     for (int i = 1; i < n; i++) {
+    //         if (intervals[i][0] <= prev[1]) {
+    //             prev[1] = Math.max(prev[1], intervals[i][1]);
+    //         } else {
+    //             res.add(prev);
+    //             prev = intervals[i];
+    //         }
+    //     }
+    //     //do not forget to add the last interval
+    //     res.add(prev);
+    //     return res.toArray(new int[res.size()][2]);
+    // }
+    
+    /*
+    follow up : return the length of the corverd intervals
+    similiar: only diff:
+    for (int[] interval : intervals) {
+            if (cur[1] >= interval[0]) {
+                cur[1] = Math.max(cur[1], interval[1]);
+            } else {
+                //cannot be merged
+                res += cur[1] - cur[0]
+                cur = interval;
+            }
+        }
+    res += res += cur[1] - cur[0]
+    */
+    public int[][] merge(int[][] intervals) {
+        int n = intervals.length;
+        if (n == 1) {
+            return intervals;
+        }
+        List<int[]> res = new LinkedList<>();
+        Arrays.sort(intervals, (a, b) -> (a[0] - b[0]));
+        //cur is merged interval
+        int[] cur = intervals[0];
+        for (int[] interval : intervals) {
+            if (cur[1] >= interval[0]) {
+                cur[1] = Math.max(cur[1], interval[1]);
+            } else {
+                //cannot be merged
+                res.add(cur);
+                cur = interval;
+            }
+        }
+        res.add(cur);
+        return res.toArray(new int[res.size()][2]);
+    }
+    
+    //TreeMap
+        /*
+            [1,4],[3,5]
+            TreeMap  key:   1   3    4    5
+            value(change):  +1  +1  -1  -1
+                    cnt:0    1   2   1   0     (1,5)
+                          
+        */         
+        // List<int[]> res = new LinkedList<>();
+        // if (intervals == null || intervals.length == 0) {
+        //     return new int[0][];
+        // }
+        // Map<Integer, Integer> map = new TreeMap<>();
+        // for (int[] interval : intervals) {
+        //     map.put(interval[0], map.getOrDefault(interval[0], 0) + 1);
+        //     map.put(interval[1], map.getOrDefault(interval[1], 0) - 1);
+        // }
+        // int cnt = 0;
+        // int start = 0;
+        // //当cnt开始为0，再一次变为0，这就是一个完整的interval
+        // for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+        //     if (cnt == 0) {
+        //         start = entry.getKey();
+        //     }
+        //     cnt += entry.getValue();
+        //     if (cnt == 0) {
+        //         res.add(new int[] {start, entry.getKey()});
+        //     }
+        // }
+        // return res.toArray(new int[res.size()][2]);
+        
+        // int[][] cur = new int[res.size()][2];
+        // for (int i = 0; i < res.size(); i++) {
+        //     cur[i] = res.get(i);
+        // }
+        // return cur;
+    
+    //heap
+//    public int[][] merge(int[][] intervals) {
+//         List<int[]> list = new ArrayList<>();
+//         Queue<Pair> pq = new PriorityQueue<>((x, y) -> x.a == y.a ? x.b - y.b : x.a - y.a);
+//         for (int[] arr : intervals) {
+//             pq.add(new Pair(arr[0], arr[1]));
+//         }
+
+//         if (pq.isEmpty()) return new int[][]{};
+
+//         Pair curr = pq.poll();
+//         while (!pq.isEmpty()) {
+//             Pair next = pq.poll();
+//             if (curr.b >= next.a) {
+//                 curr.a = Math.min(curr.a, next.a);
+//                 curr.b = Math.max(curr.b, next.b);
+//             } else {
+//                 list.add(new int[]{curr.a, curr.b});
+//                 curr = next;
+//             }
+//         }
+//         list.add(new int[]{curr.a, curr.b});
+//         int[][] res = new int[list.size()][2];
+//         int i = 0;
+//         for (int[] arr : list) {
+//             res[i] = arr;
+//             i++;
+//         }
+//         return res;
+//     }
+// }
+
+// class Pair {
+//     int a;
+//     int b;
+
+//     Pair(int x, int y) {
+//         a = x;
+//         b = y;
+//     }
+}
