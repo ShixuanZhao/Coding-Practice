8bf9d9bed1b581dca0e627ddbc9093bcff45e328
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-12-05 10:04:12 +0800

    Time: 10 ms (82.19%), Space: 57.6 MB (21.48%) - LeetHub

---
+++ 0286-walls-and-gates/0286-walls-and-gates.java
@@ -0,0 +1,31 @@
+class Solution {
+    public static final int[][] dirs = new int[][] {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+    public void wallsAndGates(int[][] rooms) {
+        Queue<int[]> q = new ArrayDeque<>();
+        int m = rooms.length;
+        int n = rooms[0].length;
+        boolean[][] visited = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (rooms[i][j] == 0) {
+                    q.offer(new int[] {i, j});
+                    visited[i][j] = true;
+                }
+            }
+        }
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            for (int[] dir : dirs) {
+                int x = cur[0] + dir[0];
+                int y = cur[1] + dir[1];
+                if (x < 0 || x >= m || y < 0 || y >= n || visited[x][y] || rooms[x][y] == -1) {
+                    continue;
+                }
+                q.offer(new int[] {x, y});
+                visited[x][y] = true;
+                //BFS第一次visited过的点，就是最短路径，并且不会被updated
+                rooms[x][y] = rooms[cur[0]][cur[1]] + 1;
+            }
+        }
+    }
+}
