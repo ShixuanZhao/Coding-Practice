b87d56bbd3b073c4f99fda9013e099c092baeaf2
Shixuan Zhao <74257885+ShixuanZhao@users.noreply.github.com>
2022-12-06 06:58:13 +0800

    Time: 2 ms (80.29%), Space: 50.1 MB (14.31%) - LeetHub

--- 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.java
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.java
@@ -34,6 +34,7 @@ class Solution {
         return dfs(head, root) || isSubPath(head, root.left) || isSubPath(head, root.right);
     }
     
+    //start from head, judge whether have a path
     private boolean dfs(ListNode head, TreeNode root) {
         if (head == null) {
             return true;
